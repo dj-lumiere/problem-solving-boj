4c9e4bb7c7a88a1dc95fc71f02bb38f2b1dbea5a
DJ_Lumiere <djminstar573@gmail.com>
2023-03-17 11:54:08 +0900

    [Gold III] Title: 나머지 합, Time: 468 ms, Memory: 262712 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.py
+++ 백준/Gold/10986. 나머지 합/나머지 합.py
@@ -6,7 +6,7 @@ accumulated_sum_list = [0]
 remainder_list = [0 for _ in range(M)]
 answer = 0
 for i, j in enumerate(A):
-    value = (accumulated_sum_list[-1] + j)%M
+    value = (accumulated_sum_list[-1] + j) % M
     accumulated_sum_list.append(value)
     if not value:
         answer += 1
