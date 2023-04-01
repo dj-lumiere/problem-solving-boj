8a0d09152dde1c1ec06094ad02a258370b7becd5
DJ_Lumiere <djminstar573@gmail.com>
2023-04-01 23:09:05 +0900

    [Diamond V] Title: N! mod P (2), Time: 1880 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Diamond/17467. N！ mod P （2）/N！ mod P （2）.cc
@@ -0,0 +1,48 @@
+/* 17467 N! mod P (2) */
+
+#include <bits/stdc++.h>
+#include <iostream>
+#include <math.h>
+#define int128 __int128_t
+#define int64 long long int
+#define int32 int
+#define uint128 __uint128_t
+#define uint64 uint64_t
+
+uint64 barrett_reduction(uint64 a, uint64 N)
+{
+    uint64 q;
+    uint64 r;
+    q = (uint64)(((uint128)(-1ULL / N) * a) >> 64);
+    r = a - q * N;
+    return r;
+}
+
+uint64 factorial_mod(uint64 n, uint64 p)
+{
+    uint64 fact1 = 1;
+    uint64 fact2 = 1;
+    for (uint64 i = 1; i < (n + 1) >> 1; i++)
+    {
+        fact1 = barrett_reduction(fact1 * 2 * i, p);
+        fact2 = barrett_reduction(fact2 * (2 * i + 1), p);
+    }
+    if (n % 2 == 0)
+    {
+        return barrett_reduction(barrett_reduction(fact1 * fact2, p) * n, p);
+    }
+    else
+    {
+        return barrett_reduction(fact1 * fact2, p);
+    }
+}
+
+int main()
+{
+    int64 n, p;
+    int64 answer;
+    answer = 1;
+    std::cin >> n >> p;
+    answer = factorial_mod(n, p);
+    std::cout << answer;
+}
