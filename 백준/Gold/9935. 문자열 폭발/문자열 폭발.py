2d7ae73881f875b03913c314ea1204fd0b79fb0c
DJ_Lumiere <djminstar573@gmail.com>
2023-04-02 11:30:14 +0900

    [Gold IV] Title: 문자열 폭발, Time: 500 ms, Memory: 71692 KB -BaekjoonHub

---
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -0,0 +1,16 @@
+# 9935 문자열 폭발
+
+letter_list = list(input())
+explosion_list = list(input())
+explosion_length = len(explosion_list)
+after_explosion = []
+for i, j in enumerate(letter_list):
+    after_explosion.append(j)
+    if explosion_list[-1] == j:
+        if explosion_list == after_explosion[-len(explosion_list):]:
+            for k in range(len(explosion_list)):
+                after_explosion.pop()
+if after_explosion:
+    print(*after_explosion, sep="")
+else:
+    print("FRULA")
