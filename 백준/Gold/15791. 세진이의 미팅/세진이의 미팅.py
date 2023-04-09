dcf33a1aa0df8b147c3f895615465c3c32276136
DJ_Lumiere <djminstar573@gmail.com>
2023-04-09 12:44:45 +0900

    [Gold I] Title: 세진이의 미팅, Time: 248 ms, Memory: 47320 KB -BaekjoonHub

---
+++ 백준/Gold/15791. 세진이의 미팅/세진이의 미팅.py
@@ -0,0 +1,45 @@
+# 15791 세진이의 미팅
+
+def find_prime() -> list[bool]:
+    a = 1000000
+    prime_list: list[bool] = [True for i in range(0, a + 1)]
+    prime_list[0] = False
+    prime_list[1] = False
+    prime_list2 = []
+    for x in range(1, a+1):
+        if prime_list[x]:
+            prime_list[x : a + 1 : x] = [False] * (a // x)
+            prime_list[x] = True
+            prime_list2.append(x)
+        else:
+            continue
+    return prime_list2
+
+prime_list = find_prime()
+
+prime_length = len(prime_list)
+
+factor_counter_up = [0 for i in range(prime_length)]
+factor_counter_down = [0 for i in range(prime_length)]
+
+N, K = list(map(int, input().split(" ")))
+if K * 2 > N:
+    K = N - K
+for i, j in enumerate(prime_list):
+    if j > N:
+        break
+    next_factor = 1
+    while next_factor <= N:
+        next_factor *= j
+        factor_counter_up[i] += N // next_factor - (N - K) // next_factor
+        factor_counter_down[i] += K // next_factor
+factor_counter = [i - j for i, j in zip(factor_counter_up, factor_counter_down)]
+remainder = 1
+for i, j in zip(prime_list, factor_counter):
+    if i > N:
+        break
+    if j == 0:
+        continue
+    else:
+        remainder = remainder * (i**j) % 1000000007
+print(remainder)
