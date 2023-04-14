44b55a009bed1e5add280fd9cfd325430cf57c7b
DJ_Lumiere <djminstar573@gmail.com>
2023-04-14 13:06:12 +0900

    [Silver II] Title: 타일링, Time: 52 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/1793. 타일링/타일링.py
@@ -0,0 +1,38 @@
+# 1793 타일링
+
+from sys import stdin
+
+
+# 공백이 될 때까지 아이템을 불러오는 함수
+def loading_list() -> list[int]:
+    string_list = []
+    while True:
+        string_element = stdin.readline().rstrip()
+        if not string_element:
+            return string_list
+        else:
+            string_list.append(int(string_element))
+
+
+def tiling_number(n: int) -> int:
+    # f(n) = 2*f(n-2)+f(n-1)
+    memo = [1, 3]
+    if n == 0:
+        return 1
+    elif n == 1:
+        return 1
+    elif n == 2:
+        return 3
+    else:
+        for i in range(n - 2):
+            memo[i % 2] = 2 * memo[(i - 2) % 2] + memo[(i - 1) % 2]
+        return memo[(n - 1) % 2]
+
+
+def main():
+    queries = loading_list()
+    for i in queries:
+        print(tiling_number(i))
+
+
+main()
