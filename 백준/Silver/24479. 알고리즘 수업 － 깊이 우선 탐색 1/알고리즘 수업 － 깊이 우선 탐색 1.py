cf5a36c4d16f188b26974472ca865e30b8e52c37
DJ_Lumiere <djminstar573@gmail.com>
2023-05-18 00:45:26 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 812 ms, Memory: 88556 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.py
@@ -6,7 +6,7 @@ N, M, R = list(map(int, input().split(" ")))
 
 graph_dict = dict()
 
-for i in range(N+1):
+for i in range(N + 1):
     graph_dict[i] = []
 
 for i in range(M):
@@ -14,23 +14,25 @@ for i in range(M):
     graph_dict[s].append(e)
     graph_dict[e].append(s)
 
-for i in range(N+1):
+for i in range(N + 1):
     if graph_dict[i]:
         graph_dict[i].sort()
 
-visited:list[bool] = [False for i in range(N+1)]
-visited_order:dict[int, int] = dict()
+visited: list[bool] = [False for i in range(N + 1)]
+visited_order: dict[int, int] = dict()
 
-def dfs(V:list[bool],E:dict[int, list[int]],R:int):
+
+def dfs(V: list[bool], E: dict[int, list[int]], R: int):
     visited_order[R] = len(visited_order) + 1
     V[R] = True
     for i in E[R]:
         if not visited[i]:
-            dfs(V,E,i)
+            dfs(V, E, i)
+
 
 dfs(visited, graph_dict, R)
 
-for i in range(1, N+1):
+for i in range(1, N + 1):
     if i in visited_order:
         stdout.writelines(f"{visited_order[i]}\n")
     else:
