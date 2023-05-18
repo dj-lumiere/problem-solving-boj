12432d4bd88d1a80775358ee5b878b4332fcaff1
DJ_Lumiere <djminstar573@gmail.com>
2023-05-18 11:30:13 +0900

    [Silver V] Title: 피보나치 수 4, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.py
@@ -0,0 +1,13 @@
+# 10826 피보나치 수 4
+
+memo = [0, 0]
+
+N = int(input())
+for i in range(N + 1):
+    if i == 0:
+        memo[0] = 0
+    elif i == 1:
+        memo[1] = 1
+    else:
+        memo[i % 2] = sum(memo)
+print(memo[N % 2])
