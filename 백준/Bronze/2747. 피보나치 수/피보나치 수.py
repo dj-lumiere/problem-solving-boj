009ecfaae394882222c1b12baeb072374397fadc
DJ_Lumiere <djminstar573@gmail.com>
2023-05-20 20:52:57 +0900

    [Bronze II] Title: 피보나치 수, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.py
@@ -0,0 +1,13 @@
+# 2747 피보나치 수
+
+memo = [0, 0]
+
+N = int(input())
+for i in range(N + 1):
+    if i == 0:
+        memo[0] = 0
+    elif i == 1:
+        memo[1] = 1
+    else:
+        memo[i % 2] = sum(memo)
+print(memo[N % 2])
