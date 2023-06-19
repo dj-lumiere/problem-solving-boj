4d9ddebd4c1f4c525a9b366f42c3f2e5b814f334
DJ_Lumiere <djminstar573@gmail.com>
2023-06-19 10:34:15 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.py
@@ -0,0 +1,9 @@
+first_string = input()
+if first_string == "SONGDO":
+    print("HIGHSCHOOL")
+elif first_string == "CODE":
+    print("MASTER")
+elif first_string == "2023":
+    print("0611")
+elif first_string == "ALGORITHM":
+    print("CONTEST")
