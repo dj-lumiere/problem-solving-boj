063c52d71b943b4a3aca6d35b9fd8f825aed2375
DJ_Lumiere <djminstar573@gmail.com>
2023-06-20 16:54:00 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.py
@@ -0,0 +1,7 @@
+# 2445 별 찍기 - 8
+N = int(input())
+
+for i in range(1, N + 1):
+    print("*" * i + " " * (2 * N - 2 * i) + "*" * i)
+for i in range(N - 1, 1 - 1, -1):
+    print("*" * i + " " * (2 * N - 2 * i) + "*" * i)
