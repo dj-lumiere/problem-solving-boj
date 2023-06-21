94311365a976c2f8f6d30f7525d69179b8b3b7d6
DJ_Lumiere <djminstar573@gmail.com>
2023-06-21 11:27:59 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 124 ms, Memory: 34832 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,25 @@
+# 1541 잃어버린 괄호
+from re import split
+
+formula = input()
+formula_splitted = split(r"(\+|\-)", formula)
+numbers_in_formula = []
+is_negative = False
+for i, v in enumerate(formula_splitted):
+    if not v:
+        continue
+    elif v == "+":
+        is_negative = False
+    elif v == "-":
+        is_negative = True
+    elif v.isdigit() and not is_negative:
+        numbers_in_formula.append(int(v))
+    elif v.isdigit() and is_negative:
+        numbers_in_formula.append(-int(v))
+is_negative = False
+for i, v in enumerate(numbers_in_formula):
+    if v < 0 and not is_negative:
+        is_negative = True
+    elif v > 0 and is_negative:
+        numbers_in_formula[i] = -v
+print(sum(numbers_in_formula))
