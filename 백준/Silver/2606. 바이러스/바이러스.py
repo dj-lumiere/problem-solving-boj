d2f103ddfc48a39345a9699625bdf0ce0ddcc04d
DJ_Lumiere <djminstar573@gmail.com>
2023-06-23 21:31:50 +0900

    [Silver III] Title: 바이러스, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.py
+++ 백준/Silver/2606. 바이러스/바이러스.py
@@ -1,19 +1,20 @@
+# 2606 바이러스
+
 from sys import stdin
 
-N = int(input())
-M = int(input())
+input = stdin.readline
+
 
 def loading_tuple_n(n: int) -> list[list[int]]:
     string_list = []
     while n:
         n -= 1
-        string_element = stdin.readline().rstrip()
+        string_element = stdin.readline().strip()
         string_list.append(list(map(int, string_element.split(" "))))
     return string_list
 
-node_list = loading_tuple_n(M)
 
-def graphify(node_count:int, connections:list[list[int]]) -> list[list[int]]:
+def graphify(node_count: int, connections: list[list[int]]) -> list[list[int]]:
     graph = [[] for i in range(0, node_count + 1)]
     for i in connections:
         graph[i[0]].append(i[1])
@@ -22,22 +23,29 @@ def graphify(node_count:int, connections:list[list[int]]) -> list[list[int]]:
         graph[i].sort()
     return graph
 
-node_graph = graphify(N, node_list)
+
+def dfs(graph: list[list[int]], init: int) -> list[bool]:
+    visited: list[bool] = [False for i in range(N + 1)]
+    visited[init] = True
+    stack = []
+    stack.append(init)
+    while stack:
+        new_starting_pt = stack.pop()
+        for i in graph[new_starting_pt]:
+            stack.append(i)
+            if not visited[i]:
+                visited[i] = True
+            else:
+                stack.pop()
+    return visited
+
 
 def sol(graph: list[list[int]]) -> int:
-    check = [False for i in range(0, N+1)]
-    def dfs(graph, init, visited):
-        stack = []
-        stack.append(init)
-        while stack:
-            new_starting_pt = stack.pop()
-            for i in graph[new_starting_pt]:
-                stack.append(i)
-                if not visited[i]:
-                    visited[i] = True
-                else:
-                    stack.pop()
-        return visited
-    return sum(dfs(graph, 1, check))-1
+    return sum(dfs(graph, 1)) - 1
 
+
+N = int(input())
+M = int(input())
+node_list = loading_tuple_n(M)
+node_graph = graphify(N, node_list)
 print(sol(node_graph))
