84581963a0f0333abc62ecf15f1afb5c0c1c5c3c
DJ_Lumiere <djminstar573@gmail.com>
2023-06-27 19:28:55 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.py
@@ -4,7 +4,17 @@ N = int(input())
 for _ in range(N):
     student_count, *score = list(map(int, input().split(" ")))
     total_score = sum(score)
-    score_over_avg_count = (
-        sum([student_count * i > total_score for i in score]) * 100 / student_count
+    score_over_avg_count = sum([student_count * i > total_score for i in score])
+    score_over_avg_percentage = (score_over_avg_count * 1000000) // student_count
+    score_over_avg_percentage_int_part, score_over_avg_percentage_frac_part = divmod(
+        score_over_avg_percentage, 10000
     )
-    print(f"{score_over_avg_count:.3f}%")
+    if score_over_avg_percentage_frac_part % 10 >= 5:
+        score_over_avg_percentage_frac_part = (
+            score_over_avg_percentage_frac_part // 10 + 1
+        )
+    else:
+        score_over_avg_percentage_frac_part = score_over_avg_percentage_frac_part // 10
+    print(
+        f"{score_over_avg_percentage_int_part}.{score_over_avg_percentage_frac_part:0>3}%"
+    )
