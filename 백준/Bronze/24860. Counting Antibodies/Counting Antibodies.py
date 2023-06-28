49b7ac5308ad04d4b58bc9ad5f369dd03c45b60c
DJ_Lumiere <djminstar573@gmail.com>
2023-06-29 00:02:40 +0900

    [Bronze IV] Title: Counting Antibodies, Time: 56 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/24860. Counting Antibodies/Counting Antibodies.py
@@ -0,0 +1,6 @@
+# 24860 Counting Antibodies
+
+vk, jk = map(int, input().split(" "))
+vl, jl = map(int, input().split(" "))
+vh, dh, jh = map(int, input().split(" "))
+print((vk * jk + vl * jl) * vh * dh * jh)
