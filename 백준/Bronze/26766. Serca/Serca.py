ff9c87a39d6eb1867d578127728e73a839b92e19
DJ_Lumiere <djminstar573@gmail.com>
2023-06-29 20:52:40 +0900

    [Bronze V] Title: Serca, Time: 200 ms, Memory: 114996 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.py
@@ -0,0 +1,7 @@
+# 26766 Serca
+from sys import stdout
+
+heart = " @@@   @@@ \n@   @ @   @\n@    @    @\n@         @\n @       @ \n  @     @  \n   @   @   \n    @ @    \n     @     \n"
+n = int(input())
+for _ in range(n):
+    stdout.write(heart)
