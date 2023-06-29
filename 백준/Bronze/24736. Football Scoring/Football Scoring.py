07a60189205023fecb4a63f7b9bf70517d37cd55
DJ_Lumiere <djminstar573@gmail.com>
2023-06-29 19:08:12 +0900

    [Bronze V] Title: Football Scoring, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/24736. Football Scoring/Football Scoring.py
@@ -0,0 +1,6 @@
+# 24736 Football Scoring
+
+print(
+    sum([i * j for i, j in zip(list(map(int, input().split(" "))), [6, 3, 2, 1, 2])]),
+    sum([i * j for i, j in zip(list(map(int, input().split(" "))), [6, 3, 2, 1, 2])]),
+)
