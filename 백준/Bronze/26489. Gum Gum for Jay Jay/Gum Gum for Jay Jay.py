42404e5ba6ae25d58d1f21a0fd62497422c90e47
DJ_Lumiere <djminstar573@gmail.com>
2023-06-29 20:53:57 +0900

    [Bronze V] Title: Gum Gum for Jay Jay, Time: 60 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/26489. Gum Gum for Jay Jay/Gum Gum for Jay Jay.py
@@ -0,0 +1,12 @@
+# 26489 Gum Gum for Jay Jay
+
+result = 0
+while True:
+    try:
+        s = input()
+        if not s:
+            break
+        result += 1
+    except:
+        break
+print(result)
