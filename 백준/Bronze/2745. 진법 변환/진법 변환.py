b2387da4584811b68f77a901f8c95038f249b365
DJ_Lumiere <djminstar573@gmail.com>
2023-06-30 12:04:45 +0900

    [Bronze II] Title: 진법 변환, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/2745. 진법 변환/진법 변환.py
@@ -0,0 +1,20 @@
+# 2745 진법 변환
+
+
+def n_ary_change(number: str, original_base: int, converting_base: int) -> str:
+    number_in_base_10 = int(number, base=original_base)
+    result = []
+    while number_in_base_10 > 0:
+        number_in_base_10, mod = divmod(number_in_base_10, converting_base)
+        result.append(mod)
+    for i, v in enumerate(result):
+        if v >= 10:
+            result[i] = chr(ord("A") + v - 10)
+        else:
+            result[i] = str(v)
+    result.reverse()
+    return "".join(result)
+
+
+N, B = input().split(" ")
+print(n_ary_change(N, int(B), 10))
