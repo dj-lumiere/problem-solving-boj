fe229507a36f7d412691cf14001f84ccb7a2e3ae
DJ_Lumiere <djminstar573@gmail.com>
2023-07-11 18:46:32 +0900

    [Platinum V] Title: 분수, Time: 124 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Platinum/18117. 분수/분수.py
+++ 백준/Platinum/18117. 분수/분수.py
@@ -1,10 +1,14 @@
+# 18117 분수
+
+
 def find_digits(a: int, b: int, x: int, y: int) -> str:
     result = ""
     for i in range(x, y):
         result_sub = ((a * pow(10, i - 1, b) % b) * 10) // b
         result += str(result_sub)
     return result
-    
+
+
 t = int(input())
 for _ in range(t):
     a, b = map(int, input().split(" "))
