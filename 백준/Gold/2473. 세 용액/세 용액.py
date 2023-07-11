bad4ec61b89c72ad14b98b7989d21bf89e759c5e
DJ_Lumiere <djminstar573@gmail.com>
2023-07-11 23:51:28 +0900

    [Gold III] Title: 세 용액, Time: 192 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Gold/2473. 세 용액/세 용액.py
+++ 백준/Gold/2473. 세 용액/세 용액.py
@@ -1,26 +1,27 @@
 # 2473 세 용액
 
+ABSOLUTE_MAX_ACIDITY = 1_000_000_000 * 3 + 1
 N = int(input())
 # 1. 리스트를 받아서 정리
 num_list = list(map(int, input().split(" ")))
 num_list.sort()
-neutrality = 3000000000
-ptr = [0,0,0]
+acidity = ABSOLUTE_MAX_ACIDITY
+pointers = [0, 0, 0]
 # 2. 포인터를 하나 고정해둬서 리스트를 한정하기
-for ptr_i in range(N-2):
+for pointer1 in range(N - 2):
     # 3. 그 한정된 리스트에서 투 포인터 쓰기
-    ptr_j = ptr_i+1
-    ptr_k = N-1
-    while ptr_j < ptr_k:
-        neutrality_sub = num_list[ptr_i]+num_list[ptr_j]+num_list[ptr_k]
-        if neutrality > abs(num_list[ptr_i]+num_list[ptr_j]+num_list[ptr_k]):
-            ptr = [ptr_i, ptr_j, ptr_k]
-            neutrality = abs(num_list[ptr_i]+num_list[ptr_j]+num_list[ptr_k])
-        if neutrality_sub > 0:
-            ptr_k -= 1
-        elif neutrality_sub < 0:
-            ptr_j += 1
-        else:
+    pointer2 = pointer1 + 1
+    pointer3 = N - 1
+    while pointer2 < pointer3:
+        acidity_sub = num_list[pointer1] + num_list[pointer2] + num_list[pointer3]
+        if acidity > abs(acidity_sub):
+            pointers = [pointer1, pointer2, pointer3]
+            acidity = abs(acidity_sub)
+        if acidity_sub == 0:
             break
+        if acidity_sub > 0:
+            pointer3 -= 1
+        elif acidity_sub < 0:
+            pointer2 += 1
 
-print(" ".join(map(str, list(num_list[i] for i in ptr))))
+print(*[num_list[i] for i in pointers])
