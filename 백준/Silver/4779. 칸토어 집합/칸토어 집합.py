749181f5aad862f5608b4f3c7d2d5c3e1835cd31
DJ_Lumiere <djminstar573@gmail.com>
2023-07-12 18:32:25 +0900

    [Silver III] Title: 칸토어 집합, Time: 172 ms, Memory: 125856 KB -BaekjoonHub

--- 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -1,21 +1,21 @@
 # 4779 칸토어 집합
 
+
 def find_cantor_set(order: int) -> str:
     cantor_set_length = 3**order
-    result = ["-"]*cantor_set_length
-    if order == 0:
-        return "".join(result)
+    result = ["-"] * cantor_set_length
     stack = [(0, cantor_set_length)]
     while stack:
         current_position, window_length = stack.pop()
         if window_length == 1:
             continue
-        crop_start = current_position + window_length//3
-        crop_end = crop_start + window_length//3
-        result[crop_start:crop_end] = [" "] * (crop_end-crop_start)
-        stack.append((current_position, window_length//3))
-        stack.append((current_position + 2*window_length//3, window_length//3))
+        crop_start = current_position + window_length // 3
+        crop_end = crop_start + window_length // 3
+        result[crop_start:crop_end] = [" "] * (crop_end - crop_start)
+        stack.append((current_position, window_length // 3))
+        stack.append((current_position + 2 * window_length // 3, window_length // 3))
     return "".join(result)
+
 
 while True:
     try:
