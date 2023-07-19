95d1bae422cb82a552cf30301f5d871abbc0dbf7
DJ_Lumiere <djminstar573@gmail.com>
2023-07-20 00:59:18 +0900

    [Platinum V] Title: 크로아티아 수 읽기, Time: 48 ms, Memory: 33376 KB -BaekjoonHub

--- 백준/Platinum/3177. 크로아티아 수 읽기/크로아티아 수 읽기.py
+++ 백준/Platinum/3177. 크로아티아 수 읽기/크로아티아 수 읽기.py
@@ -16,101 +16,66 @@ def digit_length(target: int) -> int:
     return floor(log10(target))
 
 
-def find_nth_digit(target: int, n: int) -> int:
+def find_nth_digit(target: int, n: int, is_teen: bool) -> int:
+    threshold = (2 if is_teen else 1) * 10 ** (n + 1)
+    if target >= threshold:
+        return 10
     return target // (10**n) % 10
 
 
-def count_initials_at_ith_digit(
-    upper_limit: int, digit_order: int, digit_initials: str
-) -> tuple[dict[str, int], int]:
-    result = {"D": 0, "J": 0, "M": 0, "O": 0, "P": 0, "S": 0, "T": 0, "X": 0, "A": 0}
-    removed_number = 0
-    for i in range(10):
-        nth_digit = (
-            find_nth_digit(upper_limit, digit_order)
-            if upper_limit < 10 ** (digit_order + 1)
-            else 10
-        )
-        if nth_digit < i:
-            removable_number = 0
-        elif nth_digit == i:
-            removable_number = upper_limit % (10**digit_order) + 1
-        else:
-            removable_number = 10**digit_order
-        if digit_initials[i] == "A":
-            continue
-        result[digit_initials[i]] += removable_number
-        removed_number += removable_number
-    return result, removed_number
+def count_removable_number(
+    upper_limit: int, digit_order: int, i: int, is_teen: bool
+) -> int:
+    nth_digit = find_nth_digit(upper_limit, digit_order, is_teen)
+    if nth_digit < i:
+        return 0
+    elif nth_digit == i:
+        return upper_limit % (10**digit_order) + 1
+    else:
+        return 10**digit_order
 
 
-def count_initials_special(
-    upper_limit: int, digit_order: int, digit_initials: str
-) -> tuple[dict[str, int], int]:
+def count_initials_at_ith_digit(upper_limit: int, digit_order: int) -> dict[str, int]:
     result = {"D": 0, "J": 0, "M": 0, "O": 0, "P": 0, "S": 0, "T": 0, "X": 0, "A": 0}
-    removed_number = 0
+    digit_initials = select_digit_string(digit_order)
+    if digit_order % 3 == 1:
+        subresult = count_initials_teens(upper_limit, digit_order - 1)
+        for key, value in subresult.items():
+            result[key] += value
     for i in range(10):
-        nth_digit = (
-            find_nth_digit(upper_limit, digit_order)
-            if upper_limit < 20 * 10**digit_order
-            else 10
-        )
-        if nth_digit < i:
-            removable_number = 0
-        elif nth_digit == i:
-            removable_number = upper_limit % (10**digit_order) + 1
-        else:
-            removable_number = 10**digit_order
+        removable_number = count_removable_number(upper_limit, digit_order, i, False)
         result[digit_initials[i]] += removable_number
-        removed_number += removable_number
-    return result, removed_number
+    return result
+
+
+def count_initials_teens(upper_limit: int, digit_order: int) -> dict[str, int]:
+    result = {"D": 0, "J": 0, "M": 0, "O": 0, "P": 0, "S": 0, "T": 0, "X": 0, "A": 0}
+    for i in range(10):
+        removable_number = count_removable_number(upper_limit, digit_order, i, True)
+        result[teen_digit_initial[i]] += removable_number
+    return result
+
+
+def select_digit_string(digit_order: int) -> str:
+    if digit_order == 0:
+        return one_digit_initial
+    if digit_order == 3:
+        return four_digit_initial
+    if digit_order % 3 == 0:
+        return seven_digit_initial
+    if digit_order % 3 == 1:
+        return two_digit_initial
+    if digit_order % 3 == 2:
+        return three_digit_initial
+    return ""
 
 
 def count_initials(upper_limit: int, target_initial: str) -> int:
     result = {"D": 0, "J": 0, "M": 0, "O": 0, "P": 0, "S": 0, "T": 0, "X": 0, "A": 0}
-    leftover_numbers = upper_limit
     for digit_order in range(digit_length(upper_limit) + 1):
-        if digit_order == 0:
-            subresult, removed_number = count_initials_at_ith_digit(
-                upper_limit, digit_order, one_digit_initial
-            )
-            for key, value in subresult.items():
-                result[key] += value
-            leftover_numbers -= removed_number
-        elif digit_order == 3:
-            subresult, removed_number = count_initials_at_ith_digit(
-                upper_limit, digit_order, four_digit_initial
-            )
-            for key, value in subresult.items():
-                result[key] += value
-            leftover_numbers -= removed_number
-        elif digit_order > 3 and digit_order % 3 == 0:
-            subresult, removed_number = count_initials_at_ith_digit(
-                upper_limit, digit_order, seven_digit_initial
-            )
-            for key, value in subresult.items():
-                result[key] += value
-            leftover_numbers -= removed_number
-        elif digit_order % 3 == 1:
-            subresult1, removed_number = count_initials_special(
-                upper_limit, digit_order - 1, teen_digit_initial
-            )
-            leftover_numbers -= removed_number
-            subresult, removed_number = count_initials_at_ith_digit(
-                upper_limit, digit_order, two_digit_initial
-            )
-            for key, value in subresult1.items():
-                result[key] += value
-            for key, value in subresult.items():
-                result[key] += value
-            leftover_numbers -= removed_number
-        elif digit_order % 3 == 2:
-            subresult, removed_number = count_initials_at_ith_digit(
-                upper_limit, digit_order, three_digit_initial
-            )
-            for key, value in subresult.items():
-                result[key] += value
-            leftover_numbers -= removed_number
+        subresult = count_initials_at_ith_digit(upper_limit, digit_order)
+        for key, value in subresult.items():
+            result[key] += value
     return result[target_initial]
 
 
