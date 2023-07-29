d95d96e80e8617410154ec0858618597e536ae22
DJ_Lumiere <djminstar573@gmail.com>
2023-07-29 13:30:08 +0900

    [Gold IV] Title: 플로이드, Time: 292 ms, Memory: 116160 KB -BaekjoonHub

---
+++ 백준/Gold/11404. 플로이드/플로이드.py
@@ -0,0 +1,28 @@
+# 11404 플로이드
+from itertools import product
+
+N, M = int(input()), int(input())
+INF = 10**9
+distance = [[INF for _ in range(N + 1)] for _ in range(N + 1)]
+original_waypoint = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+waypoint = [[0 for _ in range(N + 1)] for _ in range(N + 1)]
+for _ in range(M):
+    start, end, cost = map(int, input().split(" "))
+    distance[start][end] = min(cost, distance[start][end])
+    original_waypoint[start][end] = 1
+    waypoint[start][end] = end
+for mid, start, end in product(range(1, N + 1), range(1, N + 1), range(1, N + 1)):
+    if start == end:
+        distance[start][end] = 0
+        continue
+    if distance[start][mid] + distance[mid][end] < distance[start][end]:
+        distance[start][end] = distance[start][mid] + distance[mid][end]
+        waypoint[start][end] = waypoint[start][mid]
+for i in range(1, N + 1):
+    new_list = []
+    for j in range(1, N + 1):
+        if distance[i][j] == INF:
+            new_list.append("0")
+            continue
+        new_list.append(str(distance[i][j]))
+    print(" ".join(new_list))
