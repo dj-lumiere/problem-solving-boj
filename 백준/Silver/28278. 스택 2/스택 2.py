abe2ceecf7f37ad31789c5888a564ed9894f17d1
DJ_Lumiere <djminstar573@gmail.com>
2023-08-02 15:17:42 +0900

    [Silver IV] Title: 스택 2, Time: 588 ms, Memory: 144684 KB -BaekjoonHub

---
+++ 백준/Silver/28278. 스택 2/스택 2.py
@@ -0,0 +1,78 @@
+# 28278 스택 2
+
+from sys import stdin, stdout
+
+input = stdin.readline
+print = stdout.write
+
+
+class ListDeque:
+    def __init__(self):
+        self.val = [-1] * 2000001
+        self.deque_left_ptr = 1000000
+        self.deque_right_ptr = 999999
+        self.length = 0
+
+    def __str__(self):
+        result = "> "
+        for i in range(self.deque_left_ptr, self.deque_right_ptr + 1, 1):
+            result += str(self.val[i]) + " "
+        return f"{result}<"
+
+    def push_left(self, X: int):
+        self.val[self.deque_left_ptr - 1] = X
+        self.deque_left_ptr -= 1
+        self.length += 1
+
+    def push_right(self, X: int):
+        self.val[self.deque_right_ptr + 1] = X
+        self.deque_right_ptr += 1
+        self.length += 1
+
+    def pop_left(self) -> int:
+        if self.length != 0:
+            self.deque_left_ptr += 1
+            self.length -= 1
+            return self.val[self.deque_left_ptr - 1]
+        return -1
+
+    def pop_right(self) -> int:
+        if self.length != 0:
+            self.deque_right_ptr -= 1
+            self.length -= 1
+            return self.val[self.deque_right_ptr + 1]
+        return -1
+
+    def len(self) -> int:
+        return self.length
+
+    def is_empty(self) -> int:
+        if self.length == 0:
+            return 1
+        return 0
+
+    def return_left(self) -> int:
+        if self.length == 0:
+            return -1
+        return self.val[self.deque_left_ptr]
+
+    def return_right(self) -> int:
+        if self.length == 0:
+            return -1
+        return self.val[self.deque_right_ptr]
+
+
+N = int(stdin.readline())
+stack: ListDeque = ListDeque()
+for _ in range(N):
+    opcode, *operand = map(int, input().rstrip().split(" "))
+    if opcode == 1:
+        stack.push_right(operand[0])
+    elif opcode == 2:
+        print(f"{stack.pop_right()}\n")
+    elif opcode == 3:
+        print(f"{stack.len()}\n")
+    elif opcode == 4:
+        print(f"{stack.is_empty()}\n")
+    elif opcode == 5:
+        print(f"{stack.return_right()}\n")
