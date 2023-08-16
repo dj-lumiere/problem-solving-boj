c2de1d36fd12b13f22b2208a128ea49dacd40ccd
DJ_Lumiere <djminstar573@gmail.com>
2023-08-16 20:39:59 +0900

    [Gold IV] Title: 결! 합!, Time: 112 ms, Memory: 113112 KB -BaekjoonHub

--- 백준/Gold/16722. 결！ 합！/결！ 합！.py
+++ 백준/Gold/16722. 결！ 합！/결！ 합！.py
@@ -21,17 +21,30 @@ def is_hap(card_set, N):
     return True
 
 
+def find_hap(card):
+    hap = []
+    for card_set in combinations(range(1, 9 + 1), 3):
+        if is_hap(map(lambda x: card[x - 1], card_set), 3):
+            hap.append(list(card_set))
+    return hap
+
+
+def is_valid_hap(hap, hap_visited, numbers):
+    return hap and numbers in hap and not hap_visited[hap.index(numbers)]
+
+
+def is_valid_gyul(hap, hap_visited, gyul_visited):
+    return (not hap or all(hap_visited)) and not gyul_visited
+
+
 card = []
 symbol = {"CIRCLE": 0, "TRIANGLE": 1, "SQUARE": 2}
 color = {"YELLOW": 0, "RED": 1, "BLUE": 2}
 background = {"GRAY": 0, "WHITE": 1, "BLACK": 2}
-hap = []
 for _ in range(9):
     s, c, b = input().split(" ")
     card.append((symbol[s], color[c], background[b]))
-for card_set in combinations(range(1, 9 + 1), 3):
-    if is_hap(map(lambda x: card[x - 1], card_set), 3):
-        hap.append(list(card_set))
+hap = find_hap(card)
 score = 0
 N = int(input())
 hap_visited = [False] * len(hap)
@@ -40,11 +53,11 @@ for _ in range(N):
     op, *numbers = input().split(" ")
     numbers = list(map(int, numbers))
     numbers.sort()
-    if op == "H" and hap and numbers in hap and not hap_visited[hap.index(numbers)]:
+    if op == "H" and is_valid_hap(hap, hap_visited, numbers):
         hap_visited[hap.index(numbers)] = True
         score += 1
         continue
-    if op == "G" and (not hap or all(hap_visited)) and not gyul_visited:
+    if op == "G" and is_valid_gyul(hap, hap_visited, gyul_visited):
         gyul_visited = True
         score += 3
         continue
