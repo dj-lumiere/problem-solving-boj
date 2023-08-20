9f482281c83770ab57951af8c9fd9202183a324b
DJ_Lumiere <djminstar573@gmail.com>
2023-08-20 23:20:10 +0900

    [Diamond V] Title: バクテリアの増殖 (Large), Time: 412 ms, Memory: 117468 KB -BaekjoonHub

--- 백준/Diamond/12446. バクテリアの増殖 （Large）/バクテリアの増殖 （Large）.py
+++ 백준/Diamond/12446. バクテリアの増殖 （Large）/バクテリアの増殖 （Large）.py
@@ -6,59 +6,73 @@ def input():
     return stdin.readline().strip()
 
 
-def precompute_euler_phi(limit: int, eulerphi: list[int]):
-    prime_flags = [False, False] + [True for _ in range(2, limit + 1)]
-    for i in range(2, int(limit**0.5) + 1):
-        if not prime_flags[i]:
-            continue
-        prime_flags[i * 2 : limit + 1 : i] = [False] * (limit // i - 1)
+def compute_euler_phi(limit: int) -> list[int]:
+    is_prime = [False, False] + [True for _ in range(2, limit + 1)]
+    phi_values = [i for i in range(limit + 1)]
+
     for i in range(2, limit + 1):
-        if not prime_flags[i]:
+        if not is_prime[i]:
             continue
         for j in range(i, limit + 1, i):
-            eulerphi[j] *= i - 1
-            eulerphi[j] //= i
+            is_prime[j] = False
+            phi_values[j] = (phi_values[j] * (i - 1)) // i
+    return phi_values
 
 
-def find_maximum_depth(eulerphi: list[int], B: int, C: int) -> tuple[list[int], int]:
-    euler_phi_list = [0] * B
-    euler_phi_list[0] = C
-    maximum_step = B - 1
-    for j in range(1, B):
-        euler_phi_list[j] = eulerphi[euler_phi_list[j - 1]]
-        if euler_phi_list[j] == 1:
-            maximum_step = j - 1
+def find_max_depth(phi_values: list[int], B: int, C: int) -> tuple[list[int], int]:
+    euler_phi_list = [C]
+    for _ in range(1, B):
+        next_value = phi_values[euler_phi_list[-1]]
+        euler_phi_list.append(next_value)
+        if next_value == 1:
             break
-    return euler_phi_list[: maximum_step + 1], maximum_step + 1
+    return euler_phi_list, len(euler_phi_list)
 
 
-def calculate(eulerphi: list[int], A: int, B: int, C: int) -> int:
-    mod_c_list, maximum_step = find_maximum_depth(eulerphi, B, C)
-    mod_c_list.append(1)
-    maximum_step += 1
-    dp = [[0] * min(i, maximum_step) for i in range(1, B + 1)]
-    is_excessive = [[True] * min(i, maximum_step) for i in range(1, B + 1)]
-    dp[-1] = [pow(A, A, mod_c_list[i]) for i in range(maximum_step)]
+def is_exceeding_threshold(base: int, index: int, threshold: int) -> bool:
+    result = 1
+    while index:
+        if index & 1:
+            result *= base
+        base *= base
+        index >>= 1
+        if result >= threshold:
+            return False
+    return True
+
+
+def calculate(phi_values: list[int], A: int, B: int, C: int) -> int:
+    mod_c_list, max_step = find_max_depth(phi_values, B, C)
+    dp = [[0] * min(i, max_step) for i in range(1, B + 1)]
+    excessive_checks = [[True] * min(i, max_step) for i in range(1, B + 1)]
+
+    dp[-1] = [pow(A, A, mod_c_list[i]) for i in range(max_step)]
+
     if A <= 4:
-        is_excessive[-1] = [
-            A**A >= mod_c_list[i] for i in range(len(is_excessive[-1]))
+        excessive_checks[-1] = [
+            not is_exceeding_threshold(A, A, mod_c_list[i])
+            for i in range(len(excessive_checks[-1]))
         ]
+
     if A == 2 and B >= 2:
-        is_excessive[-2] = [256 >= mod_c_list[i] for i in range(len(is_excessive[-2]))]
+        excessive_checks[-2] = [
+            not is_exceeding_threshold(A, A, mod_c_list[i])
+            for i in range(len(excessive_checks[-2]))
+        ]
+
     for j in range(-2, -B - 1, -1):
-        for k in range(min(len(dp[j]), maximum_step - 1)):
+        for k in range(min(len(dp[j]), max_step - 1)):
             dp[j][k] = pow(
                 dp[j + 1][k],
-                dp[j + 1][k + 1] + mod_c_list[k + 1] * is_excessive[j + 1][k + 1],
+                dp[j + 1][k + 1] + mod_c_list[k + 1] * excessive_checks[j + 1][k + 1],
                 mod_c_list[k],
             )
     return dp[0][0]
 
 
-eulerphi = [i for i in range(1001)]
-precompute_euler_phi(1000, eulerphi)
+phi_values = compute_euler_phi(1000)
 N = int(input())
 for i in range(1, N + 1):
     A, B, C = map(int, input().split(" "))
-    result = calculate(eulerphi, A, B, C)
+    result = calculate(phi_values, A, B, C)
     print(f"Case #{i}: {result}")
