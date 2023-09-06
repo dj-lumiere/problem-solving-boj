20f2be482005547bb096eeab2afd410fa1910c69
DJ_Lumiere <djminstar573@gmail.com>
2023-09-07 00:49:07 +0900

    [Bronze III] Title: 부호, Time: 304 ms, Memory: 115268 KB -BaekjoonHub

--- 백준/Bronze/1247. 부호/부호.py
+++ 백준/Bronze/1247. 부호/부호.py
@@ -1,21 +1,2 @@
-# 1247 부호
-
-from sys import stdin
-
-
-def input():
-    return stdin.readline().strip()
-
-
-def sign(target):
-    if target > 0:
-        return "+"
-    elif target == 0:
-        return "0"
-    else:
-        return "-"
-
-
-for _ in range(3):
-    n = int(input())
-    print(sign(sum(int(input()) for _ in range(n))))
+import sys;input=sys.stdin.readline;s=lambda t:"+"if t>0 else"-"if t<0 else"0"
+for _ in range(3):print(s(sum(int(input())for _ in range(int(input())))))
