59ff1e42bd88eaec4cf8c2908a91cc16312f1d2e
DJ_Lumiere <djminstar573@gmail.com>
2023-09-07 00:42:51 +0900

    [Silver V] Title: 행렬 곱셈, Time: 144 ms, Memory: 115408 KB -BaekjoonHub

--- 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
+++ 백준/Silver/2740. 행렬 곱셈/행렬 곱셈.py
@@ -1,11 +1,135 @@
-# 2740 행렬 곱셈
+# 행렬의 거듭제곱
+from itertools import product
 
-N, M = list(map(int, input().split(" ")))
-A = [list(map(int, input().split(" "))) for _ in range(N)]
-M, K = list(map(int, input().split(" ")))
-B = [list(map(int, input().split(" "))) for _ in range(M)]
-result = [
-    [sum([A[j][k] * B[k][i] for k in range(M)]) for i in range(K)] for j in range(N)
-]
-for j in range(N):
-    print(*result[j])
+
+class Matrix:
+    def __init__(self, matrix_values: list[list[int]]):
+        self.val: list[list[int]] = matrix_values
+        self.x_size: int = len(self.val[0])
+        self.y_size: int = len(self.val)
+
+    @staticmethod
+    def identity(n: int) -> "Matrix":
+        return Matrix([[1 if i == j else 0 for j in range(n)] for i in range(n)])
+
+    @staticmethod
+    def zero(n: int) -> "Matrix":
+        return Matrix([[0 for _ in range(n)] for _ in range(n)])
+
+    def __add__(self, other: "Matrix") -> "Matrix":
+        if not (self.x_size == other.x_size and self.y_size == other.y_size):
+            raise ValueError("You should have two identically sized matrix.")
+
+        result = [[0 for _ in range(self.x_size)] for _ in range(self.y_size)]
+
+        for i, j in product(range(self.x_size), range(self.y_size)):
+            result[j][i] = self.val[j][i] + other.val[j][i]
+
+        return Matrix(result)
+
+    def __sub__(self, other: "Matrix") -> "Matrix":
+        if not (self.x_size == other.x_size and self.y_size == other.y_size):
+            raise ValueError("You should have two identically sized matrix.")
+
+        result = [[0 for _ in range(self.x_size)] for _ in range(self.y_size)]
+
+        for i, j in product(range(self.x_size), range(self.y_size)):
+            result[j][i] = self.val[j][i] - other.val[j][i]
+
+        return Matrix(result)
+
+    def __mul__(self, other: "Matrix") -> "Matrix":
+        if self.x_size != other.y_size:
+            raise ValueError(
+                "The number of columns in the first matrix must equal the number of rows in the second matrix."
+            )
+
+        # Initialize the resulting matrix with zeros
+        result = [[0 for _ in range(other.x_size)] for _ in range(self.y_size)]
+
+        # Calculate matrix multiplication
+        for i, j in product(range(self.y_size), range(other.x_size)):
+            for k in range(self.x_size):
+                result[i][j] += self.val[i][k] * other.val[k][j]
+
+        return Matrix(result)
+
+    def __iadd__(self, other: "Matrix") -> "Matrix":
+        return self + other
+
+    def __isub__(self, other: "Matrix") -> "Matrix":
+        return self - other
+
+    def __imul__(self, other: "Matrix") -> "Matrix":
+        return self * other
+
+    def mulmod(self, other: "Matrix", mod: int) -> "Matrix":
+        if self.x_size != other.y_size:
+            raise ValueError(
+                "The number of columns in the first matrix must equal the number of rows in the second matrix."
+            )
+
+        # Initialize the resulting matrix with zeros
+        result = [[0 for _ in range(other.x_size)] for _ in range(self.y_size)]
+
+        # Calculate matrix multiplication
+        for i, j in product(range(self.y_size), range(other.x_size)):
+            for k in range(self.x_size):
+                result[i][j] += self.val[i][k] * other.val[k][j]
+            result[i][j] %= mod
+
+        return Matrix(result)
+
+    def __mulmod__(self, other: "Matrix", mod: int) -> "Matrix":
+        if self.x_size != other.y_size:
+            raise ValueError(
+                "The number of columns in the first matrix must equal the number of rows in the second matrix."
+            )
+
+        # Initialize the resulting matrix with zeros
+        result = [[0 for _ in range(other.x_size)] for _ in range(self.y_size)]
+
+        # Calculate matrix multiplication
+        for i, j in product(range(self.y_size), range(other.x_size)):
+            result[i][j] = (
+                sum(self.val[i][k] * other.val[k][j] for k in range(self.x_size)) % mod
+            )
+
+        return Matrix(result)
+
+    # This is just for representation when you print the matrix
+    def __str__(self) -> str:
+        return "\n".join([" ".join(map(str, row)) for row in self.val])
+
+    def matrix_square(self) -> "Matrix":
+        if self.x_size != self.y_size:
+            raise ValueError("Power of matrix is defined in square matrix.")
+        return self * self
+
+    def __exp__(self, index: int):
+        if self.x_size != self.y_size:
+            raise ValueError("Power of matrix is defined in square matrix.")
+        result = Matrix.identity(self.x_size)
+        while index:
+            if index & 1:
+                result *= self
+            self *= self
+            index >>= 1
+        return result
+
+    def pow(self, index: int, mod: int):
+        if self.x_size != self.y_size:
+            raise ValueError("Power of matrix is defined in square matrix.")
+        result = Matrix.identity(self.x_size)
+        while index:
+            if index & 1:
+                result = self.mulmod(result, mod)
+            self = self.mulmod(self, mod)
+            index >>= 1
+        return result
+
+n1, _ = map(int, input().split(" "))
+matrix_a = Matrix([list(map(int, input().split(" "))) for _ in range(n1)])
+n2, _ = map(int, input().split(" "))
+matrix_b = Matrix([list(map(int, input().split(" "))) for _ in range(n2)])
+print(matrix_a * matrix_b)
