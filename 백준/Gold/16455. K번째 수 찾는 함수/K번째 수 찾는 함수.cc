ff05b53684365191e8dcbb8f3d42052dd94fa66d
DJ_Lumiere <djminstar573@gmail.com>
2023-09-13 17:06:15 +0900

    [Gold II] Title: K번째 수 찾는 함수, Time: 88 ms, Memory: 21556 KB -BaekjoonHub

--- 백준/Gold/16455. K번째 수 찾는 함수/K번째 수 찾는 함수.cc
+++ 백준/Gold/16455. K번째 수 찾는 함수/K번째 수 찾는 함수.cc
@@ -1,7 +1,14 @@
 #include <vector>
 #include <algorithm>
+#include <random>
+#include <vector>
+
 int kth(std::vector<int> &a, int k)
 {
+    std::random_device rd;
+    std::mt19937 generator(rd());
+ 
+    std::shuffle(a.begin(), a.begin() + a.size() / 2, generator);
     k -= 1;
     nth_element(a.begin(), a.begin() + k, a.end());
     int ans = a[k];
