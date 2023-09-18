c588ac85c7ce1e68f4647f40c619adc4cb5a3a2b
DJ_Lumiere <djminstar573@gmail.com>
2023-09-18 19:26:49 +0900

    [Bronze I] Title: 초성 중성 종성, Time: 124 ms, Memory: 113112 KB -BaekjoonHub

---
+++ 백준/Bronze/11284. 초성 중성 종성/초성 중성 종성.py
@@ -0,0 +1,95 @@
+# 11284 초성 중성 종성
+
+first_consonant_list = [
+    "ㄱ",
+    "ㄲ",
+    "ㄴ",
+    "ㄷ",
+    "ㄸ",
+    "ㄹ",
+    "ㅁ",
+    "ㅂ",
+    "ㅃ",
+    "ㅅ",
+    "ㅆ",
+    "ㅇ",
+    "ㅈ",
+    "ㅉ",
+    "ㅊ",
+    "ㅋ",
+    "ㅌ",
+    "ㅍ",
+    "ㅎ",
+]
+middle_vowel_list = [
+    "ㅏ",
+    "ㅐ",
+    "ㅑ",
+    "ㅒ",
+    "ㅓ",
+    "ㅔ",
+    "ㅕ",
+    "ㅖ",
+    "ㅗ",
+    "ㅘ",
+    "ㅙ",
+    "ㅚ",
+    "ㅛ",
+    "ㅜ",
+    "ㅝ",
+    "ㅞ",
+    "ㅟ",
+    "ㅠ",
+    "ㅡ",
+    "ㅢ",
+    "ㅣ",
+]
+last_consonant_list = [
+    "",
+    "ㄱ",
+    "ㄲ",
+    "ㄳ",
+    "ㄴ",
+    "ㄵ",
+    "ㄶ",
+    "ㄷ",
+    "ㄹ",
+    "ㄺ",
+    "ㄻ",
+    "ㄼ",
+    "ㄽ",
+    "ㄾ",
+    "ㄿ",
+    "ㅀ",
+    "ㅁ",
+    "ㅂ",
+    "ㅄ",
+    "ㅅ",
+    "ㅆ",
+    "ㅇ",
+    "ㅈ",
+    "ㅊ",
+    "ㅋ",
+    "ㅌ",
+    "ㅍ",
+    "ㅎ",
+]
+first_consonant_count = len(first_consonant_list)
+middle_vowel_count = len(middle_vowel_list)
+last_consonant_count = len(last_consonant_list)
+
+first_korean_letter_index = ord("가")
+target_letter = input()
+target_letter_index_in_korean = ord(target_letter) - first_korean_letter_index
+middle_vowel_index, last_consonant_index = divmod(
+    target_letter_index_in_korean, last_consonant_count
+)
+first_consonant_index, middle_vowel_index = divmod(
+    middle_vowel_index, middle_vowel_count
+)
+print(
+    first_consonant_list[first_consonant_index],
+    middle_vowel_list[middle_vowel_index],
+    last_consonant_list[last_consonant_index],
+    sep="\n",
+)
