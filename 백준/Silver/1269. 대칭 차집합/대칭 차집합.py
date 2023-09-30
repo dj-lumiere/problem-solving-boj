dcf1416501ccfd4fffdd079fcc36502171479c26
DJ_Lumiere <djminstar573@gmail.com>
2023-09-30 14:50:47 +0900

    [Silver IV] Title: 대칭 차집합, Time: 316 ms, Memory: 182060 KB -BaekjoonHub

--- 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
+++ 백준/Silver/1269. 대칭 차집합/대칭 차집합.py
@@ -1,6 +1,18 @@
 # 1269 대칭 차집합
 
-A_size, B_size = list(map(int, input().split(" ")))
-A_set = set(map(int, input().split(" ")))
-B_set = set(map(int, input().split(" ")))
-print(len(A_set.difference(B_set).union(B_set.difference(A_set))))
+from collections import Counter
+from sys import stdin
+
+
+def input():
+    return stdin.readline().strip()
+
+
+n, m = map(int, input().split(" "))
+A = Counter(map(int, input().split(" ")))
+B = Counter(map(int, input().split(" ")))
+intersect = {}
+for k, v in A.items():
+    if k in B:
+        intersect[k] = min(v, B[k])
+print(n + m - 2 * sum(intersect.values()))
