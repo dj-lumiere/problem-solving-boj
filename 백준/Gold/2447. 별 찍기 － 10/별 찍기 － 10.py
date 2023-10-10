34e57f8a56d5524061f63728b6b498cf88c590cd
DJ_Lumiere <djminstar573@gmail.com>
2023-10-10 19:53:29 +0900

    [Gold V] Title: 별 찍기 - 10, Time: 1312 ms, Memory: 164532 KB -BaekjoonHub

--- 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
+++ 백준/Gold/2447. 별 찍기 － 10/별 찍기 － 10.py
@@ -1,35 +1,17 @@
 # 2447 별 찍기 - 10
 
-from sys import stdout
+from itertools import product
 
-grid_size = int(input())
-star_list = [["" for i in range(grid_size)] for j in range(grid_size)]
+N = int(input())
+grid = [["*" for _ in range(N)] for _ in range(N)]
 
-def mark_star(grid_size:int, row_offset:int, column_offset:int):
-    if grid_size == 3:
-        for i in range(3):
-            for j in range(3):
-                if i == 1 and j == 1:
-                    star_list[column_offset+j][row_offset+i] = " "
-                else:
-                    star_list[column_offset+j][row_offset+i] = "*"
-    else:
-        row_offset -= grid_size // 3
-        column_offset -= grid_size // 3
-        for i in range(3):
-            row_offset += grid_size // 3
-            for j in range(3):
-                column_offset += grid_size // 3
-                if i == 1 and j == 1:
-                    for x in range(grid_size // 3):
-                        for y in range(grid_size // 3):
-                            star_list[column_offset+y][row_offset+x]= " "
-                else:
-                    mark_star(grid_size//3,row_offset,column_offset)
-            column_offset -= grid_size
-        row_offset -= grid_size
+for i, j in product(range(N), repeat=2):
+    k = 1
+    while k < N:
+        if (i // k) % 3 == (j // k) % 3 == 1:
+            grid[i][j] = " "
+            break
+        k *= 3
 
-mark_star(grid_size,0,0)
-
-for i in star_list:
-    stdout.writelines("".join(i)+"\n")
+for v in grid:
+    print(*v, sep="")
