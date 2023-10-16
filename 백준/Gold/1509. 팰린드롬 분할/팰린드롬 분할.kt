3826cf9523813b42e80ba540b8c49c1dbf3e7a78
DJ_Lumiere <djminstar573@gmail.com>
2023-10-16 20:44:31 +0900

    [Gold I] Title: 팰린드롬 분할, Time: 960 ms, Memory: 118904 KB -BaekjoonHub

--- 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.kt
+++ 백준/Gold/1509. 팰린드롬 분할/팰린드롬 분할.kt
@@ -1,11 +1,10 @@
 package problemSolving
 
 import java.util.*
-import kotlin.collections.ArrayList
 
 fun main() = with(Scanner(System.`in`)) {
     val a: String = nextLine()
-    val aArray = ArrayList<Char>(a.map{it})
+    val aArray = ArrayList<Char>(a.map { it })
     val isPalindromic =
         ArrayList<ArrayList<Boolean>>(List(a.length) { ArrayList(List(a.length) { false }) })
     for (x in a.indices) {
@@ -18,16 +17,16 @@ fun main() = with(Scanner(System.`in`)) {
             isPalindromic[y][j] = (aArray[y] == aArray[j]) and (isPalindromic[y + 1][j - 1])
         }
     }
-    val minimalPartitionCount: ArrayList<Int> = ArrayList(List(a.length){0})
+    val minimalPartitionCount: ArrayList<Int> = ArrayList(List(a.length) { 0 })
     minimalPartitionCount[0] = 1
-    for (i in 1 until a.length){
-        val subCounts = ArrayList<Int>(List(1){minimalPartitionCount[i-1]+1})
-        for (j in 0 until i){
-            if (isPalindromic[j][i]){
+    for (i in 1 until a.length) {
+        val subCounts = ArrayList<Int>(List(1) { minimalPartitionCount[i - 1] + 1 })
+        for (j in 0 until i) {
+            if (isPalindromic[j][i]) {
                 subCounts.add(if (j >= 1) minimalPartitionCount[j - 1] + 1 else 1)
             }
         }
         minimalPartitionCount[i] = subCounts.min()
     }
-    println(minimalPartitionCount[minimalPartitionCount.size-1])
+    println(minimalPartitionCount[minimalPartitionCount.size - 1])
 }
