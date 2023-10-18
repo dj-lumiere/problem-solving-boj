6a11692fdaf9cdab2bce8e0208ef3afb934e8572
DJ_Lumiere <djminstar573@gmail.com>
2023-10-18 16:36:35 +0900

    [Silver II] Title: 마인크래프트, Time: 172 ms, Memory: 112544 KB -BaekjoonHub

--- 백준/Silver/18111. 마인크래프트/마인크래프트.py
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.py
@@ -1,28 +1,31 @@
 # 18111 마인크래프트
-
+from itertools import product
 from sys import stdin
 
-N, M, B = (int(i) for i in stdin.readline().strip().split(" "))
-INT_MAX = N * M * 256 * 2
-ground_level = [[int(i) for i in stdin.readline().strip().split(" ")] for _ in range(N)]
-minimum_ground_level = min((min(ground_level[i]) for i in range(N)))
-maximum_ground_level = max((max(ground_level[i]) for i in range(N)))
-minimum_time = INT_MAX
-leveled_height = 0
-for target_height in range(minimum_ground_level, maximum_ground_level + 1):
-    ground_to_etch = 0
-    ground_to_fill = 0
-    for r in range(N):
-        for c in range(M):
-            if target_height > ground_level[r][c]:
-                ground_to_fill += target_height - ground_level[r][c]
-            elif target_height < ground_level[r][c]:
-                ground_to_etch += ground_level[r][c] - target_height
-    if ground_to_fill > B + ground_to_etch:
-        continue
-    else:
-        answer_sub = ground_to_etch * 2 + ground_to_fill * 1
-        if minimum_time >= answer_sub:
-            leveled_height = target_height
-            minimum_time = answer_sub
-print(minimum_time, leveled_height)
+
+def input():
+    return stdin.readline().strip()
+
+
+N, M, B = map(int, input().split(" "))
+ground_level = [list(map(int, input().split(" "))) for _ in range(N)]
+cell_height_count = [0 for _ in range(257)]
+for i, j in product(range(N), range(M)):
+    cell_height_count[ground_level[i][j]] += 1
+result = N * M * 256 * 2 + 1
+result_sub = 0
+blocks_to_fill = 0
+timing = 0
+for i, j in product(range(257), repeat=2):
+    if j == 0:
+        result_sub = 0
+        blocks_to_fill = 0
+    result_sub += ((i - j) if i > j else 2 * (j - i)) * cell_height_count[j]
+    blocks_to_fill += (i - j) * cell_height_count[j]
+    if j == 256:
+        if blocks_to_fill > B:
+            continue
+        result = min(result, result_sub)
+        if result == result_sub:
+            timing = i
+print(result, timing)
