28ccea19353f1446af3d2a7f9fafdf211716a0ac
DJ_Lumiere <djminstar573@gmail.com>
2023-10-28 20:53:01 +0900

    [Bronze III] Title: 삼각수의 합, Time: 168 ms, Memory: 110404 KB -BaekjoonHub

--- 백준/Bronze/2721. 삼각수의 합/삼각수의 합.py
+++ 백준/Bronze/2721. 삼각수의 합/삼각수의 합.py
@@ -1,5 +1,6 @@
-triangle = lambda n: n*(n+1)//2
+# 2721 삼각수의 합
+triangle = lambda n: n * (n + 1) // 2
 T = int(input())
 for _ in range(T):
     N = int(input())
-    print(sum(triangle(i+1)*i for i in range(1, N+1)))
+    print(sum(triangle(i + 1) * i for i in range(1, N + 1)))
