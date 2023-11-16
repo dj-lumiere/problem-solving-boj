9a3023455e3c964d90e91ac0c0e0fa65e777ffca
DJ_Lumiere <djminstar573@gmail.com>
2023-11-16 13:40:10 +0900

    [Bronze I] Title: 행복 점수, Time: 112 ms, Memory: 109240 KB -BaekjoonHub

--- 백준/Bronze/30445. 행복 점수/행복 점수.py
+++ 백준/Bronze/30445. 행복 점수/행복 점수.py
@@ -3,6 +3,16 @@
 sentence = input()
 happy_point = sum([letter in "HAPPY" for letter in sentence])
 sad_point = sum([letter in "SAD" for letter in sentence])
-print(
-    f"{(happy_point) / (happy_point + sad_point) * 100 if any((happy_point, sad_point)) else 50:.2f}"
+happiness = (
+    (happy_point) * 100000 // (happy_point + sad_point)
+    if any((happy_point, sad_point))
+    else 50000
 )
+int_part, frac_part = divmod(happiness, 1000)
+frac_part, rounding_part = divmod(frac_part, 10)
+if rounding_part >= 5:
+    frac_part += 1
+    if frac_part >= 100:
+        frac_part -= 100
+        int_part += 1
+print(f"{int_part}.{frac_part:0>2}")
