2d9bd33b48d65c77cd2b083bfa779f67c0a44367
DJ_Lumiere <djminstar573@gmail.com>
2023-11-22 18:06:28 +0900

    [Gold IV] Title: 알파벳, Time: 5812 ms, Memory: 115060 KB -BaekjoonHub

--- 백준/Gold/1987. 알파벳/알파벳.py
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -1,46 +1,30 @@
 # 1987 알파벳
-from functools import lru_cache
-
 R, C = list(map(int, input().split(" ")))
-A = ord("A")
-alphabet_to_order = lambda x: ord(x) - A
-graph: list[list[int]] = [list(map(alphabet_to_order, input())) for _ in range(R)]
+graph: list[list[str]] = [list(input()) for _ in range(R)]
 delta_list: list[tuple[int, int]] = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 
 
-@lru_cache(maxsize=None)
-def dfs(
-    pos_x: int,
-    pos_y: int,
-    available_alphabet: int,
-    available_alphabet_count: int,
-) -> int:
-    value = graph[pos_y][pos_x]
-    if available_alphabet & (1 << value):
-        available_alphabet = available_alphabet ^ (1 << value)
-        max_count: int = 0
-        for i, j in delta_list:
-            new_x = pos_x + i
-            new_y = pos_y + j
-            if 0 <= new_x < C and 0 <= new_y < R:
-                count = dfs(
-                    pos_x=new_x,
-                    pos_y=new_y,
-                    available_alphabet=available_alphabet,
-                    available_alphabet_count=available_alphabet_count + 1,
-                )
-                max_count = max(max_count, count)
-        available_alphabet = available_alphabet ^ (1 << value)
-        return max_count
-    else:
-        return available_alphabet_count
+def iterative_dfs():
+    visited_alphabet = (1 << 27) - 1
+    stack = [(0, 0, visited_alphabet, 0)]  # pos_x, pos_y, available_alphabet, count
+    max_count = 0
 
+    while stack:
+        pos_x, pos_y, available_alphabet, count = stack.pop()
+        alphabet_order = ord(graph[pos_y][pos_x]) - ord("A")
+        
+        if available_alphabet & (1 << alphabet_order):
+            available_alphabet ^= (1 << alphabet_order)
+            max_count = max(max_count, count + 1)
+            
+            for dx, dy in delta_list:
+                new_x, new_y = pos_x + dx, pos_y + dy
+                if 0 <= new_x < C and 0 <= new_y < R:
+                    stack.append((new_x, new_y, available_alphabet, count + 1))
+            
+            available_alphabet ^= (1 << alphabet_order)
 
-visited_alphabet: int = (1 << 26) - 1
-answer: int = dfs(
-    pos_x=0,
-    pos_y=0,
-    available_alphabet=visited_alphabet,
-    available_alphabet_count=0,
-)
+    return max_count
+
+answer = iterative_dfs()
 print(answer)
