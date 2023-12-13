bc8a565aeae35c294affd511d97998ae2f2dae66
DJ_Lumiere <djminstar573@gmail.com>
2023-12-14 01:07:09 +0900

    [Platinum II] Title: Quaternion inverse, Time: 360 ms, Memory: 117120 KB -BaekjoonHub

--- 백준/Platinum/10909. Quaternion inverse/Quaternion inverse.py
+++ 백준/Platinum/10909. Quaternion inverse/Quaternion inverse.py
@@ -8,11 +8,7 @@ def input():
     return stdin.readline().strip()
 
 
-def sol(a, b, c, d, M):
-    """
-    sy.solve([a*w-b*x-c*y-d*z-1, a*x+b*w+c*z-d*y, a*y-b*z+c*w+d*x, a*z+b*y-c*x+d*w], [w, x, y, z])
-    -> {w: a/(a**2 + b**2 + c**2 + d**2), x: -b/(a**2 + b**2 + c**2 + d**2), y: -c/(a**2 + b**2 + c**2 + d**2), z: -d/(a**2 + b**2 + c**2 + d**2)}
-    """
+def sol(M, a, b, c, d):
     e = a**2 + b**2 + c**2 + d**2
     g = gcd(e, M)
     if g != 1:
@@ -24,4 +20,21 @@ def sol(a, b, c, d, M):
 
 M, T = map(int, input().split())
 for _ in range(T):
-    print(*sol(*map(int, input().split()), M))
+    print(*sol(M, *map(int, input().split())))
+
+
+"""
+import sympy as sy
+
+a1, b1, c1, d1, a2, b2, c2, d2 = sy.symbols("a1 b1 c1 d1 a2 b2 c2 d2")
+solution = sy.solve(
+    [
+        a1 * a2 - b1 * b2 - c1 * c2 - d1 * d2 - 1,
+        a1 * b2 + b1 * a2 + c1 * d2 - d1 * c2,
+        a1 * c2 - b1 * d2 + c1 * a2 + d1 * b2,
+        a1 * d2 + b1 * c2 - c1 * b2 + d1 * a2,
+    ],
+    [a2, b2, c2, d2],
+)
+print(solution) # {a2: a1/(a1**2 + b1**2 + c1**2 + d1**2), b2: -b1/(a1**2 + b1**2 + c1**2 + d1**2), c2: -c1/(a1**2 + b1**2 + c1**2 + d1**2), d2: -d1/(a1**2 + b1**2 + c1**2 + d1**2)}
+"""
