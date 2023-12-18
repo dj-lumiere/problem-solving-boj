b905e297751d8405ab4482539b0572d4f7f0257f
DJ_Lumiere <djminstar573@gmail.com>
2023-12-19 02:29:16 +0900

    [Bronze III] Title: 서울사이버대학을 다니고, Time: 212 ms, Memory: 119560 KB -BaekjoonHub

---
+++ 백준/Bronze/30958. 서울사이버대학을 다니고/서울사이버대학을 다니고.py
@@ -0,0 +1,8 @@
+# 30958 서울사이버대학을 다니고
+
+from re import split
+from collections import Counter
+from string import ascii_lowercase
+
+_, S = input(), input()
+print(max(Counter([i for i in S if i in ascii_lowercase]).values()))
