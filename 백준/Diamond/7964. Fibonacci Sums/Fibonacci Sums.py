8c527b4922e49cf085a25053300f4764614007d0
DJ_Lumiere <djminstar573@gmail.com>
2024-01-03 12:03:26 +0900

    [Diamond III] Title: Fibonacci Sums, Time: 680 ms, Memory: 279680 KB -BaekjoonHub

--- 백준/Diamond/7964. Fibonacci Sums/Fibonacci Sums.py
+++ 백준/Diamond/7964. Fibonacci Sums/Fibonacci Sums.py
@@ -5,66 +5,44 @@ from sys import stdin
 N1, *A1 = list(map(int, stdin.readline().strip().split(" ")))
 N2, *A2 = list(map(int, stdin.readline().strip().split(" ")))
 
-if A1 == [0] and A2 == [0]:
-    print(1, 0)
-else:
-    N = max(N1, N2) + 3
-    result = [i + j for (i, j) in zip_longest(A1, A2, fillvalue=0)]
-    result.append(0)
-    result.append(0)
-    result.append(0)
-    
-    # First stage: Move from right to left, removing 2's
-    for i in range(len(result) - 3, -1, -1):
-        x = result[i]
-        if result[i + 1 : i + 4] == [0, 2, 0]:
-            result[i : i + 4] = [x + 1, 0, 0, 1]
-        elif result[i + 1 : i + 4] == [0, 3, 0]:
-            result[i : i + 4] = [x + 1, 0, 1, 1]
-        elif result[i + 1 : i + 4] == [1, 2, 0]:
-            result[i : i + 4] = [x, 0, 1, 1]
-        elif result[i + 1 : i + 4] == [2, 1, 0]:
-            result[i : i + 4] = [x, 1, 0, 1]
-        if i == 0:
-            # 2
-            if result[i : i + 3] == [2, 0, 0]:
-                result[i : i + 3] = [0, 1, 0]
-            # 3
-            elif result[i : i + 3] == [3, 0, 0]:
-                result[i : i + 3] = [0, 0, 1]
-            # 4
-            elif result[i : i + 3] == [0, 2, 0]:
-                result[i : i + 3] = [1, 0, 1]
-            elif result[i : i + 3] == [2, 1, 0]:
-                result[i : i + 3] = [1, 0, 1]
-            # 5
-            elif result[i : i + 3] == [2, 0, 1]:
-                result[i : i + 3] = [0, 1, 1]
-            elif result[i : i + 3] == [3, 1, 0]:
-                result[i : i + 3] = [0, 1, 1]
-            elif result[i : i + 3] == [1, 2, 0]:
-                result[i : i + 3] = [0, 1, 1]
-            # 6
-            elif result[i : i + 3] == [0, 3, 0]:
-                result[i : i + 3] = [1, 1, 1]
-            elif result[i : i + 3] == [3, 0, 1]:
-                result[i : i + 3] = [1, 1, 1]
-            elif result[i : i + 3] == [2, 2, 0]:
-                result[i : i + 3] = [1, 1, 1]
-            elif result[i : i + 3] == [0, 0, 2]:
-                result[i : i + 3] = [1, 1, 1]
-    
-    # Second stage: Move from left to right, removing consecutive 1's
-    for i in range(len(result) - 2):
-        if result[i : i + 3] == [1, 1, 0]:
-            result[i : i + 3] = [0, 0, 1]
-    
-    # Third stage: Move from right to left, removing consecutive 1's
-    for i in range(len(result) - 3, -1, -1):
-        if result[i : i + 3] == [1, 1, 0]:
-            result[i : i + 3] = [0, 0, 1]
-    
-    while len(result) > 1 and result[-1] == 0:
-        result.pop()
-    
-    print(len(result), *result)
+N = max(N1, N2) + 3
+result = [i + j for (i, j) in zip_longest(A1, A2, fillvalue=0)] + [0, 0, 0]
+
+# First stage: Move from right to left, removing 2's
+for i in range(len(result) - 3, -1, -1):
+    x = result[i]
+    if result[i + 1 : i + 4] == [0, 2, 0]:
+        result[i : i + 4] = [x + 1, 0, 0, 1]
+    elif result[i + 1 : i + 4] == [0, 3, 0]:
+        result[i : i + 4] = [x + 1, 0, 1, 1]
+    elif result[i + 1 : i + 4] == [1, 2, 0]:
+        result[i : i + 4] = [x, 0, 1, 1]
+    elif result[i + 1 : i + 4] == [2, 1, 0]:
+        result[i : i + 4] = [x, 1, 0, 1]
+
+last_three_sum = result[0] * 1 + result[1] * 2 + result[2] * 3
+if last_three_sum == 2:
+    result[:3] = [0, 1, 0]
+elif last_three_sum == 3:
+    result[:3] = [0, 0, 1]
+elif last_three_sum == 4:
+    result[:3] = [1, 0, 1]
+elif last_three_sum == 5:
+    result[:3] = [0, 1, 1]
+elif last_three_sum == 6:
+    result[:3] = [1, 1, 1]
+
+# Second stage: Move from left to right, removing consecutive 1's
+for i in range(len(result) - 2):
+    if result[i : i + 3] == [1, 1, 0]:
+        result[i : i + 3] = [0, 0, 1]
+
+# Third stage: Move from right to left, removing consecutive 1's
+for i in range(len(result) - 3, -1, -1):
+    if result[i : i + 3] == [1, 1, 0]:
+        result[i : i + 3] = [0, 0, 1]
+
+while len(result) > 1 and result[-1] == 0:
+    result.pop()
+
+print(len(result), *result)
