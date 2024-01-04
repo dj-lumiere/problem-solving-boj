24bcde3951bdf177326b5b6bd64fdbc172cec962
DJ_Lumiere <djminstar573@gmail.com>
2024-01-04 19:16:29 +0900

    [Gold III] Title: 사회망 서비스(SNS), Time: 5080 ms, Memory: 259516 KB -BaekjoonHub

--- 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
+++ 백준/Gold/2533. 사회망 서비스（SNS）/사회망 서비스（SNS）.py
@@ -1,39 +1,23 @@
 # 2533 사회망 서비스(SNS)
-from sys import stdin
+from sys import stdin, setrecursionlimit
+
+setrecursionlimit(1000000)
 
 
 def input():
     return stdin.readline().strip()
 
 
-def find_post_order(start):
-    stack = [(start, False)]
-    post_order = []
-    visited = [False for _ in range(N + 1)]
-    parent = [0 for _ in range(N + 1)]  # To keep track of the parent of each node
-
-    while stack:
-        node, current_node_visited = stack.pop()
-        if current_node_visited:
-            post_order.append(node)
+def dfs(current_node):
+    visited[current_node] = True
+    early[current_node] = 1
+    not_early[current_node] = 0
+    for next_node in graph[current_node]:
+        if visited[next_node]:
             continue
-        stack.append((node, True))
-        visited[node] = True
-        for neighbor in graph[node]:
-            if not visited[neighbor]:
-                stack.append((neighbor, False))
-    return post_order, parent
-
-
-def iterative_dfs(post_order):
-    for node in post_order:
-        early[node] = 1  # Count self as early adopter
-        not_early[node] = 0  # Initially, count as not an early adopter
-        for next_node in graph[node]:
-            if next_node == parent[node]:
-                continue
-            early[node] += min(early[next_node], not_early[next_node])
-            not_early[node] += early[next_node]
+        dfs(next_node)
+        early[current_node] += min(early[next_node], not_early[next_node])
+        not_early[current_node] += early[next_node]
 
 
 N = int(input())
@@ -45,9 +29,7 @@ for _ in range(N - 1):
 
 early = [0 for _ in range(N + 1)]
 not_early = [0 for _ in range(N + 1)]
-
+visited = [False for _ in range(N + 1)]
 start = 1
-post_order, parent = find_post_order(start)
-iterative_dfs(post_order)
-
+dfs(start)
 print(min(early[start], not_early[start]))
