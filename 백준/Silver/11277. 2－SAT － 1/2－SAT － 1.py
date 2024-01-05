53ef10f31709aca7840355a052a65e824fb8f27a
DJ_Lumiere <djminstar573@gmail.com>
2024-01-05 12:22:53 +0900

    [Silver I] Title: 2-SAT - 1, Time: 1696 ms, Memory: 111384 KB -BaekjoonHub

---
+++ 백준/Silver/11277. 2－SAT － 1/2－SAT － 1.py
@@ -0,0 +1,28 @@
+# 11277 2-SAT - 1
+
+from itertools import product
+
+N, M = map(int, input().split())
+
+clauses = []
+for _ in range(M):
+    i, j = map(int, input().split())
+    clauses.append((i, j))
+can_be_true = False
+for states in product((True, False), repeat=N):
+    result = True
+    for i, j in clauses:
+        sub_result = False
+        if i < 0:
+            sub_result |= not states[-i - 1]
+        else:
+            sub_result |= states[i - 1]
+        if j < 0:
+            sub_result |= not states[-j - 1]
+        else:
+            sub_result |= states[j - 1]
+        result &= sub_result
+    if result:
+        can_be_true = True
+        break
+print(int(can_be_true))
