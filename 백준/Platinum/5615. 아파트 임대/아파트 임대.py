09754068aba667bc585ad751f37ec06e2484cd51
DJ_Lumiere <djminstar573@gmail.com>
2024-01-15 01:19:17 +0900

    [Platinum I] Title: 아파트 임대, Time: 424 ms, Memory: 111660 KB -BaekjoonHub

--- 백준/Platinum/5615. 아파트 임대/아파트 임대.py
+++ 백준/Platinum/5615. 아파트 임대/아파트 임대.py
@@ -5,32 +5,41 @@
 
 # if n < 4,759,123,141, it is enough to test a = 2, 7, and 61 (from wikipedia)
 prime_list = [2, 7, 61]
+def powmod(a:int, b:int, m:int) -> int:
+    result:int = 1
+    next_b:int = b
+    while b > 0:
+        if b % 2:
+            result = (result*a) % m
+        b //= 2
+        a = a * a % m
+    return result
 def is_prime(n: int):
-    # prime_list의 원소들을 미리 소수로 처리
     if n in prime_list:
         return True
-    # 1과 짝수는 소수가 아님
     if n == 1 or n % 2 == 0:
         return False
-    # (n-1) = 2^s * d
-    s = 0
-    d = n - 1
+
+    s, d = 0, n - 1
     while d % 2 == 0:
-        d //= 2
         s += 1
-    y = 1
-    # 소수 리스트를 참조하여 소수 판별
-    for i in prime_list:
-        x = pow(i, d, n)
+        d //= 2
+
+    for a in [2, 7, 61]:
+        x = pow(a, d, n)
+
         if x == 1 or x == n - 1:
             continue
+
         for _ in range(s):
             y = pow(x, 2, n)
-            if y == 1 and x != 1 and x + 1 != n:
+            if y == 1 and x != 1 and x != n - 1:
                 return False
             x = y
-        if y != 1:
+
+        if x != 1:
             return False
+
     return True
 
 T = int(input())
