edf50e210286e00f105aafafb70aa6c61422ca3c
DJ_Lumiere <djminstar573@gmail.com>
2024-01-21 11:13:37 +0900

    [Bronze I] Title: Starman, Time: 144 ms, Memory: 110880 KB -BaekjoonHub

--- 백준/Bronze/12791. Starman/Starman.py
+++ 백준/Bronze/12791. Starman/Starman.py
@@ -1,34 +1,36 @@
 # 12791 Starman
 
-ALBUMS = {1967: ['DavidBowie'],
-          1969: ['SpaceOddity'],
-          1970: ['TheManWhoSoldTheWorld'],
-          1971: ['HunkyDory'],
-          1972: ['TheRiseAndFallOfZiggyStardustAndTheSpidersFromMars'],
-          1973: ['AladdinSane', 'PinUps'],
-          1974: ['DiamondDogs'],
-          1975: ['YoungAmericans'],
-          1976: ['StationToStation'],
-          1977: ['Low', 'Heroes'],
-          1979: ['Lodger'],
-          1980: ['ScaryMonstersAndSuperCreeps'],
-          1983: ['LetsDance'],
-          1984: ['Tonight'],
-          1987: ['NeverLetMeDown'],
-          1993: ['BlackTieWhiteNoise'],
-          1995: ['1.Outside'],
-          1997: ['Earthling'],
-          1999: ['Hours'],
-          2002: ['Heathen'],
-          2003: ['Reality'],
-          2013: ['TheNextDay'],
-          2016: ['BlackStar']}
+ALBUMS = {
+    1967: ["DavidBowie"],
+    1969: ["SpaceOddity"],
+    1970: ["TheManWhoSoldTheWorld"],
+    1971: ["HunkyDory"],
+    1972: ["TheRiseAndFallOfZiggyStardustAndTheSpidersFromMars"],
+    1973: ["AladdinSane", "PinUps"],
+    1974: ["DiamondDogs"],
+    1975: ["YoungAmericans"],
+    1976: ["StationToStation"],
+    1977: ["Low", "Heroes"],
+    1979: ["Lodger"],
+    1980: ["ScaryMonstersAndSuperCreeps"],
+    1983: ["LetsDance"],
+    1984: ["Tonight"],
+    1987: ["NeverLetMeDown"],
+    1993: ["BlackTieWhiteNoise"],
+    1995: ["1.Outside"],
+    1997: ["Earthling"],
+    1999: ["Hours"],
+    2002: ["Heathen"],
+    2003: ["Reality"],
+    2013: ["TheNextDay"],
+    2016: ["BlackStar"],
+}
 
 q = int(input())
 for _ in range(q):
     a, b = map(int, input().split())
     answer = []
-    for i in range(a, b+1):
+    for i in range(a, b + 1):
         if i in ALBUMS:
             answer.append((i, ALBUMS[i]))
     if not answer:
