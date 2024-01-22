63810d7af4e8f39a53ee9bece61269eea7e2f51e
DJ_Lumiere <djminstar573@gmail.com>
2024-01-22 13:44:10 +0900

    [Silver III] Title: 대한민국을 지키는 가장 긴 힘, Time: 140 ms, Memory: 110932 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.py
+++ 백준/Silver/31263. 대한민국을 지키는 가장 긴 힘/대한민국을 지키는 가장 긴 힘.py
@@ -1,25 +1,30 @@
-# A번 - 대한민국을 지키는 가장 긴 힘
-INF = 10**18
+# 31263 대한민국을 지키는 가장 긴 힘
 
-
-def answer(string):
-    cutting = [string[:1], string[:2], string[:3]]
-    if not any(cutting):
-        return 0
-    possible_cutting = []
-    possible_cutting_index = []
-    for i, cut in enumerate(cutting):
-        if cut[0] == "0":
-            continue
-        if not 1 <= int(cut) <= 641:
-            continue
-        possible_cutting.append(int(cut))
-        possible_cutting_index.append(i + 1)
-    if not possible_cutting:
-        return INF
-    return min(1 + answer(string[i:]) for i in possible_cutting_index)
-
-
-_ = int(input())
+INF = 10 ** 18
+N = int(input())
 string = input()
-print(answer(string))
+is_valid_substring = [[False for _ in range(N)] for _ in range(3)]
+for i in range(N):
+    if string[i] == "0":
+        continue
+    if 1 <= int(string[i: i + 1]) <= 641:
+        is_valid_substring[0][i] = True
+    if i + 2 > N:
+        continue
+    if 1 <= int(string[i: i + 2]) <= 641:
+        is_valid_substring[1][i] = True
+    if i + 3 > N:
+        continue
+    if 1 <= int(string[i: i + 3]) <= 641:
+        is_valid_substring[2][i] = True
+answer = [INF for _ in range(N)] + [0 for _ in range(3)]
+for i in range(N):
+    if is_valid_substring[0][i]:
+        answer[i] = min(answer[i], answer[i - 1] + 1)
+    if i >= 1 and is_valid_substring[1][i - 1]:
+        answer[i] = min(answer[i], answer[i - 2] + 1)
+    if i >= 2 and is_valid_substring[2][i - 2]:
+        answer[i] = min(answer[i], answer[i - 3] + 1)
+# print(*is_valid_substring, sep="\n")
+# print(answer)
+print(answer[N - 1])
