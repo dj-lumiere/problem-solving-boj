5fa254b3611e96e684de68e4efbf8db91ae11f8d
DJ_Lumiere <djminstar573@gmail.com>
2024-02-21 19:54:02 +0900

    [Silver II] Title: 걷다보니 신천역 삼 (Small), Time: 112 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/14650. 걷다보니 신천역 삼 （Small）/걷다보니 신천역 삼 （Small）.py
@@ -0,0 +1,10 @@
+# 14651 걷다보니 신천역 삼 (Large)
+
+N = int(input())
+answer = [[0 for _ in range(3)] for _ in range(N)]
+answer[0][1] = answer[0][2] = 1
+for i in range(1, N):
+    for j in range(3):
+        for k in range(3):
+            answer[i][(j * 10 + k) % 3] += answer[i - 1][j]
+print(answer[-1][0])
