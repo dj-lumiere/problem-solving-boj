4e7ccade56a8ded7b6f52aa6caab1575afe6c4a2
DJ_Lumiere <djminstar573@gmail.com>
2024-04-05 12:33:58 +0900

    [Bronze V] Title: 果物 (Fruit), Time: 116 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/31606. 果物 （Fruit）/果物 （Fruit）.py
@@ -0,0 +1,15 @@
+import os
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: os.write(1, "\n".join(x).encode())
+    eprint = lambda x: os.write(2, (str(x) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    for a in range(t):
+        x = int(input())
+        y = int(input())
+        answers[a] = f"{x + y + 3}"
+    print(answers)
