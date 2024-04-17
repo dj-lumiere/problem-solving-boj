bdee2e9c488f9295ffd9b2491fd134fa0a4836dc
DJ_Lumiere <djminstar573@gmail.com>
2024-04-18 01:05:34 +0900

    [Silver IV] Title: 피하자, Time: 324 ms, Memory: 199864 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/25379. 피하자/피하자.py
+++ 백준/Silver/25379. 피하자/피하자.py
@@ -15,13 +15,8 @@ with open(0, 'rb') as f:
         x = [int(input()) for _ in range(n)]
         even = sum(i % 2 == 0 for i in x)
         odd = n - even
-        # eprint((even, odd))
-        wrong_even = [v % 2 == 0 if i < even else v % 2 == 1 for i, v in enumerate(x)]
-        wrong_odd = [v % 2 == 1 if i < odd else v % 2 == 0 for i, v in enumerate(x)]
-        # eprint((wrong_even, wrong_odd))
-        answer_odd = sum(i for i, v in enumerate(wrong_odd) if not v) if any(i for i in wrong_odd) else 10 ** 18
-        answer_even = sum(i for i, v in enumerate(wrong_even) if not v) if any(i for i in wrong_even) else 10 ** 18
-        # eprint((answer_even, answer_odd))
+        answer_odd = sum(i for i, v in enumerate(x) if v % 2 == 1) - sum(range(odd))
+        answer_even = sum(i for i, v in enumerate(x) if v % 2 == 0) - sum(range(even))
         answer = min(answer_even, answer_odd)
         answers[h] = f"{answer}"
     print(answers)
