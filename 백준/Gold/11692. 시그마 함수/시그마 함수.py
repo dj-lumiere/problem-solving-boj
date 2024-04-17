ef1e9ba35c8e0c95aa6074ed56fffde03cf184f8
DJ_Lumiere <djminstar573@gmail.com>
2024-04-17 12:35:10 +0900

    [Gold III] Title: 시그마 함수, Time: 140 ms, Memory: 112004 KB -BaekjoonHub

---
+++ 백준/Gold/11692. 시그마 함수/시그마 함수.py
@@ -0,0 +1,18 @@
+import os
+from collections import Counter
+from itertools import product
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: os.write(1, "\n".join(x).encode())
+    eprint = lambda x: os.write(2, (str(x) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    for h in range(t):
+        n = int(input())
+        # x^2 or 2x^2 꼴
+        answer = n - (int(n ** .5) + int((n // 2) ** .5))
+        answers[h] = f"{answer}"
+    print(answers)
