be808a30d5854a8c9c19f9a3775197ff3b5e962b
DJ_Lumiere <djminstar573@gmail.com>
2024-04-19 14:19:28 +0900

    [Gold III] Title: 큰 나눗셈, Time: 868 ms, Memory: 126596 KB -BaekjoonHub

--- 백준/Gold/9363. 큰 나눗셈/큰 나눗셈.py
+++ 백준/Gold/9363. 큰 나눗셈/큰 나눗셈.py
@@ -1,35 +1,48 @@
+from collections import defaultdict
+
 # 9363 큰 나눗셈
-from sys import setrecursionlimit
-
-setrecursionlimit(110000)
 
 
-def gcd(x: int, y: int) -> int:
-    if x < y:
-        x, y = y, x
-    if x % y:
-        return gcd(x % y, y)
-    else:
-        return y
-
+finder_limit = 10 ** 3
+prime_list: list[bool] = [True for i in range(0, finder_limit + 1)]
+prime_list[0] = False
+prime_list[1] = False
+for x in range(1, int(finder_limit ** 0.5) + 1):
+    if prime_list[x]:
+        prime_list[x: finder_limit + 1: x] = [False] * (finder_limit // x)
+        prime_list[x] = True
+prime_list2 = [i for i, j in enumerate(prime_list) if j]
 
 T = int(input())
 
 for i in range(1, T + 1):
-    a, b = list(map(int, input().split(" ")))
+    A, B = list(map(int, input().split(" ")))
     a_list = list(map(int, input().split(" ")))
     b_list = list(map(int, input().split(" ")))
-    if a > b:
-        b_list += [1] * (a - b)
-        b = a
-    else:
-        a_list += [1] * (b - a)
-        a = b
-    numerator, denominator = 1, 1
-    for x, y in zip(a_list, b_list):
-        numerator *= x
-        denominator *= y
-    g = gcd(numerator, denominator)
-    numerator //= g
-    denominator //= g
+    factorization = defaultdict(int)
+    for x in prime_list2:
+        factorization[x] = 0
+    for a in a_list:
+        next_factor = a
+        for j in prime_list2:
+            while next_factor % j == 0:
+                factorization[j] += 1
+                next_factor = next_factor // j
+        if next_factor > 1000:
+            factorization[next_factor] += 1
+    for b in b_list:
+        next_factor = b
+        for j in prime_list2:
+            while next_factor % j == 0:
+                factorization[j] -= 1
+                next_factor = next_factor // j
+        if next_factor > 1000:
+            factorization[next_factor] -= 1
+    numerator = 1
+    denominator = 1
+    for p, q in factorization.items():
+        if q < 0:
+            denominator *= p ** (-1 * q)
+        elif q > 0:
+            numerator *= p ** q
     print(f"Case #{i}: {numerator} / {denominator}")
