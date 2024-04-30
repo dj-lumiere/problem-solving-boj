29fa51e17a2ba7d555cb3b1c84458abb6d769242
DJ_Lumiere <djminstar573@gmail.com>
2024-04-30 12:50:03 +0900

    [Bronze II] Title: 배수들의 합, Time: 348 ms, Memory: 137504 KB -BaekjoonHub

---
+++ 백준/Bronze/17173. 배수들의 합/배수들의 합.py
@@ -0,0 +1,36 @@
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    MOD = 10 ** 9 + 7
+    for h in range(t):
+        n = int(input())
+        m = int(input())
+        k = [int(input()) for _ in range(m)]
+        contained = [False for i in range(n + 1)]
+        for i, v in enumerate(contained):
+            for j in k:
+                if i % j == 0:
+                    contained[i] = True
+                    break
+        answer = sum(i * v for i, v in enumerate(contained))
+        answers[h] = f"{answer}"
+    print(answers)
