0d9c1bda54f1e0a9dcf030bad7d68cc908f5b85d
DJ_Lumiere <djminstar573@gmail.com>
2024-05-03 13:22:09 +0900

    [Bronze I] Title: Lie Detector, Time: 360 ms, Memory: 144808 KB -BaekjoonHub

---
+++ 백준/Bronze/16581. Lie Detector/Lie Detector.py
@@ -0,0 +1,36 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    MOD = 10 ** 9 + 7
+    for h in range(t):
+        n = int(input())
+        result = [input().decode() == "TRUTH" for _ in range(n)]
+        answer = result[-1]
+        for i, v in enumerate(reversed(result)):
+            if i == 0:
+                continue
+            answer = v if answer else not v
+        answers[h] = "TRUTH" if answer else "LIE"
+    print(answers)
