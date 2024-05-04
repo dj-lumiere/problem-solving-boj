9b1feab420c0e9231d56e5e7e765ca4a85202d4c
DJ_Lumiere <djminstar573@gmail.com>
2024-05-04 21:21:26 +0900

    [Bronze II] Title: 직사각형을 만드는 방법, Time: 336 ms, Memory: 137380 KB -BaekjoonHub

---
+++ 백준/Bronze/8320. 직사각형을 만드는 방법/직사각형을 만드는 방법.py
@@ -0,0 +1,35 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    MOD = 11092019
+    for h in range(t):
+        n = int(input())
+        answer = 0
+        for i in range(1, n + 1):
+            for j in range(1, int(i ** .5) + 1):
+                if i % j == 0:
+                    answer += 1
+        answers[h] = f"{answer}"
+    print(answers)
