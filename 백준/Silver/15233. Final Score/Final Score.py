e62c8f3db8a0ff6a23dab455a3b9ad46ec144bc6
DJ_Lumiere <djminstar573@gmail.com>
2024-05-05 23:12:02 +0900

    [Silver IV] Title: Final Score, Time: 308 ms, Memory: 134112 KB -BaekjoonHub

---
+++ 백준/Silver/15233. Final Score/Final Score.py
@@ -0,0 +1,43 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    MOD = 11092019
+    for h in range(t):
+        a, b, g = int(input()), int(input()), int(input())
+        team_name = {input().decode(): "A" for _ in range(a)}
+        for _ in range(b):
+            team_name[input().decode()] = "B"
+        goal_name = [input().decode() for _ in range(g)]
+        a_score = sum(team_name[name] == "A" for name in goal_name)
+        b_score = g - a_score
+        answer = ""
+        if a_score > b_score:
+            answer = "A"
+        elif a_score < b_score:
+            answer = "B"
+        else:
+            answer = "TIE"
+        answers[h] = f"{answer}"
+    print(answers)
