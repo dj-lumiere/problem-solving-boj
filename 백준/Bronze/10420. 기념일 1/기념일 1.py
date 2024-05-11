28f755b332f3eef4a9f09e4365004871bc5fb76d
DJ_Lumiere <djminstar573@gmail.com>
2024-05-11 18:10:39 +0900

    [Bronze II] Title: 기념일 1, Time: 328 ms, Memory: 133992 KB -BaekjoonHub

---
+++ 백준/Bronze/10420. 기념일 1/기념일 1.py
@@ -0,0 +1,33 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    MOD = 11092019
+    for hh in range(t):
+        start = datetime(year=2014, month=4, day=2)
+        n = int(input())
+        start += timedelta(days=n - 1)
+        answer = f"{start.year:0>4}-{start.month:0>2}-{start.day:0>2}"
+        answers[hh] = f"{answer}"
+    print(answers)
