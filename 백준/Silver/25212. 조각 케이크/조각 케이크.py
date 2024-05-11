4f21aa7e626b47fab717dac7ed334117eb8da4e0
DJ_Lumiere <djminstar573@gmail.com>
2024-05-11 17:48:49 +0900

    [Silver II] Title: 조각 케이크, Time: 360 ms, Memory: 137508 KB -BaekjoonHub

---
+++ 백준/Silver/25212. 조각 케이크/조각 케이크.py
@@ -0,0 +1,37 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    MOD = 11092019
+    for hh in range(t):
+        n = int(input())
+        x = [1 / int(input()) for _ in range(n)]
+        answer = 0
+        for mask in range(1 << n):
+            containing = [mask & (1 << i) != 0 for i in range(n)]
+            cake_sum = sum(i * j for i, j in zip(x, containing))
+            if 0.99 <= cake_sum <= 1.01:
+                answer += 1
+        answers[hh] = f"{answer}"
+    print(answers)
