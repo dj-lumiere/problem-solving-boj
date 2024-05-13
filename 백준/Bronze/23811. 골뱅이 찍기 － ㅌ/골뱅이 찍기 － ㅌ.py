d3fee7f3735b975d6c7b8dc3835759fa3adb69b0
DJ_Lumiere <djminstar573@gmail.com>
2024-05-13 22:35:47 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅌ, Time: 332 ms, Memory: 136096 KB -BaekjoonHub

---
+++ 백준/Bronze/23811. 골뱅이 찍기 － ㅌ/골뱅이 찍기 － ㅌ.py
@@ -0,0 +1,34 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    MOD = 998244353
+    for hh in range(t):
+        n = int(input())
+        grid = [["" for _ in range(n*5)] for _ in range(n*5)]
+        for x1, y1 in [(i, 0) for i in range(5)]+[(i, 2) for i in range(5)]+[(i, 4) for i in range(5)]+[(0,1),(0,3)]:
+            for x2, y2 in product(range(n), repeat=2):
+                grid[y1*n+y2][x1*n+x2] = "@"
+        answers[hh] = "\n".join("".join(x) for x in grid)
+    print(answers)
