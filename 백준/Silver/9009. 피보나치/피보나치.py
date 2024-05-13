e5c1865ce686c2799c3ca1a74aa6c7b7c7078764
DJ_Lumiere <djminstar573@gmail.com>
2024-05-13 23:59:39 +0900

    [Silver I] Title: 피보나치, Time: 364 ms, Memory: 137684 KB -BaekjoonHub

---
+++ 백준/Silver/9009. 피보나치/피보나치.py
@@ -0,0 +1,42 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = int(input())
+    answers = ["" for _ in range(t)]
+    MOD = 998244353
+    fibonacci = [1, 2]
+    for _ in range(300):
+        if fibonacci[-1] >= 10 ** 9 + 1:
+            fibonacci.pop()
+            break
+        fibonacci.append(sum(fibonacci[-2:]))
+    for hh in range(t):
+        n = int(input())
+        answer = []
+        for i, v in enumerate(reversed(fibonacci)):
+            if n >= v:
+                n -= v
+                answer.append(v)
+        answer.reverse()
+        answers[hh] = " ".join(map(str, answer))
+    print(answers)
