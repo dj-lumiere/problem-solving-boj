2f52023285958951cf9b7a44260fbedeb43a5202
DJ_Lumiere <djminstar573@gmail.com>
2024-05-14 14:37:38 +0900

    [Bronze I] Title: Czy umiesz potęgować?, Time: 312 ms, Memory: 133992 KB -BaekjoonHub

---
+++ 백준/Bronze/8416. Czy umiesz potęgować？/Czy umiesz potęgować？.py
@@ -0,0 +1,30 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    for hh in range(t):
+        a, b = int(input()), int(input())
+        answer = pow(a, b, 10)
+        answers[hh] = f"{answer}"
+    print(answers)
