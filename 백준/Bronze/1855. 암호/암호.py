03fb096a296b5ee70fa070f23bbea38a6d222d2b
DJ_Lumiere <djminstar573@gmail.com>
2024-05-17 14:23:56 +0900

    [Bronze I] Title: 암호, Time: 360 ms, Memory: 134264 KB -BaekjoonHub

---
+++ 백준/Bronze/1855. 암호/암호.py
@@ -0,0 +1,38 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(0)]
+    for hh in range(t):
+        n = int(input())
+        code = input().decode()
+        answer = ""
+        grid = [["" for _ in range(n)] for _ in range(len(code)//n)]
+        for i in range(len(code)//n):
+            for j in range(n):
+                grid[i][j] = code[i*n+j] if i % 2 == 0 else code[i*n+n-1-j]
+        for j in range(n):
+            for i in range(len(code)//n):
+                answer += grid[i][j]
+        answers.append(f"{answer}")
+    print(answers)
