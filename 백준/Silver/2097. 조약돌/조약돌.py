fb6d7c00ba86f1074682af5353fa77bc2f29453d
DJ_Lumiere <djminstar573@gmail.com>
2024-05-24 14:40:54 +0900

    [Silver V] Title: 조약돌, Time: 328 ms, Memory: 134136 KB -BaekjoonHub

---
+++ 백준/Silver/2097. 조약돌/조약돌.py
@@ -0,0 +1,38 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    for hh in range(t):
+        l = int(input())
+        r = max(1, ceil(l ** .5) - 1)
+        c = max(1, ceil(l ** .5) - 1)
+        while True:
+            if (r + 1) * (c + 1) < l or c < 1:
+                c += 1
+                break
+            c -= 1
+        answer = (r + c) * 2
+        answers.append(f"{answer}")
+    print(answers)
