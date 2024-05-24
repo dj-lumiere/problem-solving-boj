68a17728f09c3048ec4d65207af3f3797fa4c597
DJ_Lumiere <djminstar573@gmail.com>
2024-05-24 13:59:14 +0900

    [Silver V] Title: 무한 문자열, Time: 320 ms, Memory: 134268 KB -BaekjoonHub

---
+++ 백준/Silver/12871. 무한 문자열/무한 문자열.py
@@ -0,0 +1,38 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    for hh in range(t):
+        n = input().decode()
+        m = input().decode()
+        answer = 0
+        if len(n) > len(m):
+            n, m = m, n
+        n *= lcm(len(n), len(m))//len(n)
+        m *= lcm(len(m), len(n))//len(m)
+        if n == m:
+            answer = 1
+        answers.append(f"{answer}")
+    print(answers)
