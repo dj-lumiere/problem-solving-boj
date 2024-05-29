3d22b6cf275485b35b5012312ed0053d289066c1
DJ_Lumiere <djminstar573@gmail.com>
2024-05-30 00:51:20 +0900

    [Bronze IV] Title: Файлы, Time: 336 ms, Memory: 142716 KB -BaekjoonHub

---
+++ 백준/Bronze/29546. Файлы/Файлы.py
@@ -0,0 +1,42 @@
+from bisect import bisect_left
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    beat = [*range(1, 16)] + [*range(14, 1, -1)]
+    for hh in range(t):
+        n = int(input())
+        filenames = [""]
+        for _ in range(n):
+            filename = input().decode()
+            filenames.append(filename)
+        query_result = []
+        m = int(input())
+        for _ in range(m):
+            l, r = int(input()), int(input())
+            query_result.extend(filenames[l:r+1])
+        answer = "\n".join(query_result)
+        answers.append(f"{answer}")
+    print(answers)
