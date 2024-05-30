ab2daa220a5e84f6810736a2dade55fae97a2908
DJ_Lumiere <djminstar573@gmail.com>
2024-05-31 01:07:59 +0900

    [Bronze III] Title: SAHUR & IMSA’, Time: 320 ms, Memory: 137268 KB -BaekjoonHub

---
+++ 백준/Bronze/9698. SAHUR ＆ IMSA’/SAHUR ＆ IMSA’.py
@@ -0,0 +1,39 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = int(input())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    for hh in range(t):
+        h, m = int(input()), int(input())
+        m -= 45
+        if m < 0:
+            h -= 1
+            m += 60
+        if h < 0:
+            h += 24
+        answer = f"Case #{hh + 1}: {h} {m}"
+        answers.append(f"{answer}")
+    print(answers)
