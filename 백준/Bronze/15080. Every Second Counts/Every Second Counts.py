461ecb4eeb2765098bd2872e110321fdbce23bc8
DJ_Lumiere <djminstar573@gmail.com>
2024-05-31 00:21:44 +0900

    [Bronze IV] Title: Every Second Counts, Time: 344 ms, Memory: 137276 KB -BaekjoonHub

---
+++ 백준/Bronze/15080. Every Second Counts/Every Second Counts.py
@@ -0,0 +1,36 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    for hh in range(t):
+        h1, _, m1, _, s1 = int(input()), input(), int(input()), input(), int(input())
+        h2, _, m2, _, s2 = int(input()), input(), int(input()), input(), int(input())
+        time1 = h1 * 3600 + m1 * 60 + s1
+        time2 = h2 * 3600 + m2 * 60 + s2
+        answer = (time2 - time1) % 86400
+        answers.append(f"{answer}")
+    print(answers)
