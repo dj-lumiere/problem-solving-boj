1c30385a336377566425a4e4aa3496c0c0313d9f
DJ_Lumiere <djminstar573@gmail.com>
2024-06-02 13:43:48 +0900

    [Bronze IV] Title: Simple Sum, Time: 344 ms, Memory: 136764 KB -BaekjoonHub

---
+++ 백준/Bronze/26531. Simple Sum/Simple Sum.py
@@ -0,0 +1,31 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(0)]
+    for hh in range(t):
+        a, _, b, _, c = int(input()), input(), int(input()), input(), int(input())
+        answer = "YES" if a + b == c else "NO"
+        answers.append(f"{answer}")
+    print(answers)
