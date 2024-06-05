8c1230f849c7b309a9b5d3460962b11242c16512
DJ_Lumiere <djminstar573@gmail.com>
2024-06-05 23:19:52 +0900

    [Bronze III] Title: Absolutely Flat, Time: 380 ms, Memory: 137256 KB -BaekjoonHub

---
+++ 백준/Bronze/26863. Absolutely Flat/Absolutely Flat.py
@@ -0,0 +1,42 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        a = [int(input()) for _ in range(4)]
+        b = int(input())
+        answer = 0
+        for i in range(4):
+            a2 = a[:]
+            a2[i] += b
+            if all(i == a2[0] for i in a2):
+                answer = 1
+                break
+        if all(i == a[0] for i in a):
+            answer = 1
+        answers.append(f"{answer}")
+    print(answers)
