74a5cf903b21e104bbff1131d5edff754f65f1b2
DJ_Lumiere <djminstar573@gmail.com>
2024-06-08 21:58:11 +0900

    [Bronze III] Title: ДИНИ, Time: 336 ms, Memory: 137272 KB -BaekjoonHub

---
+++ 백준/Bronze/24312. ДИНИ/ДИНИ.py
@@ -0,0 +1,39 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        weights = [int(input()) for _ in range(4)]
+        min_difference = INF
+        for mask in range(1, 1 << 4):
+            left_weight = sum(v * (mask & (1 << i) != 0) for i, v in enumerate(weights))
+            right_weight = sum(weights) - left_weight
+            if min_difference > abs(right_weight - left_weight):
+                min_difference = abs(right_weight - left_weight)
+        answer = min_difference
+        answers.append(f"{answer}")
+    print(answers)
