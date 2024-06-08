ac627d1ecaa1cf00647e9c25b348bf93aeb34805
DJ_Lumiere <djminstar573@gmail.com>
2024-06-08 21:48:35 +0900

    [Silver II] Title: 예산, Time: 352 ms, Memory: 137500 KB -BaekjoonHub

---
+++ 백준/Silver/2512. 예산/예산.py
@@ -0,0 +1,44 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        n = int(input())
+        a = [int(input()) for _ in range(n)]
+        x = int(input())
+        start = 0
+        end = max(a) + 1
+        while start + 1 < end:
+            mid = (start + end) // 2
+            total_budget = [min(mid, i) for i in a]
+            if sum(total_budget) > x:
+                end = mid
+            else:
+                start = mid
+        answer = start
+        answers.append(f"{answer}")
+    print(answers)
