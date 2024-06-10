3ea3a90547f564243377e720d26a7269de35195d
DJ_Lumiere <djminstar573@gmail.com>
2024-06-10 20:44:58 +0900

    [Bronze I] Title: Spreadsheets, Time: 332 ms, Memory: 137272 KB -BaekjoonHub

---
+++ 백준/Bronze/16265. Spreadsheets/Spreadsheets.py
@@ -0,0 +1,36 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split(b"\n"))
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = int(input())
+    for hh in range(t):
+        c = int(input())
+        digit = floor(log(c * 25 + 25, 26))
+        column_number_for_alphabet = c - (26 ** digit - 26) // 25 - 1
+        column_number_list = [column_number_for_alphabet // (26 ** i) % 26 for i in range(digit)]
+        answer = "".join(map(lambda x: chr(x + ord("A")), reversed(column_number_list)))
+        answers.append(f"{answer}")
+    print(answers)
