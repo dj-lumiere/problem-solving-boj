b9311fe465adaa6af2b0454cdd8d96bc219b818e
DJ_Lumiere <djminstar573@gmail.com>
2024-06-18 14:59:10 +0900

    [Silver IV] Title: 수열, Time: 300 ms, Memory: 148612 KB -BaekjoonHub

---
+++ 백준/Silver/2491. 수열/수열.py
@@ -0,0 +1,43 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        n = int(input())
+        a = [int(input()) for _ in range(n)]
+        increasing_combo = [1 for _ in range(n)]
+        decreasing_combo = [1 for _ in range(n)]
+        for i, v in enumerate(a):
+            if i == 0:
+                continue
+            if a[i-1]<=v:
+                increasing_combo[i] = increasing_combo[i-1]+1
+            if a[i-1]>=v:
+                decreasing_combo[i] = decreasing_combo[i-1]+1
+        answer = max(increasing_combo+decreasing_combo)
+        answers.append(f"{answer}")
+    print(answers)
