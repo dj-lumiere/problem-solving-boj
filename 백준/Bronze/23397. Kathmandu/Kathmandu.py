c6811f4c65614de48990c8b323ab08120a166566
DJ_Lumiere <djminstar573@gmail.com>
2024-06-19 16:25:52 +0900

    [Bronze III] Title: Kathmandu, Time: 292 ms, Memory: 137272 KB -BaekjoonHub

---
+++ 백준/Bronze/23397. Kathmandu/Kathmandu.py
@@ -0,0 +1,34 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        t, d, m = int(input()), int(input()), int(input())
+        a = [0] + [int(input()) for _ in range(m)] + [d]
+        answer = "N" if all(j - i < t for i, j in zip(a, a[1:])) else "Y"
+        answers.append(f"{answer}")
+    print(answers)
