6f5eeda8900f6a4faa99c8d8d670ed06de4ede00
DJ_Lumiere <djminstar573@gmail.com>
2024-06-21 20:51:20 +0900

    [Silver IV] Title: 서로소 평균, Time: 424 ms, Memory: 207424 KB -BaekjoonHub

---
+++ 백준/Silver/21920. 서로소 평균/서로소 평균.py
@@ -0,0 +1,57 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+
+def invert_case(x: str):
+    result = ""
+    for v in x:
+        if not v.isalpha():
+            result += v
+        elif v.isupper():
+            result += v.lower()
+        elif v.islower():
+            result += v.upper()
+    return result
+
+
+def remove_number(x: str):
+    result = ""
+    for v in x:
+        if not v.isdigit():
+            result += v
+    return result
+
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        n = int(input())
+        a = [int(input()) for _ in range(n)]
+        x = int(input())
+        x_coprime = [v for v in a if gcd(v, x) == 1]
+        answer = sum(x_coprime) / len(x_coprime)
+        answers.append(f"{answer}")
+    print(answers)
