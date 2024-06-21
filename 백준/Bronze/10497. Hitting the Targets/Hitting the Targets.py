e5a2582246ffbbe727eb0e5dafaa93fe1d0ef4c7
DJ_Lumiere <djminstar573@gmail.com>
2024-06-21 13:48:26 +0900

    [Bronze I] Title: Hitting the Targets, Time: 336 ms, Memory: 137540 KB -BaekjoonHub

---
+++ 백준/Bronze/10497. Hitting the Targets/Hitting the Targets.py
@@ -0,0 +1,66 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+
+def invert_case(x: str):
+    result = ""
+    for v in x:
+        if not v.isalpha():
+            result += v
+        elif v.isupper():
+            result += v.lower()
+        elif v.islower():
+            result += v.upper()
+    return result
+
+
+def remove_number(x: str):
+    result = ""
+    for v in x:
+        if not v.isdigit():
+            result += v
+    return result
+
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        targets = []
+        n = int(input())
+        for i in range(n):
+            target_type = input().decode()
+            if target_type == "rectangle":
+                x1, y1, x2, y2 = (int(input()) for _ in range(4))
+                targets.append(lambda x, y, x1=x1, y1=y1, x2=x2, y2=y2: (x1 <= x <= x2) and (y1 <= y <= y2))
+            if target_type == "circle":
+                x3, y3, r = (int(input()) for _ in range(3))
+                targets.append(lambda x, y, x3=x3, y3=y3, r=r: (x3 - x) ** 2 + (y3 - y) ** 2 <= r ** 2)
+        m = int(input())
+        for _ in range(m):
+            x, y = int(input()), int(input())
+            answer = sum(target(x, y) for target in targets)
+            answers.append(f"{answer}")
+    print(answers)
