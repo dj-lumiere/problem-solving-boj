65a024b8af89aae3ffd529b4ddfdb861af1f3bd1
DJ_Lumiere <djminstar573@gmail.com>
2024-06-21 13:53:51 +0900

    [Silver V] Title: BABBA, Time: 296 ms, Memory: 137256 KB -BaekjoonHub

---
+++ 백준/Silver/9625. BABBA/BABBA.py
@@ -0,0 +1,58 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+
+def invert_case(x: str):
+    result = ""
+    for v in x:
+        if not v.isalpha():
+            result += v
+        elif v.isupper():
+            result += v.lower()
+        elif v.islower():
+            result += v.upper()
+    return result
+
+
+def remove_number(x: str):
+    result = ""
+    for v in x:
+        if not v.isdigit():
+            result += v
+    return result
+
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        n = int(input())
+        letter_count = [(1, 0)]
+        for _ in range(n):
+            a, b = letter_count[-1]
+            letter_count.append((b, a + b))
+        answer = f"{letter_count[-1][0]} {letter_count[-1][1]}"
+        answers.append(answer)
+    print(answers)
