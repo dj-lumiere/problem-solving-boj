44e3a7626adda3703ed88dc179a0e474a945f196
DJ_Lumiere <djminstar573@gmail.com>
2024-06-25 14:44:31 +0900

    [Silver IV] Title: Grave, Time: 452 ms, Memory: 134772 KB -BaekjoonHub

---
+++ 백준/Silver/10378. Grave/Grave.py
@@ -0,0 +1,45 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).strip().encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        x1, y1, x2, y2 = (int(input()) for _ in range(4))
+        x3, y3, x4, y4 = (int(input()) for _ in range(4))
+        w, h = (int(input()) for _ in range(2))
+        answer = "No"
+        if x3 - x1 >= w and y2 - y1 >= h:
+            answer = "Yes"
+        elif x2 - x4 >= w and y2 - y1 >= h:
+            answer = "Yes"
+        elif y3 - y1 >= h and x2 - x1 >= w:
+            answer = "Yes"
+        elif y2 - y4 >= h and x2 - x1 >= w:
+            answer = "Yes"
+        answers.append(f"{answer}")
+    print(answers)
