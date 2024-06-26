8f568b379d734e56520a712f11f2de39e5f7f169
DJ_Lumiere <djminstar573@gmail.com>
2024-06-26 21:54:02 +0900

    [Platinum III] Title: Suffix Array, Time: 576 ms, Memory: 212476 KB -BaekjoonHub

---
+++ 백준/Platinum/9248. Suffix Array/Suffix Array.py
@@ -0,0 +1,113 @@
+"""from https://github.com/cheran-senthil/PyRival/blob/master/pyrival/strings/suffix_array.py"""
+
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+MAX_STEP = ceil(log2(10 ** 5))
+
+def SAIS(A):
+    n = len(A)
+    buckets = [0] * (max(A) + 2)
+    for a in A:
+        buckets[a + 1] += 1
+    for b in range(1, len(buckets)):
+        buckets[b] += buckets[b - 1]
+    isL = [1] * n
+    for i in reversed(range(n - 1)):
+        isL[i] = +(A[i] > A[i + 1]) if A[i] != A[i + 1] else isL[i + 1]
+
+    def induced_sort(LMS):
+        SA = [-1] * (n)
+        SA.append(n)
+        endpoint = buckets[1:]
+        for j in reversed(LMS):
+            endpoint[A[j]] -= 1
+            SA[endpoint[A[j]]] = j
+        startpoint = buckets[:-1]
+        for i in range(-1, n):
+            j = SA[i] - 1
+            if j >= 0 and isL[j]:
+                SA[startpoint[A[j]]] = j
+                startpoint[A[j]] += 1
+        SA.pop()
+        endpoint = buckets[1:]
+        for i in reversed(range(n)):
+            j = SA[i] - 1
+            if j >= 0 and not isL[j]:
+                endpoint[A[j]] -= 1
+                SA[endpoint[A[j]]] = j
+        return SA
+
+    isLMS = [+(i and isL[i - 1] and not isL[i]) for i in range(n)]
+    isLMS.append(1)
+    LMS = [i for i in range(n) if isLMS[i]]
+    if len(LMS) > 1:
+        SA = induced_sort(LMS)
+        LMS2 = [i for i in SA if isLMS[i]]
+        prev = -1
+        j = 0
+        for i in LMS2:
+            i1 = prev
+            i2 = i
+            while prev >= 0 and A[i1] == A[i2]:
+                i1 += 1
+                i2 += 1
+                if isLMS[i1] or isLMS[i2]:
+                    j -= isLMS[i1] and isLMS[i2]
+                    break
+            j += 1
+            prev = i
+            SA[i] = j
+        LMS = [LMS[i] for i in SAIS([SA[i] for i in LMS])]
+    return induced_sort(LMS)
+
+
+def KASAI(A, SA):
+    n = len(A)
+    rank = [0] * n
+    for i in range(n):
+        rank[SA[i]] = i
+    LCP = [0] * (n - 1)
+    k = 0
+    for i in range(n):
+        SAind = rank[i]
+        if SAind == n - 1:
+            continue
+        j = SA[SAind + 1]
+        while i + k < n and A[i + k] == A[j + k]:
+            k += 1
+        LCP[SAind] = k
+        k -= k > 0
+    return LCP
+
+
+# with open(0, 'rb') as f:
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda *args, sep="\n", end="\n": write(1, (sep.join(map(str, args)) + end).encode())
+    eprint = lambda *args, sep="\n", end="\n": write(2, (sep.join(map(str, args)) + end).encode())
+    t = 1
+    answers = ["" for _ in range(0)]
+    for hh in range(t):
+        x = input()
+        sa = SAIS([ord(c) for c in x])
+        sa2 = [i + 1 for i in sa]
+        answers.append(" ".join(map(str, sa2)))
+        answers.append("x " + " ".join(map(str, KASAI(x, sa))))
+    print(*answers)
