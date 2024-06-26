d2a4050ea39a54e1328eb4d19162e373ea00520e
DJ_Lumiere <djminstar573@gmail.com>
2024-06-26 22:45:44 +0900

    [Silver V] Title: 속도 위반, Time: 316 ms, Memory: 134776 KB -BaekjoonHub

---
+++ 백준/Silver/11971. 속도 위반/속도 위반.py
@@ -0,0 +1,44 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens) if tokens else exit(0)
+    print = lambda *args, sep="\n", end="": write(1, (sep.join(map(str, args)) + end).encode())
+    eprint = lambda *args, sep="\n", end="\n": write(2, (sep.join(map(str, args)) + end).encode())
+    answers = []
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        n = int(input())
+        m = int(input())
+        speed_limit = []
+        current_speed = []
+        for _ in range(n):
+            i, v = int(input()), int(input())
+            speed_limit.extend([v] * i)
+        for _ in range(m):
+            i, v = int(input()), int(input())
+            current_speed.extend([v] * i)
+        answer = max(max(c - s, 0) for c, s in zip(current_speed, speed_limit))
+        answers.append(f"{answer}")
+    print(*answers)
