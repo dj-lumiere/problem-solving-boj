57d5d45df94edcfbd95b7e2e77168682f8076437
DJ_Lumiere <djminstar573@gmail.com>
2024-07-30 19:39:28 +0900

    [Bronze II] Title: 最長昇順連続部分列 (Longest Ascending Contiguous Subsequence), Time: 388 ms, Memory: 137012 KB -BaekjoonHub

---
+++ 백준/Bronze/18416. 最長昇順連続部分列 （Longest Ascending Contiguous Subsequence）/最長昇順連続部分列 （Longest Ascending Contiguous Subsequence）.py
@@ -0,0 +1,37 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda *args, sep="\n", end="": write(1, (sep.join(map(str, args)) + end).encode())
+    eprint = lambda *args, sep="\n", end="\n": write(2, (sep.join(map(str, args)) + end).encode())
+    answers = []
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        n = int(input())
+        x = [0] + [int(input()) for _ in range(n)]
+        answer = max(r - l + 1 for l, r in permutations(range(1, n + 1), 2) if
+                     all(i <= j for i, j in zip(x[l:r + 1], x[l + 1:r + 1]))) if n != 1 else 1
+        answers.append(f"{answer}")
+    print(*answers)
