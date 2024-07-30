c9de5cbb4e556a607e1f38985d8d1920988a336a
DJ_Lumiere <djminstar573@gmail.com>
2024-07-30 20:29:26 +0900

    [Silver II] Title: 슬라임 잡고 레벨 업!, Time: 708 ms, Memory: 139636 KB -BaekjoonHub

---
+++ 백준/Silver/29717. 슬라임 잡고 레벨 업！/슬라임 잡고 레벨 업！.py
@@ -0,0 +1,32 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 30
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = int(input())
+    answers = ["" for _ in range(0)]
+    for hh in range(t):
+        n = int(input())
+        total_exp = n * (n + 1) // 2
+        # lv*(lv+1)=exp
+        answer = int(Decimal("-0.5")+Decimal(1+4*total_exp)**Decimal(".5")/2)+1
+        answers.append(f"{answer}")
+    print(answers)
