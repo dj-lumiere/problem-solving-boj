7e4d3cf1a1d4db1053cab4141589572892ced3e8
DJ_Lumiere <djminstar573@gmail.com>
2024-07-30 20:27:38 +0900

    [Bronze II] Title: 애너그램 만들기, Time: 328 ms, Memory: 134272 KB -BaekjoonHub

---
+++ 백준/Bronze/1919. 애너그램 만들기/애너그램 만들기.py
@@ -0,0 +1,41 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(0)]
+    for hh in range(t):
+        n = Counter(input().decode())
+        m = Counter(input().decode())
+        answer = 0
+        for k, v in n.items():
+            if k not in m:
+                answer += v
+            else:
+                answer += v - min(v, m[k])
+        for k, v in m.items():
+            if k not in n:
+                answer += v
+            else:
+                answer += v - min(v, n[k])
+        answers.append(f"{answer}")
+    print(answers)
