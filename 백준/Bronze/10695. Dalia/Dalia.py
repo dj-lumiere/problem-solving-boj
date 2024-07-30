e5bd082eb3ccc5846fdec848d1e122218afd98de
DJ_Lumiere <djminstar573@gmail.com>
2024-07-30 20:30:12 +0900

    [Bronze III] Title: Dalia, Time: 324 ms, Memory: 134112 KB -BaekjoonHub

---
+++ 백준/Bronze/10695. Dalia/Dalia.py
@@ -0,0 +1,35 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = int(input())
+    answers = ["" for _ in range(0)]
+    for hh in range(t):
+        n, r1, c1, r2, c2 = [int(input()) for _ in range(5)]
+        can_be_moved = lambda r, c: [(r - 1, c + 2), (r - 1, c - 2), (r + 1, c + 2), (r + 1, c - 2), (r - 2, c + 1),
+            (r - 2, c - 1), (r + 2, c + 1), (r + 2, c - 1)]
+        if (r2, c2) in can_be_moved(r1, c1):
+            answer = "YES"
+        else:
+            answer = "NO"
+        answers.append(f"Case {hh+1}: {answer}")
+    print(answers)
