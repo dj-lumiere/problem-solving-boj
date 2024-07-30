598bc29fbb60c7111228609fcb793a509cd18fad
DJ_Lumiere <djminstar573@gmail.com>
2024-07-30 20:30:51 +0900

    [Bronze IV] Title: 재수강, Time: 328 ms, Memory: 133992 KB -BaekjoonHub

---
+++ 백준/Bronze/31822. 재수강/재수강.py
@@ -0,0 +1,36 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split(b"\n"))
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(t)]
+    MOD = 998244353
+    for hh in range(t):
+        code = input().decode()
+        answer = 0
+        n = int(input())
+        for _ in range(n):
+            possible_code = input().decode()
+            if possible_code.startswith(code[:5]):
+                answer += 1
+        answers[hh] = f"{answer}"
+    print(answers)
