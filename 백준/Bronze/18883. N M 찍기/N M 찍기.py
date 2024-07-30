34d7aaa70da27a2663c511964a37848433aa77fd
DJ_Lumiere <djminstar573@gmail.com>
2024-07-30 20:26:10 +0900

    [Bronze III] Title: N M 찍기, Time: 392 ms, Memory: 167804 KB -BaekjoonHub

---
+++ 백준/Bronze/18883. N M 찍기/N M 찍기.py
@@ -0,0 +1,32 @@
+from bisect import bisect_left
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda x: write(1, "\n".join(x).encode())
+    eprint = lambda *args, **sep: write(2, (" ".join(map(str, args)) + "\n").encode())
+    t = 1
+    answers = ["" for _ in range(0)]
+    INF = 10 ** 18
+    for hh in range(t):
+        n, m = int(input()), int(input())
+        grid = [[r * m + c + 1 for c in range(m)] for r in range(n)]
+        answer = ("\n".join(" ".join(map(str, x)) for x in grid))+"\n"
+        answers.append(f"{answer}")
+    print(answers)
