51e6323aa7efdd6973a711619eea3e75cfdd23db
DJ_Lumiere <djminstar573@gmail.com>
2024-07-30 21:44:33 +0900

    [Bronze II] Title: Teenage Mutant, Time: 336 ms, Memory: 134776 KB -BaekjoonHub

---
+++ 백준/Bronze/16847. Teenage Mutant/Teenage Mutant.py
@@ -0,0 +1,40 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, cos, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda *args, sep="\n", end="": write(1, (sep.join(map(str, args)) + end).encode())
+    eprint = lambda *args, sep=" ", end="\n": write(2, (sep.join(map(str, args)) + end).encode())
+    answers = []
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = int(input())
+    for hh in range(t):
+        n, m = int(input()), int(input())
+        mine, *others = [input().decode() for _ in range(n + 1)]
+        all_different = 0
+        for i, *j in zip(mine, *others):
+            if all(i != k for k in j):
+                all_different += 1
+        answer = f"Data Set {hh + 1}:\n{all_different}/{m}\n"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
