be15eade7c975278632e3123efa6eea47621b104
DJ_Lumiere <djminstar573@gmail.com>
2024-08-04 17:49:21 +0900

    [Gold IV] Title: 카드 정렬하기, Time: 480 ms, Memory: 146204 KB -BaekjoonHub

---
+++ 백준/Gold/1715. 카드 정렬하기/카드 정렬하기.py
@@ -0,0 +1,42 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, cos, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda *args, sep="\n", end="": write(1, (sep.join(map(str, args)) + end).encode())
+    eprint = lambda *args, sep=" ", end="\n": write(2, (sep.join(map(str, args)) + end).encode())
+    answers = []
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        n = int(input())
+        a = [int(input()) for _ in range(n)]
+        heapify(a)
+        answer = 0
+        while len(a) > 1:
+            x = heappop(a)
+            y = heappop(a)
+            heappush(a, x + y)
+            answer += x + y
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
