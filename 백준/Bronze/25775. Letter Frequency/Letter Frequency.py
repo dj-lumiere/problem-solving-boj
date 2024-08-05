2a2dc2c237faf6a8d67e0ed5465bea0cfff4a928
DJ_Lumiere <djminstar573@gmail.com>
2024-08-05 20:04:46 +0900

    [Bronze I] Title: Letter Frequency, Time: 284 ms, Memory: 134780 KB -BaekjoonHub

---
+++ 백준/Bronze/25775. Letter Frequency/Letter Frequency.py
@@ -0,0 +1,48 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, cos, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+
+
+    def input():
+        try:
+            return next(tokens)
+        except StopIteration:
+            return ""
+
+
+    print = lambda *args, sep="\n", end="\n": write(1, (sep.join(map(str, args)) + end).encode())
+    eprint = lambda *args, sep=" ", end="": write(2, (sep.join(map(str, args)) + end).encode())
+    answers = []
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        n = int(input())
+        words = [input().decode() for _ in range(n)]
+        for i, letters in enumerate(zip_longest(*words, fillvalue=""), start=1):
+            letter_count = Counter(letters)
+            del letter_count[""]
+            most_frequent_items = sorted([item for item, count in letter_count.items() if
+                                          count == max(letter_count.values())])
+            answers.append(f"{i}: {' '.join(most_frequent_items)}")
+    print(*answers)
