bc0190527043a9e7511e16a5d88a947bb01cd795
DJ_Lumiere <djminstar573@gmail.com>
2024-08-05 21:18:40 +0900

    [Bronze III] Title: 문문문, Time: 248 ms, Memory: 134776 KB -BaekjoonHub

---
+++ 백준/Bronze/17210. 문문문/문문문.py
@@ -0,0 +1,51 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, cos, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+# with open(0, 'rb') as f:
+with open(0, 'rb') as f:
+    tokens = iter(f.read().split())
+
+
+    def input():
+        try:
+            return next(tokens)
+        except StopIteration:
+            return ""
+
+
+    print = lambda *args, sep="\n", end="\n": write(1, (sep.join(map(str, args)) + end).encode())
+    eprint = lambda *args, sep=" ", end="\n": write(2, (sep.join(map(str, args)) + end).encode())
+    answers = []
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    for hh in range(t):
+        n = int(input())
+        x = int(input())
+        if n >= 6:
+            answer = f"Love is open door"
+        else:
+            sequence = [x]
+            for _ in range(n - 1):
+                sequence.append(1 - sequence[-1])
+            sequence.pop(0)
+            answer = "\n".join(map(str, sequence))
+        answers.append(answer)
+    print(*answers)
