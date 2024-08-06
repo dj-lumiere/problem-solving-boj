6582f8dbbfd495e038a0c18e350751ea191da4dc
DJ_Lumiere <djminstar573@gmail.com>
2024-08-06 15:12:45 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 452 ms, Memory: 293364 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -1,6 +1,18 @@
-from sys import stdin, stdout
+from sys import stdout, stderr
 
-N = int(input())
-for i in range(N):
-    A, B = list(map(int, stdin.readline().rstrip().split(" ")))
-    stdout.writelines(f"{A+B}\n")
+with open(0, 'r') as f:
+    tokens = iter(f.read().split("\n"))
+    input = lambda: next(tokens)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    answers = []
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    for hh in range(t):
+        a, b = map(int, input().split())
+        answer = a + b
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
