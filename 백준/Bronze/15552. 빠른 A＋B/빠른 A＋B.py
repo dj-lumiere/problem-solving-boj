164279673dc3bb52faba1d443e8675afbc042455
DJ_Lumiere <djminstar573@gmail.com>
2024-08-06 15:13:16 +0900

    [Bronze IV] Title: 빠른 A+B, Time: 424 ms, Memory: 276228 KB -BaekjoonHub

--- 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
+++ 백준/Bronze/15552. 빠른 A＋B/빠른 A＋B.py
@@ -1,4 +1,5 @@
 from sys import stdout, stderr
+from __pypy__ import newlist_hint
 
 with open(0, 'r') as f:
     tokens = iter(f.read().split("\n"))
@@ -7,10 +8,10 @@ with open(0, 'r') as f:
     eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
     is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
     DELTA = [(-1, 0), (1, 0), (0, -1), (0, 1)]
-    answers = []
     INF = 10 ** 18
     MOD = 1_000_000_000
     t = int(input())
+    answers = newlist_hint(t)
     for hh in range(t):
         a, b = map(int, input().split())
         answer = a + b
