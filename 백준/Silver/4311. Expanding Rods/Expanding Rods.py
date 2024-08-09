615c69a0b5f759ecf05e087726186b8edaa14e75
DJ_Lumiere <djminstar573@gmail.com>
2024-08-09 09:55:36 +0900

    [Silver II] Title: Expanding Rods, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/4311. Expanding Rods/Expanding Rods.py
+++ 백준/Silver/4311. Expanding Rods/Expanding Rods.py
@@ -1,70 +1,5 @@
-from decimal import Decimal, getcontext
-from math import factorial
+from math import pi, sin, cos
 from sys import stdout, stderr
-
-getcontext().prec = 40
-
-pi = Decimal(
-    "3.1415926535897932384626433832795028841971693993751058209749445923078164062862089986280348253421170679"
-)
-error = Decimal("1e-20")
-
-
-def sin(x: Decimal) -> Decimal:
-    sign = 1
-    if not (0 <= x <= pi / 2):
-        x = x - (x // (2 * pi)) * 2 * pi
-        if x >= Decimal(1.5) * pi:
-            x = 2 * pi - x
-            sign = -1
-        elif x >= pi:
-            x -= pi
-            sign = -1
-        elif x >= Decimal(0.5) * pi:
-            x = pi - x
-    i = 0
-    value: Decimal = Decimal(0)
-    while True:
-        value_sub = Decimal(0)
-        if i % 4 == 1:
-            value_sub = x ** i / factorial(i)
-            value += value_sub
-        elif i % 4 == 3:
-            value_sub = Decimal(-1) * x ** i / factorial(i)
-            value += value_sub
-        if i % 2 == 1 and (-1 * error < value_sub < error or value_sub == Decimal("0")):
-            return sign * value
-        else:
-            i += 1
-
-
-def cos(x: Decimal) -> Decimal:
-    sign = 1
-    if not (0 <= x <= pi / 2):
-        x = x - (x // (2 * pi)) * 2 * pi
-        if x >= Decimal(1.5) * pi:
-            x = 2 * pi - x
-        elif x >= pi:
-            x -= pi
-            sign = -1
-        elif x >= Decimal(0.5) * pi:
-            x = pi - x
-            sign = -1
-    i = 0
-    value: Decimal = Decimal(0)
-    while True:
-        value_sub = Decimal(0)
-        if i % 4 == 0:
-            value_sub = x ** i / factorial(i)
-            value += value_sub
-        elif i % 4 == 2:
-            value_sub = Decimal(-1) * x ** i / factorial(i)
-            value += value_sub
-        if i % 2 == 0 and (-1 * error < value_sub < error or value_sub == Decimal("0")):
-            return sign * value
-        else:
-            i += 1
-
 
 with open(0, 'r') as f:
     tokens = iter(f.read().split())
@@ -79,7 +14,7 @@ with open(0, 'r') as f:
     t = INF
     answers = []
     for hh in range(t):
-        l, n, c = (Decimal(input()) for _ in range(3))
+        l, n, c = (float(input()) for _ in range(3))
         if l < 0 and n < 0 and c < 0:
             break
         l_prime = (1 + n * c) * l
@@ -91,7 +26,7 @@ with open(0, 'r') as f:
             theta = pi / 4
             while True:
                 next_theta = theta - f(theta) / fp(theta)
-                if abs(theta - next_theta) < error:
+                if abs(theta - next_theta) < 10 ** -7:
                     break
                 theta = next_theta
             r = l_prime / 2 / theta
