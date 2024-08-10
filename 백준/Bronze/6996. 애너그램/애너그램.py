43475a760f5bec001c392ef31ec86f63ae572040
DJ_Lumiere <djminstar573@gmail.com>
2024-08-10 22:57:09 +0900

    [Bronze I] Title: 애너그램, Time: 328 ms, Memory: 137256 KB -BaekjoonHub

---
+++ 백준/Bronze/6996. 애너그램/애너그램.py
@@ -0,0 +1,38 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from sys import stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        a = input()
+        b = input()
+        result = f"{a} & {b} are {'' if sorted(Counter(a).items()) == sorted(Counter(b).items()) else 'NOT '}anagrams."
+        answer = f"{result}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
