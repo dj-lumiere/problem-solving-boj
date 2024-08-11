0b9b1b33cc3b8a87ba840f5d1e43b70b551f3b3c
DJ_Lumiere <djminstar573@gmail.com>
2024-08-11 22:08:46 +0900

    [Bronze II] Title: 커맨드, Time: 268 ms, Memory: 138264 KB -BaekjoonHub

---
+++ 백준/Bronze/17838. 커맨드/커맨드.py
@@ -0,0 +1,58 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from sys import stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, pairwise
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+
+class MapIndex:
+    def __init__(self, function, iterable):
+        self.function = function
+        self.iterable = iterable
+
+    def __len__(self):
+        return len(self.iterable)
+
+    def __getitem__(self, key):
+        return self.function(self.iterable[key])
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        n = input()
+        characters = set(n)
+        answer = 1
+        if len(characters) != 2:
+            answer = 0
+        elif len(n) != 7:
+            answer = 0
+        else:
+            a, b = characters
+            if n not in (a + a + b + b + a + b + b, b + b + a + a + b + a + a):
+                answer = 0
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
