bd41dab7a347ba5a256f190844131efad125f835
DJ_Lumiere <djminstar573@gmail.com>
2024-08-12 21:43:01 +0900

    [Bronze III] Title: Sir Bedavere’s Bogus Division Solutions, Time: 276 ms, Memory: 137552 KB -BaekjoonHub

---
+++ 백준/Bronze/9161. Sir Bedavere’s Bogus Division Solutions/Sir Bedavere’s Bogus Division Solutions.py
@@ -0,0 +1,55 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit
+from os import write
+from sys import stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, pairwise, groupby, islice, starmap
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+
+class MapIndex:
+    def __init__(self, function, iterable):
+        self.function = function
+        self.iterable = iterable
+
+    def __len__(self):
+        return len(self.iterable)
+
+    def __getitem__(self, key):
+        return self.function(self.iterable[key])
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        result = []
+        for i, j in product(range(100, 1000), repeat=2):
+            if i % 111 == 0 and j % 111 == 0:
+                continue
+            ni, nj = i // 10, j % 100
+            if i * nj == j * ni and i % 10 == j // 100:
+                result.append(f"{i} / {j} = {ni} / {nj}")
+        answer = "\n".join(result)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
