026e317f4764d5895cfdae8d86ceddb25cf0ca49
Lumi <djminstar573@gmail.com>
2024-08-13 20:51:00 +0900

    [Bronze II] Title: Number Pairs, Time: 280 ms, Memory: 128516 KB -BaekjoonHub

---
+++ 백준/Bronze/15234. Number Pairs/Number Pairs.py
@@ -0,0 +1,40 @@
+from collections import deque
+from decimal import getcontext
+from itertools import combinations, product
+from math import floor, log10
+from sys import stdout, stderr
+
+getcontext().prec = 1000
+
+
+class MapIndex:
+    def __init__(self, function, iterable):
+        self.function = function
+        self.iterable = iterable
+
+    def __len__(self):
+        return len(self.iterable)
+
+    def __getitem__(self, key):
+        return self.function(self.iterable[key])
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        k = int(input())
+        numbers = [int(input()) for _ in range(n)]
+        answer = sum(i + j == k for i, j in combinations(numbers, 2))
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
