27a756c3e3241387a8fa5d0a1033f3d2ac3ed042
Lumi <djminstar573@gmail.com>
2024-08-16 15:32:32 +0900

    [Silver III] Title: 문자열 암호화, Time: 96 ms, Memory: 110404 KB -BaekjoonHub

---
+++ 백준/Silver/6616. 문자열 암호화/문자열 암호화.py
@@ -0,0 +1,31 @@
+from sys import stdout, stderr
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split("\n"))
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = INF
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        if n == 0:
+            break
+        s = "".join(input().split()).upper()
+        grid = ["" for _ in range(len(s))]
+        offset = 0
+        group = 0
+        for i, v in enumerate(s):
+            grid[offset] = v
+            offset += n
+            if offset >= len(s):
+                group += 1
+                offset = group
+        answer = "".join(grid)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
