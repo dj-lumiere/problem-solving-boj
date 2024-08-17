49c5c596b319cdf3b8ad5a92d150a3d489307378
Lumi <djminstar573@gmail.com>
2024-08-17 15:24:20 +0900

    [Silver IV] Title: 드럼, Time: 292 ms, Memory: 137504 KB -BaekjoonHub

---
+++ 백준/Silver/30890. 드럼/드럼.py
@@ -0,0 +1,45 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import stdout, stderr, setrecursionlimit
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+from array import array
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        x, y = int(input()), int(input())
+        x_timing = [Fraction(i, x) for i in range(1, x + 1)]
+        y_timing = [Fraction(i, y) for i in range(1, y + 1)]
+        timings = sorted(set(x_timing + y_timing))
+        answer = ""
+        for i, v in enumerate(timings):
+            if v in x_timing and v in y_timing:
+                answer += "3"
+            elif v in x_timing:
+                answer += "1"
+            elif v in y_timing:
+                answer += "2"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
