46b0cac6d0e2b3d9e6319b5ac21f9d0782fdd10f
Lumi <djminstar573@gmail.com>
2024-08-18 14:50:43 +0900

    [Silver III] Title: 가희와 파일 탐색기, Time: 1300 ms, Memory: 246164 KB -BaekjoonHub

---
+++ 백준/Silver/22232. 가희와 파일 탐색기/가희와 파일 탐색기.py
@@ -0,0 +1,40 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import stdout, stderr, setrecursionlimit
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+from array import array
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    valid_date = [0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for hh in range(t):
+        n = int(input())
+        m = int(input())
+        filenames = [input().split(".") for _ in range(n)]
+        recognizable_extension = set(input() for _ in range(m))
+        filenames.sort(key=lambda x: (x[0], x[1] not in recognizable_extension, x[1]))
+        answer = "\n".join(f"{x}.{y}" for x, y in filenames)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
