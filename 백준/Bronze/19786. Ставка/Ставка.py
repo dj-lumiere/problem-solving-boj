4eb1b9be2ec9dbf85fa311528bbd1cf34914af66
Lumi <djminstar573@gmail.com>
2024-08-18 14:58:34 +0900

    [Bronze III] Title: Ставка, Time: 356 ms, Memory: 142808 KB -BaekjoonHub

---
+++ 백준/Bronze/19786. Ставка/Ставка.py
@@ -0,0 +1,37 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import stdout, stderr, setrecursionlimit
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+from array import array
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    prices = lambda A, C, r, g, b: A * (r ** 2 + g ** 2 + b ** 2) + C * min(r, g, b)
+    for hh in range(t):
+        A, C, r, g, b = (int(input()) for _ in range(5))
+        price = {"RED": prices(A, C, r + 1, g, b), "GREEN": prices(A, C, r, g + 1, b), "BLUE": prices(A, C, r, g, b + 1)}
+        answer = [k for k, v in price.items() if v == max(price.values())][0]
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
