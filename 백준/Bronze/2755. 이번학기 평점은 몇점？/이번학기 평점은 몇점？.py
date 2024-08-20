1208260a339fedbafed51afe446201bfb919cb02
Lumi <djminstar573@gmail.com>
2024-08-20 21:09:00 +0900

    [Bronze I] Title: 이번학기 평점은 몇점?, Time: 260 ms, Memory: 137324 KB -BaekjoonHub

---
+++ 백준/Bronze/2755. 이번학기 평점은 몇점？/이번학기 평점은 몇점？.py
@@ -0,0 +1,46 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import stdout, stderr, setrecursionlimit
+from random import randint, shuffle
+from collections import deque, Counter
+from math import comb, lcm, log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, pairwise, permutations, combinations_with_replacement, product, zip_longest, chain
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+from array import array
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    grades = {"A+": 43, "A0": 40, "A-": 37, "B+": 33, "B0": 30, "B-": 27, "C+": 23, "C0": 20, "C-": 17, "D+": 13, "D0": 10, "D-": 7, "F": 0}
+    for hh in range(t):
+        n = int(input())
+        score_sum = 0
+        grade_sum = 0
+        for _ in range(n):
+            _, a, b = input(), int(input()), input()
+            grade_sum += a
+            score_sum += grades[b] * a
+        int_part, frac_part = divmod(score_sum * 100 // grade_sum, 1000)
+        frac_part, rounding_part = divmod(frac_part, 10)
+        if rounding_part >= 5:
+            frac_part += 1
+        answer = f"{int_part}.{frac_part:0>2}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
