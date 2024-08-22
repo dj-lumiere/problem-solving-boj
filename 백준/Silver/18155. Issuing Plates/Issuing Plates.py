8866c6a3b37c9bd5ecaa37018b1f603df845b544
Lumi <djminstar573@gmail.com>
2024-08-22 20:38:44 +0900

    [Silver V] Title: Issuing Plates, Time: 260 ms, Memory: 137556 KB -BaekjoonHub

---
+++ 백준/Silver/18155. Issuing Plates/Issuing Plates.py
@@ -0,0 +1,39 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    leets = {"0": "O", "1": "L", "2": "Z", "3": "E", "5": "S", "6": "B", "7": "T", "8": "B"}
+    for hh in range(t):
+        n, m = int(input()), int(input())
+        bad_words = [input() for _ in range(n)]
+        target = [input() for _ in range(m)]
+        for i, v in enumerate(target):
+            leet_convert = "".join([leets.get(v2, v2) for v2 in v])
+            answer = "INVALID" if any(j in leet_convert for j in bad_words) else "VALID"
+            answers.append(f"{answer}")
+    print(*answers, sep="\n")
