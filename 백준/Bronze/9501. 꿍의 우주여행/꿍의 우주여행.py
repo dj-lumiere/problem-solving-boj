7031651c6410277e4eb0b6d08e433ae3d62165ad
Lumi <djminstar573@gmail.com>
2024-08-22 20:40:57 +0900

    [Bronze III] Title: 꿍의 우주여행, Time: 284 ms, Memory: 137528 KB -BaekjoonHub

---
+++ 백준/Bronze/9501. 꿍의 우주여행/꿍의 우주여행.py
@@ -0,0 +1,36 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    leets = {"0": "O", "1": "L", "2": "Z", "3": "E", "5": "S", "6": "B", "7": "T", "8": "B"}
+    for hh in range(t):
+        n, d = int(input()), int(input())
+        spaceships = [(int(input()) for _ in range(3)) for _ in range(n)]
+        answer = sum(d * c <= f * v for v, f, c in spaceships)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
