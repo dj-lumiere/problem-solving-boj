4e510f8279303d6f8dfdbac9bf14b4296d68970a
Lumi <djminstar573@gmail.com>
2024-08-27 17:15:19 +0900

    [Gold IV] Title: N-Queen, Time: 4804 ms, Memory: 138052 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.py
+++ 백준/Gold/9663. N－Queen/N－Queen.py
@@ -1,4 +1,19 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
 from sys import setrecursionlimit, stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop, heapify
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
 
 with open(0, 'r') as f:
     tokens = iter(f.read().split())
@@ -43,4 +58,4 @@ with open(0, 'r') as f:
                     diag_rd_check ^= (1 << diag_rd)
                     row += 1
         answers.append(f"{answer}")
-    print(*answers, sep="\n")
+print(*answers, sep="\n")
