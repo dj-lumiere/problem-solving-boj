bf281ffee1c19ef5ccfab1e53174810b58b185b3
Lumi <djminstar573@gmail.com>
2024-08-27 19:56:20 +0900

    [Gold V] Title: 문자열 변환과 쿼리, Time: 840 ms, Memory: 218508 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Gold/27396. 문자열 변환과 쿼리/문자열 변환과 쿼리.py
@@ -0,0 +1,30 @@
+from string import ascii_lowercase, ascii_uppercase
+from sys import stdout, stderr
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        s = input()
+        n = int(input())
+        conversion = {i: i for i in ascii_lowercase + ascii_uppercase}
+        for _ in range(n):
+            q = int(input())
+            if q == 2:
+                answer = "".join(map(lambda x: conversion[x], s))
+                answers.append(f"{answer}")
+            elif q == 1:
+                a, b = input(), input()
+                for k, v in conversion.items():
+                    if v == a:
+                        conversion[k] = b
+    print(*answers, sep="\n")
