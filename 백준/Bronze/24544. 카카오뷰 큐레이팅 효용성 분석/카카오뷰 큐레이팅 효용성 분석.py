c9a9abaf4479efbbe12a8d8b1d6ce5bdd70faa8b
Lumi <djminstar573@gmail.com>
2024-08-29 19:25:43 +0900

    [Bronze II] Title: 카카오뷰 큐레이팅 효용성 분석, Time: 284 ms, Memory: 137308 KB -BaekjoonHub

---
+++ 백준/Bronze/24544. 카카오뷰 큐레이팅 효용성 분석/카카오뷰 큐레이팅 효용성 분석.py
@@ -0,0 +1,36 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop, heapify
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, "")
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        a = [int(input()) for _ in range(n)]
+        b = [int(input()) for _ in range(n)]
+        answer = f"{sum(a)}\n{sum(i for i, j in zip(a, b) if j == 0)}"
+        answers.append(answer)
+    print(*answers, sep="\n")
