568595f61bc5191bc37a4bfb7d8f5421e967a7c3
Lumi <djminstar573@gmail.com>
2024-09-01 11:54:29 +0900

    [Silver V] Title: 2021은 무엇이 특별할까?, Time: 260 ms, Memory: 137372 KB -BaekjoonHub

---
+++ 백준/Silver/24039. 2021은 무엇이 특별할까？/2021은 무엇이 특별할까？.py
@@ -0,0 +1,46 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop, heapify
+from itertools import combinations, pairwise, permutations, combinations_with_replacement, product, zip_longest, chain, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+
+def extract_primes_from_sieve(limit: int) -> list[int]:
+    is_prime = [False, False] + [True for _ in range(2, limit + 1)]
+    for i in range(2, int(limit ** 0.5) + 1):
+        if not is_prime[i]:
+            continue
+        is_prime[i * 2: limit + 1: i] = [False] * (limit // i - 1)
+    return [i for i, v in enumerate(is_prime) if v]
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    x_min, x_max, y_min, y_max = INF, -INF, INF, -INF
+    for hh in range(t):
+        n = int(input())
+        special_numbers = sorted(i * j for i, j in pairwise(extract_primes_from_sieve(110)))
+        answer = special_numbers[bisect_right(special_numbers, n)]
+        answers.append(f"{answer}")
+print(*answers, sep="\n")
