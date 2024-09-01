30cbe40f6c93f19549864e160d2b744a6b09c584
Lumi <djminstar573@gmail.com>
2024-09-01 22:04:52 +0900

    [Bronze I] Title: Total Circle, Time: 296 ms, Memory: 137556 KB -BaekjoonHub

---
+++ 백준/Bronze/17093. Total Circle/Total Circle.py
@@ -0,0 +1,36 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop, heapify
+from itertools import combinations, pairwise, permutations, combinations_with_replacement, product, zip_longest, chain, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n, m = int(input()), int(input())
+        p = [(int(input()), int(input())) for _ in range(n)]
+        q = [(int(input()), int(input())) for _ in range(m)]
+        answer = max(max((x1 - x2) ** 2 + (y1 - y2) ** 2 for x1, y1 in p) for x2, y2 in q)
+        answers.append(f"{answer}")
+print(*answers, sep="\n")
