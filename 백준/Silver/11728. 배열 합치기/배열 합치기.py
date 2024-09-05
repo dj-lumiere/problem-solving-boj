8a5578a4d09121f47e6b180181f37e282b219749
Lumi <djminstar573@gmail.com>
2024-09-05 11:25:14 +0900

    [Silver V] Title: 배열 합치기, Time: 836 ms, Memory: 439516 KB -BaekjoonHub

---
+++ 백준/Silver/11728. 배열 합치기/배열 합치기.py
@@ -0,0 +1,59 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+def merge(target1, target2):
+    result = []
+    target11 = target1[:]
+    target21 = target2[:]
+    while target11 and target21:
+        i = target11.pop()
+        j = target21.pop()
+        if i >= j:
+            result.append(i)
+            target21.append(j)
+        else:
+            result.append(j)
+            target11.append(i)
+    result.extend(target11[::-1])
+    result.extend(target21[::-1])
+    result.reverse()
+    return result
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
+    DIAGONAL = [(-1, -1), (1, -1), (1, 1), (-1, 1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        m = int(input())
+        a = [int(input()) for _ in range(n)]
+        b = [int(input()) for _ in range(m)]
+        answer = " ".join(map(str, merge(a, b)))
+        answers.append(f"{answer}")
+print(*answers, sep="\n")
