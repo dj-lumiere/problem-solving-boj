874ee8ff69dfe30b2a5d792b357d706b58f38a40
Lumi <djminstar573@gmail.com>
2024-09-06 13:45:14 +0900

    [Bronze III] Title: 등차수열에서 항 번호 찾기, Time: 292 ms, Memory: 134772 KB -BaekjoonHub

---
+++ 백준/Bronze/14913. 등차수열에서 항 번호 찾기/등차수열에서 항 번호 찾기.py
@@ -0,0 +1,42 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        a, d, k = (int(input()) for _ in range(3))
+        answer = ""
+        order, remainder = divmod(k - a, d)
+        if remainder != 0 or order < 0:
+            answer = "X"
+        else:
+            answer = f"{order + 1}"
+        answers.append(f"{answer}")
+print(*answers, sep="\n")
