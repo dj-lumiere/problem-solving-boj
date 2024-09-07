8d3bbbbcd151186ce32c82f8b4bde998c17b2589
Lumi <djminstar573@gmail.com>
2024-09-07 21:10:41 +0900

    [Bronze II] Title: 막대기, Time: 284 ms, Memory: 144920 KB -BaekjoonHub

---
+++ 백준/Bronze/17608. 막대기/막대기.py
@@ -0,0 +1,41 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop, heapify
+from itertools import combinations, pairwise, permutations, combinations_with_replacement, product, zip_longest, chain, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        a = [int(input()) for _ in range(n)]
+        a.reverse()
+        answer = 0
+        current_height = 0
+        for v in a:
+            if current_height < v:
+                answer += 1
+                current_height = v
+        answers.append(f"{answer}")
+print(*answers, sep="\n")
