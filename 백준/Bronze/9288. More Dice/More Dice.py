b6c18cab0546e943dc5ff3d0e08117499f1ef4c3
Lumi <djminstar573@gmail.com>
2024-09-08 22:37:49 +0900

    [Bronze III] Title: More Dice, Time: 280 ms, Memory: 137344 KB -BaekjoonHub

---
+++ 백준/Bronze/9288. More Dice/More Dice.py
@@ -0,0 +1,34 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop, heapify
+from itertools import combinations, pairwise, permutations, combinations_with_replacement, product, zip_longest, chain, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 998244353
+    t = int(input())
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        answer = "\n".join([f"Case {hh}:"]+[f"({a},{b})" for a, b in combinations_with_replacement(range(1, 7), 2) if a + b == n])
+        answers.append(f"{answer}")
+print(*answers, sep="\n")
