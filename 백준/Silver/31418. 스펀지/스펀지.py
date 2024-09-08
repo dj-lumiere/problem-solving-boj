3361b5747867fe40d6aa72473e12e6e414f3e651
Lumi <djminstar573@gmail.com>
2024-09-08 21:21:31 +0900

    [Silver IV] Title: 스펀지, Time: 788 ms, Memory: 376704 KB -BaekjoonHub

---
+++ 백준/Silver/31418. 스펀지/스펀지.py
@@ -0,0 +1,39 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop, heapify
+from itertools import combinations, pairwise, permutations, combinations_with_replacement, product, zip_longest, chain, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 998244353
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        w, h, k, tt = (int(input()) for _ in range(4))
+        viruses = [(int(input()), int(input())) for _ in range(k)]
+        movable_points = [(max(1, x - tt), max(1, y - tt), min(w, x + tt), min(h, y + tt)) for x, y in viruses]
+        answer = 1
+        for x1, y1, x2, y2 in movable_points:
+            answer *= (x2 - x1 + 1) * (y2 - y1 + 1)
+            answer %= MOD
+        answers.append(f"{answer}")
+print(*answers, sep="\n")
