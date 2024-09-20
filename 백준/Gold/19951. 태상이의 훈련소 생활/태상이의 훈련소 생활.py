11ae8de0b8de63811d6179bd20ec419374f51a3b
Lumi <djminstar573@gmail.com>
2024-09-20 13:47:58 +0900

    [Gold V] Title: 태상이의 훈련소 생활, Time: 356 ms, Memory: 168976 KB -BaekjoonHub

---
+++ 백준/Gold/19951. 태상이의 훈련소 생활/태상이의 훈련소 생활.py
@@ -0,0 +1,46 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        m = int(input())
+        grid = [0] + [int(input()) for _ in range(n)]
+        grid_rectangle = [0 for _ in range(n + 2)]
+        for _ in range(m):
+            a, b, k = (int(input()) for _ in range(3))
+            grid_rectangle[b + 1] -= k
+            grid_rectangle[a] += k
+        for i in range(1, n + 2):
+            grid_rectangle[i] += grid_rectangle[i - 1]
+        answer = " ".join(map(str, [i + j for i, j in zip(grid[1:], grid_rectangle[1:])]))
+        answers.append(f"{answer}")
+print(*answers, sep="\n")
