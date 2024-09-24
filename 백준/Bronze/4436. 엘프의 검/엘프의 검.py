605e593366bf0276f9143403283dcd2d44415be2
Lumi <djminstar573@gmail.com>
2024-09-24 10:21:18 +0900

    [Bronze II] Title: 엘프의 검, Time: 260 ms, Memory: 134776 KB -BaekjoonHub

---
+++ 백준/Bronze/4436. 엘프의 검/엘프의 검.py
@@ -0,0 +1,49 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = INF
+    answers = []
+    for hh in range(1, t + 1):
+        s = input()
+        if s is None:
+            break
+        n = int(s)
+        all_digits = set('0123456789')
+        seen_digits = set()
+        get_digits = lambda x: set(str(x))
+        k = 0
+        while seen_digits != all_digits:
+            k += 1
+            product = k * n
+            seen_digits.update(get_digits(product))
+        answer = k
+        answers.append(f"{answer}")
+print(*answers, sep="\n")
