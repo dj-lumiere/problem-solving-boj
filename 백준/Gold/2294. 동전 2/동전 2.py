a04f3a418c2eeee474e6569907426424be86dd78
Lumi <djminstar573@gmail.com>
2024-09-27 09:06:07 +0900

    [Gold V] Title: 동전 2, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Gold/2294. 동전 2/동전 2.py
@@ -0,0 +1,28 @@
+from sys import setrecursionlimit, stdout, stderr
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        m = int(input())
+        coins = [int(input()) for _ in range(n)]
+        dp = [INF for _ in range(m + 1)]
+        dp[0] = 0
+        for v in coins:
+            for j in range(m+1):
+                if j < v:
+                    continue
+                dp[j] = min(dp[j - v]+1, dp[j])
+        answer = -1 if dp[m] == INF else dp[m]
+        answers.append(f"{answer}")
+print(*answers, sep="\n")
