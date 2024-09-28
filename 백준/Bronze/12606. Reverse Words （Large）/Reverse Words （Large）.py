793d91f3ac78c96d6254d06dd661ae942af22cfe
Lumi <djminstar573@gmail.com>
2024-09-28 20:40:49 +0900

    [Bronze II] Title: Reverse Words (Large), Time: 212 ms, Memory: 128512 KB -BaekjoonHub

---
+++ 백준/Bronze/12606. Reverse Words （Large）/Reverse Words （Large）.py
@@ -0,0 +1,28 @@
+from collections import deque
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from math import isqrt
+from sys import stdout, stderr
+from itertools import permutations
+
+getcontext().prec = 30
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split("\n"))
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(1, t + 1):
+        sentence = input()
+        words = sentence.split()
+        reversed_words = words[::-1]
+        result = f"Case #{hh}: {' '.join(reversed_words)}"
+        answers.append(result)
+    print(*answers, sep="\n")
