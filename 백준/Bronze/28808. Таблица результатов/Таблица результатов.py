1297c18feab340dca69ae862e6c0c9ae92533a44
Lumi <djminstar573@gmail.com>
2024-09-28 19:37:33 +0900

    [Bronze IV] Title: Таблица результатов, Time: 212 ms, Memory: 128380 KB -BaekjoonHub

---
+++ 백준/Bronze/28808. Таблица результатов/Таблица результатов.py
@@ -0,0 +1,30 @@
+from collections import deque
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from math import isqrt
+from sys import stdout, stderr
+from itertools import permutations
+
+getcontext().prec = 30
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n, m = int(input()), int(input())
+        solved_problems_count = 0
+        for _ in range(n):
+            row = input()
+            if '+' in row:
+                solved_problems_count += 1
+        answers.append(f"{solved_problems_count}")
+    print(*answers, sep="\n")
