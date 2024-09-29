12f365f8d6984011fc8da9d1e073fbbb56981d9a
Lumi <djminstar573@gmail.com>
2024-09-29 12:49:59 +0900

    [Silver III] Title: 단순한 문제 (Large), Time: 516 ms, Memory: 291900 KB -BaekjoonHub

---
+++ 백준/Silver/25487. 단순한 문제 （Large）/단순한 문제 （Large）.py
@@ -0,0 +1,28 @@
+from bisect import bisect_right
+from collections import deque
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from math import isqrt
+from operator import index
+from sys import stdout, stderr
+from itertools import permutations
+
+getcontext().prec = 30
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(1, t + 1):
+        a, b, c = int(input()), int(input()), int(input())
+        answer = min(a, b, c)
+        answers.append(answer)
+    print(*answers, sep="\n")
