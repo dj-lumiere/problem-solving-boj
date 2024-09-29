9bd93c67a9e82e53f75dd03a2022789f13ea94dc
Lumi <djminstar573@gmail.com>
2024-09-29 12:56:05 +0900

    [Bronze II] Title: Stacked Floating Mountains, Time: 220 ms, Memory: 128892 KB -BaekjoonHub

---
+++ 백준/Bronze/5238. Stacked Floating Mountains/Stacked Floating Mountains.py
@@ -0,0 +1,37 @@
+from bisect import bisect_right
+from collections import Counter, deque
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from math import isqrt
+from operator import index
+from sys import stdout, stderr
+from itertools import permutations
+
+getcontext().prec = 30
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(1, t + 1):
+        k = int(input())
+        sequence = [int(input()) for _ in range(k)]
+        is_fibonacci_like = True
+        if k > 2:
+            for i in range(2, k):
+                if sequence[i] != sequence[i - 1] + sequence[i - 2]:
+                    is_fibonacci_like = False
+                    break
+        if is_fibonacci_like:
+            answers.append("YES")
+        else:
+            answers.append("NO")
+    print(*answers, sep="\n")
