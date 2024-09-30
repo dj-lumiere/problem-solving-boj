be4047e95b561303223f5d9ba8042020475cdcef
Lumi <djminstar573@gmail.com>
2024-09-30 12:27:54 +0900

    [Bronze II] Title: 벼락치기, Time: 228 ms, Memory: 126268 KB -BaekjoonHub

---
+++ 백준/Bronze/25373. 벼락치기/벼락치기.py
@@ -0,0 +1,33 @@
+from collections import deque, Counter
+from itertools import product, chain, permutations, combinations
+from string import ascii_lowercase
+from sys import stdout, stderr
+from time import perf_counter
+from decimal import Decimal
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        start = 0
+        end = n
+        while start + 1 < end:
+            mid = (start + end) // 2
+            watch_count = sum(max(mid - i, 0) for i in range(7))
+            if watch_count >= n:
+                end = mid
+            else:
+                start = mid
+        answer = end
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
