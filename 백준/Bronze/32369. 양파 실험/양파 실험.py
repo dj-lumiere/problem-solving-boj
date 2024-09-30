fc0f3eca6e0b233e936577674ee79367594c507f
Lumi <djminstar573@gmail.com>
2024-09-30 13:49:43 +0900

    [Bronze IV] Title: 양파 실험, Time: 216 ms, Memory: 126248 KB -BaekjoonHub

---
+++ 백준/Bronze/32369. 양파 실험/양파 실험.py
@@ -0,0 +1,33 @@
+from collections import deque, Counter
+from itertools import product, chain, permutations, combinations
+from string import ascii_lowercase
+from sys import stdout, stderr
+from time import perf_counter
+from decimal import Decimal
+from math import isqrt
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        appraise, condemn = 1, 1
+        a, b = int(input()), int(input())
+        for _ in range(n):
+            appraise += a
+            condemn += b
+            if appraise < condemn:
+                appraise, condemn = condemn, appraise
+            elif appraise == condemn:
+                condemn -= 1
+        answers.append(f"{appraise} {condemn}")
+    print(*answers, sep="\n")
