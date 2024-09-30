4b78febc3e2323832cf999e160ca42bf4339d40d
Lumi <djminstar573@gmail.com>
2024-09-30 17:13:27 +0900

    [Silver V] Title: 현권이와 신기한 수열, Time: 292 ms, Memory: 155004 KB -BaekjoonHub

---
+++ 백준/Silver/32172. 현권이와 신기한 수열/현권이와 신기한 수열.py
@@ -0,0 +1,42 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop, heapify
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+getcontext().prec = 1000
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        a = [0]
+        n = int(input())
+        already_exist = {0}
+        for i in range(1, 100000 + 1):
+            a_i = a[-1] - i
+            if a_i < 0 or a_i in already_exist:
+                a_i = a[-1] + i
+            a.append(a_i)
+            already_exist.add(a_i)
+        answer = a[n]
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
