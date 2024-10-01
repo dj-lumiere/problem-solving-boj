10dfe9b206a76c743961cea8d0d4e609f8ee4775
Lumi <djminstar573@gmail.com>
2024-10-01 17:20:18 +0900

    [Platinum III] Title: 삶의 질, Time: 2836 ms, Memory: 272556 KB, Score: 80 point -BaekjoonHub

--- 백준/Platinum/10227. 삶의 질/삶의 질.py
+++ 백준/Platinum/10227. 삶의 질/삶의 질.py
@@ -7,126 +7,6 @@ from operator import index
 from sys import stdout, stderr
 from itertools import permutations, product
 
-getcontext().prec = 30
-
-"""from https://github.com/cheran-senthil/PyRival/blob/master/pyrival/data_structures/SortedList.py"""
-
-
-class FenwickTree:
-    def __init__(self, x):
-        bit = self.bit = list(x)
-        size = self.size = len(bit)
-        for i in range(size):
-            j = i | (i + 1)
-            if j < size:
-                bit[j] += bit[i]
-
-    def update(self, idx, x):
-        """updates bit[idx] += x"""
-        while idx < self.size:
-            self.bit[idx] += x
-            idx |= idx + 1
-
-    def __call__(self, end):
-        """calc sum(bit[:end])"""
-        x = 0
-        while end:
-            x += self.bit[end - 1]
-            end &= end - 1
-        return x
-
-    def find_kth(self, k):
-        """Find largest idx such that sum(bit[:idx]) <= k"""
-        idx = -1
-        for d in reversed(range(self.size.bit_length())):
-            right_idx = idx + (1 << d)
-            if right_idx < self.size and self.bit[right_idx] <= k:
-                idx = right_idx
-                k -= self.bit[idx]
-        return idx + 1, k
-
-
-class SortedList:
-    block_size = 700
-
-    def __init__(self, iterable=()):
-        self.macro = []
-        self.micros = [[]]
-        self.micro_size = [0]
-        self.fenwick = FenwickTree([0])
-        self.size = 0
-        for item in iterable:
-            self.add(item)
-
-    def add(self, x):
-        i = bisect_left(self.macro, x)
-        j = bisect_right(self.micros[i], x)
-        self.micros[i].insert(j, x)
-        self.size += 1
-        self.micro_size[i] += 1
-        self.fenwick.update(i, 1)
-        if len(self.micros[i]) >= self.block_size:
-            self.micros[i:i + 1] = self.micros[i][:self.block_size >> 1], self.micros[i][self.block_size >> 1:]
-            self.micro_size[i:i + 1] = self.block_size >> 1, self.block_size >> 1
-            self.fenwick = FenwickTree(self.micro_size)
-            self.macro.insert(i, self.micros[i + 1][0])
-
-    def pop(self, k=-1):
-        i, j = self._find_kth(k)
-        self.size -= 1
-        self.micro_size[i] -= 1
-        self.fenwick.update(i, -1)
-        return self.micros[i].pop(j)
-
-    def remove(self, k):
-        if k in self:
-            self.pop(self.bisect_left(k))
-
-    def __getitem__(self, k):
-        if not (-len(self) <= k < len(self)):
-            raise IndexError("list index out of range")
-        if k < 0:
-            k += len(self)
-        i, j = self._find_kth(k)
-        return self.micros[i][j]
-
-    def count(self, x):
-        return self.bisect_right(x) - self.bisect_left(x)
-
-    def __contains__(self, x):
-        return self.count(x) > 0
-
-    def bisect_left(self, x):
-        i = bisect_left(self.macro, x)
-        return self.fenwick(i) + bisect_left(self.micros[i], x)
-
-    def bisect_right(self, x):
-        i = bisect_right(self.macro, x)
-        return self.fenwick(i) + bisect_right(self.micros[i], x)
-
-    def _find_kth(self, k):
-        return self.fenwick.find_kth(k + self.size if k < 0 else k)
-
-    def __len__(self):
-        return self.size
-
-    def __iter__(self):
-        return (x for micro in self.micros for x in micro)
-
-    def __repr__(self):
-        return str(list(self))
-
-    def __bool__(self):
-        return len(self) != 0
-
-    def clear(self):
-        self.macro = []
-        self.micros = [[]]
-        self.micro_size = [0]
-        self.fenwick = FenwickTree([0])
-        self.size = 0
-
-
 with open(0, 'r') as f:
     tokens = iter(f.read().split())
     input = lambda: next(tokens, None)
@@ -142,31 +22,26 @@ with open(0, 'r') as f:
     for hh in range(1, t + 1):
         r, c, h, w = (int(input()) for _ in range(4))
         grid = [[int(input()) for _ in range(c)] for _ in range(r)]
-        highest_quality_rank = INF
-        current_grid = SortedList([grid[k][l] for k, l in product(range(h), range(w))])
-        highest_quality_rank = min(highest_quality_rank, current_grid[h * w // 2])
-        for i in range(r - h + 1):
-            if i & 1:
-                for j in reversed(range(c - w)):
-                    for k in range(h):
-                        current_grid.remove(grid[i + k][j + w])
-                        current_grid.add(grid[i + k][j])
-                    highest_quality_rank = min(highest_quality_rank, current_grid[h * w // 2])
-                if i != r - h:
-                    for k in range(w):
-                        current_grid.remove(grid[i][k])
-                        current_grid.add(grid[i + h][k])
-                    highest_quality_rank = min(highest_quality_rank, current_grid[h * w // 2])
+        answer = 0
+        start = 0
+        end = r * c
+        while start + 1 < end:
+            mid = (start + end) // 2
+            is_bigger = [[0 if grid[y][x] == mid else (grid[y][x] - mid) // abs(grid[y][x] - mid) for x in range(c)] for
+                         y in range(r)]
+            accumulated_sum = [[0] * (c + 1)] + [[0] + [is_bigger[y][x] for x in range(c)] for y in range(r)]
+            for x, y in product(range(1, c), range(r)):
+                accumulated_sum[y + 1][x + 1] += accumulated_sum[y + 1][x]
+            for x, y in product(range(c), range(1, r)):
+                accumulated_sum[y + 1][x + 1] += accumulated_sum[y][x + 1]
+            for x, y in product(range(c - w + 1), range(r - h + 1)):
+                bigger_than_mid_count = accumulated_sum[y + h][x + w] - accumulated_sum[y + h][x] - \
+                                        accumulated_sum[y][x + w] + accumulated_sum[y][x]
+                if bigger_than_mid_count <= 0:
+                    end = mid
+                    break
             else:
-                for j in range(c - w):
-                    for k in range(h):
-                        current_grid.remove(grid[i + k][j])
-                        current_grid.add(grid[i + k][j + w])
-                    highest_quality_rank = min(highest_quality_rank, current_grid[h * w // 2])
-                if i != r - h:
-                    for k in range(1, w + 1):
-                        current_grid.remove(grid[i][c - k])
-                        current_grid.add(grid[i + h][c - k])
-                    highest_quality_rank = min(highest_quality_rank, current_grid[h * w // 2])
-        answers.append(f"{highest_quality_rank}")
+                start = mid
+        answer = end
+        answers.append(f"{answer}")
     print(*answers, sep="\n")
