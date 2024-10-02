01615cce6e8e3488b34dc94ffd6bd8855d345bdc
Lumi <djminstar573@gmail.com>
2024-10-02 12:43:14 +0900

    [Bronze I] Title: Chess Puzzle, Time: 204 ms, Memory: 126248 KB -BaekjoonHub

---
+++ 백준/Bronze/5239. Chess Puzzle/Chess Puzzle.py
@@ -0,0 +1,33 @@
+from collections import deque, Counter
+from itertools import product, chain, permutations, combinations
+from string import ascii_lowercase
+from sys import stdout, stderr
+from time import perf_counter
+from decimal import Decimal
+from math import isqrt
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(1, t + 1):
+        rooks = int(input())
+        rows = set()
+        cols = set()
+        safe = True
+        for _ in range(rooks):
+            x, y = int(input()), int(input())
+            if x in rows or y in cols:
+                safe = False
+            rows.add(x)
+            cols.add(y)
+        answers.append("SAFE" if safe else "NOT SAFE")
+    print(*answers, sep="\n")
