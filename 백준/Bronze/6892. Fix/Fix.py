c1c4d93a9a5a186c2ae8294dfc0132313df190a6
Lumi <djminstar573@gmail.com>
2024-10-02 13:04:15 +0900

    [Bronze II] Title: Fix, Time: 220 ms, Memory: 126248 KB -BaekjoonHub

---
+++ 백준/Bronze/6892. Fix/Fix.py
@@ -0,0 +1,41 @@
+from collections import deque, Counter
+from itertools import product, chain, permutations, combinations
+from string import ascii_lowercase
+from sys import stdout, stderr
+from time import perf_counter
+from decimal import Decimal
+from math import isqrt
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+
+
+    def is_fix_free(words):
+        for i in range(3):
+            for j in range(3):
+                if i != j:
+                    if words[i].startswith(words[j]) or words[i].endswith(words[j]):
+                        return False
+        return True
+
+
+    for hh in range(1, t + 1):
+        N = int(input())
+        answers = []
+        for _ in range(N):
+            words = [input() for _ in range(3)]
+            if is_fix_free(words):
+                answers.append("Yes")
+            else:
+                answers.append("No")
+    print(*answers, sep="\n")
