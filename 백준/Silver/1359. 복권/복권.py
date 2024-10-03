2a91ca745c54a2466ee6a09ca860ecaf887b7590
Lumi <djminstar573@gmail.com>
2024-10-03 17:25:30 +0900

    [Silver IV] Title: 복권, Time: 280 ms, Memory: 134776 KB -BaekjoonHub

---
+++ 백준/Silver/1359. 복권/복권.py
@@ -0,0 +1,42 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        N = int(input())
+        M = int(input())
+        K = int(input())
+        total_combinations = comb(N, M)
+        success_combinations = 0
+        for i in range(K, M + 1): 
+            success_combinations += comb(M, i) * comb(N - M, M - i)
+        probability = success_combinations / total_combinations
+        answers.append(probability)
+    print(*answers, sep="\n")
