9590d5d76c96083a3cfa448a9d60f3a6b769537b
Lumi <djminstar573@gmail.com>
2024-10-03 17:30:43 +0900

    [Bronze I] Title: 샷건, Time: 280 ms, Memory: 134252 KB -BaekjoonHub

---
+++ 백준/Bronze/32371. 샷건/샷건.py
@@ -0,0 +1,49 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split("\n"))
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        keyboard = [input() for _ in range(4)]
+        pressed_chars = set(input())
+        answer = ""
+        for i, j in product(range(4), range(10)):
+            if keyboard[i][j] not in pressed_chars:
+                continue
+            neighbors = {keyboard[i][j]}
+            for dx, dy in DELTA:
+                nx, ny = i + dx, j + dy
+                if not is_inbound(nx, 4, ny, 10):
+                    continue
+                neighbors.add(keyboard[nx][ny])
+            if neighbors == pressed_chars:
+                answer = keyboard[i][j]
+                break
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
