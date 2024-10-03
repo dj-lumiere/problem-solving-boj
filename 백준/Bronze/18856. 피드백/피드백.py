730593f4de973388fab16fef9c83ec9591d78dec
Lumi <djminstar573@gmail.com>
2024-10-03 17:21:13 +0900

    [Bronze III] Title: 피드백, Time: 292 ms, Memory: 134772 KB -BaekjoonHub

---
+++ 백준/Bronze/18856. 피드백/피드백.py
@@ -0,0 +1,47 @@
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        def is_prime(num):
+            if num < 2:
+                return False
+            for i in range(2, int(num ** 0.5) + 1):
+                if num % i == 0:
+                    return False
+            return True
+        N = int(input())
+        A = [i + 1 for i in range(N)]
+        for num in range(N, 1001):
+            if is_prime(num):
+                A[-1] = num
+        answers.append(N)
+        answers.append(" ".join(map(str, A)))
+    print(*answers, sep="\n")
