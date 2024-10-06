ebd560e6fd6a5e684ed884794a0967cdc39c76be
Lumi <djminstar573@gmail.com>
2024-10-06 21:18:59 +0900

    [Silver IV] Title: Cookie Clicker Alpha (Large), Time: 4188 ms, Memory: 154760 KB -BaekjoonHub

---
+++ 백준/Silver/12261. Cookie Clicker Alpha （Large）/Cookie Clicker Alpha （Large）.py
@@ -0,0 +1,51 @@
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from inspect import stack
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = int(input())
+    answers = []
+    for hh in range(1, t + 1):
+        C = Decimal(input())
+        F = Decimal(input())
+        X = Decimal(input())
+        production_rate = Decimal(2.0)
+        total_time = Decimal(0.0)
+        while True:
+            time_to_win_without_farm = X / production_rate
+            time_to_buy_farm = C / production_rate
+            time_to_win_with_farm = time_to_buy_farm + (X / (production_rate + F))
+            if time_to_win_without_farm <= time_to_win_with_farm:
+                total_time += time_to_win_without_farm
+                break
+            else:
+                total_time += time_to_buy_farm
+                production_rate += F
+        answers.append(f"Case #{hh}: {total_time:.7f}")
+    print(*answers, sep="\n")
