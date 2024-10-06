6f4bdb673f7ccd662921b7ee940873c406354e7e
Lumi <djminstar573@gmail.com>
2024-10-06 21:15:05 +0900

    [Bronze I] Title: Safe Squares, Time: 348 ms, Memory: 144772 KB -BaekjoonHub

---
+++ 백준/Bronze/20721. Safe Squares/Safe Squares.py
@@ -0,0 +1,51 @@
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from inspect import stack
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        board = []
+        rook_rows = [False] * 8
+        rook_cols = [False] * 8
+        for i in range(8):
+            line = input()
+            board.append(line)
+            for j in range(8):
+                if line[j] == 'R':
+                    rook_rows[i] = True
+                    rook_cols[j] = True
+        safe_count = 0
+        for i in range(8):
+            for j in range(8):
+                if board[i][j] == '.' and not rook_rows[i] and not rook_cols[j]:
+                    safe_count += 1
+        answers.append(safe_count)
+    print(*answers, sep="\n")
