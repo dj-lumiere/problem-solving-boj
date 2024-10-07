2a8a4cae7b32b00700f2d36c03f88334a050de24
Lumi <djminstar573@gmail.com>
2024-10-07 16:05:44 +0900

    [Bronze IV] Title: Corona Virus Testing, Time: 220 ms, Memory: 126248 KB -BaekjoonHub

---
+++ 백준/Bronze/25828. Corona Virus Testing/Corona Virus Testing.py
@@ -0,0 +1,35 @@
+from collections import deque, Counter
+from itertools import product, chain, permutations, combinations, repeat
+from string import ascii_lowercase
+from sys import stdout, stderr
+from time import perf_counter
+from decimal import Decimal
+from math import isqrt
+from heapq import heappush, heappop
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    T = 1
+    answers = []
+    for hh in range(1, T + 1):
+        g = int(input())
+        p = int(input())
+        t = int(input())
+        individual_tests = g * p
+        group_tests = g + t * p
+        if individual_tests < group_tests:
+            result = 1
+        elif individual_tests > group_tests:
+            result = 2
+        else:
+            result = 0
+        answers.append(result)
+    print(*answers, sep="\n")
