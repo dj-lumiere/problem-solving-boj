ff6433888013484a5391fe798e1ef59b1379a7e7
Lumi <djminstar573@gmail.com>
2024-10-07 15:37:53 +0900

    [Bronze II] Title: Košnja, Time: 252 ms, Memory: 138640 KB -BaekjoonHub

---
+++ 백준/Bronze/15322. Košnja/Košnja.py
@@ -0,0 +1,30 @@
+from collections import deque, Counter
+from itertools import product, chain, permutations, combinations, repeat
+from string import ascii_lowercase
+from sys import stdout, stderr
+from time import perf_counter
+from decimal import Decimal
+from math import isqrt
+from heapq import heappush, heappop
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(1, t + 1):
+        N = int(input())
+        M = int(input())
+        if N == 1 or M == 1:
+            turns = 0
+        else:
+            turns = 2 * (min(N, M) - 1)
+        answers.append(turns)
+    print(*answers, sep="\n")
