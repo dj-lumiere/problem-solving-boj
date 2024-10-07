92aed38678b637a849c7eca8275251843965ff25
Lumi <djminstar573@gmail.com>
2024-10-07 16:08:42 +0900

    [Bronze II] Title: Цапли, Time: 216 ms, Memory: 126248 KB -BaekjoonHub

---
+++ 백준/Bronze/21569. Цапли/Цапли.py
@@ -0,0 +1,28 @@
+from collections import deque, Counter
+from itertools import product, chain, permutations, combinations, repeat
+from string import ascii_lowercase
+from sys import stdout, stderr
+from time import perf_counter
+from decimal import Decimal
+from math import isqrt
+from heapq import heappush, heappop
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    T = 1
+    answers = []
+    for hh in range(1, T + 1):
+        a = int(input())
+        b = int(input())
+        min_herons = max((a + 1) // 2, (b + 1) // 2)
+        max_herons = min(a, b)
+        answers.extend((min_herons, max_herons))
+    print(*answers, sep="\n")
