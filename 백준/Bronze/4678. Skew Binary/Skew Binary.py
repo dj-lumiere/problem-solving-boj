01c4e0c17c20e63b8b32e5fb8b81f8a5643a2e24
Lumi <djminstar573@gmail.com>
2024-10-08 17:39:56 +0900

    [Bronze II] Title: Skew Binary, Time: 236 ms, Memory: 131648 KB -BaekjoonHub

---
+++ 백준/Bronze/4678. Skew Binary/Skew Binary.py
@@ -0,0 +1,35 @@
+from bisect import bisect_left, bisect_right
+from collections import deque, Counter
+from itertools import product, chain, permutations, combinations, repeat
+from string import ascii_lowercase
+from sys import stdout, stderr
+from time import perf_counter
+from decimal import Decimal
+from math import isqrt
+from heapq import heappush, heappop
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    T = INF
+    answers = []
+    for hh in range(1, T + 1):
+        skew_number = input()
+        if skew_number == "0":
+            break
+        decimal_value = 0
+        length = len(skew_number)
+        for i in range(length):
+            digit = int(skew_number[length - i - 1])
+            value = digit * ((1 << (i + 1)) - 1)
+            decimal_value += value
+        answers.append(decimal_value)
+    print(*answers, sep="\n")
