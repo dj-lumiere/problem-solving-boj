b6b1fb2e354656e1d6adc8dd63e417f6ff51fe40
Lumi <djminstar573@gmail.com>
2024-10-09 23:25:55 +0900

    [Bronze II] Title: КАРТИ, Time: 356 ms, Memory: 144772 KB -BaekjoonHub

---
+++ 백준/Bronze/24347. КАРТИ/КАРТИ.py
@@ -0,0 +1,48 @@
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from inspect import stack
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        c1 = input()
+        c2 = input()
+        c3 = input()
+        c4 = input()
+        numbers = [c1, c2, c3, c4]
+        for i in range(3):
+            for j in range(3 - i):
+                if numbers[j] + numbers[j + 1] < numbers[j + 1] + numbers[j]:
+                    temp = numbers[j]
+                    numbers[j] = numbers[j + 1]
+                    numbers[j + 1] = temp
+        largest_number = numbers[0] + numbers[1] + numbers[2] + numbers[3]
+        answers.append(largest_number)
+    print(*answers, sep="\n")
