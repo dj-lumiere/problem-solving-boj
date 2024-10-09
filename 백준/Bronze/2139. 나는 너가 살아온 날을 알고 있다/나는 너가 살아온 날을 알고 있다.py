abf2032b1d22f01c26f7efbb98b4cc631c12fabf
Lumi <djminstar573@gmail.com>
2024-10-09 23:29:02 +0900

    [Bronze II] Title: 나는 너가 살아온 날을 알고 있다, Time: 332 ms, Memory: 144772 KB -BaekjoonHub

---
+++ 백준/Bronze/2139. 나는 너가 살아온 날을 알고 있다/나는 너가 살아온 날을 알고 있다.py
@@ -0,0 +1,51 @@
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from inspect import stack
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = INF
+    answers = []
+    days_in_month_normal = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    days_in_month_leap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    for hh in range(1, t + 1):
+        d = int(input())
+        m = int(input())
+        y = int(input())
+        if d == 0 and m == 0 and y == 0:
+            break
+        if (y % 4 == 0 and y % 100 != 0) or (y % 400 == 0):
+            days_in_month = days_in_month_leap
+        else:
+            days_in_month = days_in_month_normal
+        days_count = 0
+        for month in range(1, m):
+            days_count += days_in_month[month - 1] 
+        days_count += d
+        answers.append(days_count)
+    print(*answers, sep="\n")
