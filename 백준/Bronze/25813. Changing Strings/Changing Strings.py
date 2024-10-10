f4f24c79944459791e2b448e7a96a442c0365a63
Lumi <djminstar573@gmail.com>
2024-10-10 23:07:16 +0900

    [Bronze III] Title: Changing Strings, Time: 336 ms, Memory: 144788 KB -BaekjoonHub

---
+++ 백준/Bronze/25813. Changing Strings/Changing Strings.py
@@ -0,0 +1,53 @@
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from inspect import stack
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+from datetime import datetime, time, timedelta
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    x = 1
+    answers = []
+    for hh in range(1, x + 1):
+        S = input()
+        left_U = -1
+        right_F = -1
+        for i in range(len(S)):
+            if S[i] == 'U' and left_U == -1:
+                left_U = i
+            if S[i] == 'F':
+                right_F = i
+        result = ""
+        for i in range(left_U):
+            result += '-'
+        result += 'U'
+        for i in range(left_U + 1, right_F):
+            result += 'C'
+        result += 'F'
+        for i in range(right_F + 1, len(S)):
+            result += '-'
+        answers.append(result)
+    print(*answers, sep="\n")
