2e0078cca89c5c93c0227f949db264597d4aee3e
Lumi <djminstar573@gmail.com>
2024-10-13 00:30:05 +0900

    [Bronze II] Title: Grading Exams, Time: 284 ms, Memory: 135800 KB -BaekjoonHub

---
+++ 백준/Bronze/9296. Grading Exams/Grading Exams.py
@@ -0,0 +1,42 @@
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    x = int(input())
+    answers = []
+    for hh in range(1, x + 1):
+        L = int(input())
+        answer_key = []
+        student_responses = []
+        answer_key.extend(input())
+        student_responses.extend(input())
+        incorrect_count = 0
+        for i in range(L):
+            if answer_key[i] != student_responses[i]:
+                incorrect_count += 1
+        answers.append(f"Case {hh}: {incorrect_count}")
+    print(*answers, sep="\n")
