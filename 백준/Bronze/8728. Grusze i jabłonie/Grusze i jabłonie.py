740f49bba965651c035eacb66ae0c74b147ece8d
Lumi <djminstar573@gmail.com>
2024-10-13 00:26:52 +0900

    [Bronze II] Title: Grusze i jabłonie, Time: 424 ms, Memory: 231904 KB -BaekjoonHub

---
+++ 백준/Bronze/8728. Grusze i jabłonie/Grusze i jabłonie.py
@@ -0,0 +1,51 @@
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    x = 1
+    answers = []
+    for hh in range(1, x + 1):
+        n = int(input())
+        trees = []
+        for i in range(n):
+            trees.append(int(input()))
+        first_pear = -1
+        first_apple = -1
+        last_pear = -1
+        last_apple = -1
+        for i in range(n):
+            if trees[i] == 0:
+                if first_pear == -1:
+                    first_pear = i
+                last_pear = i
+            elif trees[i] == 1:
+                if first_apple == -1:
+                    first_apple = i
+                last_apple = i
+        max_distance = max(abs(first_pear - last_apple), abs(first_apple - last_pear))
+        answers.append(max_distance)
+    print(*answers, sep="\n")
