850a2be1d0f5e9b7d729d704584107002425a6ef
Lumi <djminstar573@gmail.com>
2024-10-13 23:07:39 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 2412 ms, Memory: 130336 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,11 +1,12 @@
 # 10989 수 정렬하기 3
 
 from sys import stdin, stdout
+from array import array
 
-N = int(stdin.readline().rstrip())
+N = int(stdin.readline().strip())
 num_list = [0] * 10001
 for _ in range(N):
-    num_list[int(stdin.readline().rstrip())] += 1
+    num_list[int(stdin.readline().strip())] += 1
 for i, j in enumerate(num_list):
     for _ in range(j):
-        stdout.writelines(f"{i}\n")
+        stdout.write(f"{i}\n")
