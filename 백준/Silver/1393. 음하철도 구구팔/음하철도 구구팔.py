b25085e11e50f1de5821846906e7ca9bb172a861
Lumi <djminstar573@gmail.com>
2024-10-13 21:32:54 +0900

    [Silver I] Title: 음하철도 구구팔, Time: 268 ms, Memory: 134764 KB -BaekjoonHub

---
+++ 백준/Silver/1393. 음하철도 구구팔/음하철도 구구팔.py
@@ -0,0 +1,44 @@
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    x = 1
+    answers = []
+    for hh in range(1, x + 1):
+        xs, ys, xe, ye, dx, dy = (int(input()) for _ in range(6))
+        on_line = lambda x, y: ((y - ye) * dx == dy * (x - xe)) and (y - ye) * dy >= 0 and (x - xe) * dx >= 0
+        min_distance = INF
+        min_point = (INF, INF)
+        for x, y in product(range(-100, 101), repeat=2):
+            if not on_line(x, y):
+                continue
+            distance = (xs - x) ** 2 + (ys - y) ** 2
+            if distance < min_distance:
+                min_distance = distance
+                min_point = (x, y)
+        answers.append(f"{min_point[0]} {min_point[1]}")
+    print(*answers, sep="\n")
