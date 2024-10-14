4becb85579b52ada254d89c5080b53b16c17d14c
Lumi <djminstar573@gmail.com>
2024-10-14 19:38:19 +0900

    [Bronze III] Title: Leiutaja number üks, Time: 272 ms, Memory: 134768 KB -BaekjoonHub

---
+++ 백준/Bronze/29918. Leiutaja number üks/Leiutaja number üks.py
@@ -0,0 +1,48 @@
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import perf_counter_ns, sleep
+from datetime import datetime, time, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import cos, comb, log, gcd, floor, log2, log10, pi, ceil, factorial, sin, sqrt, atan2, tau
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, groupby
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    x = 1
+    answers = []
+    for hh in range(1, x + 1):
+        WEEKS = 10
+        N0 = int(input())
+        M0 = int(input())
+        max_competitor_inventions = 0
+        for i in range(5):
+            Ni = int(input())
+            Mi = int(input())
+            competitor_final_inventions = Ni + Mi * WEEKS
+            if competitor_final_inventions > max_competitor_inventions:
+                max_competitor_inventions = competitor_final_inventions
+        adalbert_final_inventions = N0 + M0 * WEEKS
+        if adalbert_final_inventions > max_competitor_inventions:
+            answers.append(0)
+        else:
+            required_inventions = max_competitor_inventions - adalbert_final_inventions + 1
+            answers.append(required_inventions)
+    print(*answers, sep="\n")
