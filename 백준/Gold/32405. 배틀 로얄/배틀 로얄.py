bd2711f52556a94d4b47744c1a22be76f72a189b
Lumi <djminstar573@gmail.com>
2024-10-15 09:42:26 +0900

    [Gold IV] Title: 배틀 로얄, Time: 560 ms, Memory: 259908 KB -BaekjoonHub

---
+++ 백준/Gold/32405. 배틀 로얄/배틀 로얄.py
@@ -0,0 +1,48 @@
+import re
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from collections import deque, Counter
+from datetime import datetime, time, timedelta
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+from heapq import heapify, heappush, heappop
+from inspect import stack
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from math import cos, comb, log, gcd, floor, log2, log10, log1p, pi, ceil, factorial, sin, sqrt, atan2, tau
+from os import write
+from random import randint, shuffle
+from string import ascii_uppercase, ascii_lowercase
+from sys import setrecursionlimit, stdout, stderr
+from time import perf_counter_ns, sleep
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        d = [int(input()) for _ in range(n)]
+        h = [int(input()) for _ in range(n)]
+        person = [(v1, v2, i) for i, (v1, v2) in enumerate(zip(d, h))]
+        total_damage = 0
+        queue = deque(person)
+        while len(queue) > 1:
+            di, hi, i = queue.popleft()
+            if total_damage >= hi:
+                continue
+            total_damage += di
+            queue.append((di, hi + di, i))
+        answer = queue.popleft()[2] + 1
+        answers.append(answer)
+    print(*answers, sep="\n")
