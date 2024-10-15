05c11c29c56d6518ff1cbf3a4e863cdbef7a0930
Lumi <djminstar573@gmail.com>
2024-10-15 22:15:15 +0900

    [Bronze III] Title: 繰り返し (Repetition), Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/31613. 繰り返し （Repetition）/繰り返し （Repetition）.py
@@ -0,0 +1,28 @@
+from sys import stderr, stdout
+
+with open(0, "r") as f:
+    tokens = iter(f.read().split("\n"))
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    x = 1
+    answers = []
+    for hh in range(1, x + 1):
+        X, N = int(input()), int(input())
+        steps = 0
+        while X < N:
+            r = X % 3
+            if r == 0:
+                X += 1
+            elif r == 1:
+                X *= 2
+            else:
+                X *= 3
+            steps += 1
+        answers.append(f"{steps}")
+    print(*answers, sep="\n")
