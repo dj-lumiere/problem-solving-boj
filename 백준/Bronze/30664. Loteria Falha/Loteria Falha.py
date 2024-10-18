88f3ec8109fcc97e3c951afbd915fd9cb728941a
Lumi <djminstar573@gmail.com>
2024-10-18 21:04:47 +0900

    [Bronze V] Title: Loteria Falha, Time: 84 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/30664. Loteria Falha/Loteria Falha.py
@@ -0,0 +1,21 @@
+from sys import stderr, stdout
+
+with open(0, "r") as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(1, 0), (0, 1), (-1, 0), (0, -1), (-1, 1), (1, -1), (1, 1), (-1, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = INF
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        if n == 0:
+            break
+        answer = "PREMIADO" if n % 42 == 0 else "TENTE NOVAMENTE"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
