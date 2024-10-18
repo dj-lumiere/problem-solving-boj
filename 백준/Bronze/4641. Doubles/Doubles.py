120a422767acd0e9de8e9e8d015c3393cbf9b2e2
Lumi <djminstar573@gmail.com>
2024-10-18 17:52:18 +0900

    [Bronze I] Title: Doubles, Time: 256 ms, Memory: 130156 KB -BaekjoonHub

---
+++ 백준/Bronze/4641. Doubles/Doubles.py
@@ -0,0 +1,39 @@
+import re
+from array import array
+from base64 import b64decode, b64encode
+from bisect import bisect_left, bisect_right
+from collections import deque, Counter
+from datetime import datetime, time, timedelta
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+from heapq import heapify, heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest, chain, repeat, \
+    groupby
+from math import cos, comb, log, gcd, floor, log2, log10, log1p, pi, ceil, factorial, sin, sqrt, isqrt, atan2, tau
+from os import write
+from random import randint, shuffle
+from string import ascii_uppercase, ascii_lowercase
+from sys import setrecursionlimit, stdout, stderr
+from time import perf_counter_ns, sleep
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split("\n"))
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = INF
+    answers = []
+    for hh in range(1, t + 1):
+        nums = list(map(int, input().split()))
+        if nums == [-1]:
+            break
+        nums = nums[:-1]  # Remove the trailing 0
+        answer = sum(1 for num in nums if num * 2 in nums)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
