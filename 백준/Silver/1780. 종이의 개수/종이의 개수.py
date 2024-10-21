70e06f62567d4cce1ade4c62d41f7263f407eecc
Lumi <djminstar573@gmail.com>
2024-10-21 22:52:11 +0900

    [Silver II] Title: 종이의 개수, Time: 2072 ms, Memory: 420016 KB -BaekjoonHub

--- 백준/Silver/1780. 종이의 개수/종이의 개수.py
+++ 백준/Silver/1780. 종이의 개수/종이의 개수.py
@@ -1,39 +1,46 @@
-from sys import stdin
-from math import log
+from itertools import product
+from sys import stderr, stdout
 
-initial_grid_size = int(input())
-initial_number_grid = []
-for i in range(initial_grid_size):
-    initial_number_grid.append(list(map(int, stdin.readline().rstrip().split(" "))))
-
-number_paper_count = {"plus_1":0, "zero":0, "minus_1":0}
-
-def number_paper(grid_size:int, number_grid: list[list[int]], x_start:int, y_start:int, number_paper_count:dict[str, int]) -> dict[str, int]:
-    x_finish = x_start + grid_size
-    y_finish = y_start + grid_size
-    plus_1_count = 0
-    zero_count = 0
-    minus_1_count = 0
-    for i in range(x_start, x_finish):
-        for j in range(y_start, y_finish):
-            if number_grid[j][i] == 1:
-                plus_1_count += 1
-            elif number_grid[j][i] == -1:
-                minus_1_count += 1
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        grid = [[int(input()) for _ in range(n)] for _ in range(n)]
+        paper_count = [0, 0, 0]
+        stack = [(n, 0, 0)]
+        while stack:
+            size, x_start, y_start = stack.pop()
+            x_end, y_end = x_start + size, y_start + size
+            plus_1_count = 0
+            zero_count = 0
+            minus_1_count = 0
+            for j in range(y_start, y_end):
+                for i in range(x_start, x_end):
+                    if grid[j][i] == 1:
+                        plus_1_count += 1
+                    elif grid[j][i] == -1:
+                        minus_1_count += 1
+                    else:
+                        zero_count +=1
+            if plus_1_count == size * size:
+                paper_count[1] += 1
+            elif zero_count == size * size:
+                paper_count[0] += 1
+            elif minus_1_count == size * size:
+                paper_count[-1] += 1
             else:
-                zero_count +=1
-    if plus_1_count == (grid_size)**2:
-        number_paper_count["plus_1"] += 1
-    elif zero_count == (grid_size)**2:
-        number_paper_count["zero"] += 1
-    elif minus_1_count == (grid_size)**2:
-        number_paper_count["minus_1"] += 1
-    else:
-        for i in range(3):
-            for j in range(3):
-                number_paper(grid_size//3, number_grid, x_start+((grid_size//3)*i), y_start+((grid_size//3)*j), number_paper_count)
-    return number_paper_count
-
-number_paper_result = number_paper(initial_grid_size, initial_number_grid, 0, 0, number_paper_count)
-
-print(f'{number_paper_result["minus_1"]}\n{number_paper_result["zero"]}\n{number_paper_result["plus_1"]}')
+                for i, j in product(range(3), repeat=2):
+                    stack.append((size // 3, x_start + size // 3 * i, y_start + size // 3 * j))
+        answer = f"{paper_count[-1]}\n{paper_count[0]}\n{paper_count[1]}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
