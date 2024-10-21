e3fb01ea97a108f59606edddf03efd3193882a50
Lumi <djminstar573@gmail.com>
2024-10-21 16:14:34 +0900

    [Gold V] Title: 무한 수열 2, Time: 3104 ms, Memory: 478424 KB -BaekjoonHub

--- 백준/Gold/1354. 무한 수열 2/무한 수열 2.py
+++ 백준/Gold/1354. 무한 수열 2/무한 수열 2.py
@@ -12,6 +12,8 @@ with open(0, 'r') as f:
     MOD = 1_000_000_007
     t = 1
     answers = []
+    MERGE = 1
+    SORT = 2
     for hh in range(1, t + 1):
         n = int(input())
         p = int(input())
@@ -19,16 +21,32 @@ with open(0, 'r') as f:
         x = int(input())
         y = int(input())
         results = dict()
-        results[0] = 1
+        THRESHOLD = 10 ** 6
+        result_small = [0 for _ in range(THRESHOLD + 1)]
+        result_small[0] = 1
         stack = [n]
+        for i in range(1, THRESHOLD + 1):
+            next_number1 = max(i // p - x, 0)
+            next_number2 = max(i // q - y, 0)
+            result_small[i] = result_small[next_number1] + result_small[next_number2]
         while stack:
             i = stack.pop()
-            if i == 0:
+            if i <= THRESHOLD:
                 continue
             next_number1 = max(i // p - x, 0)
             next_number2 = max(i // q - y, 0)
-            if next_number1 in results and next_number2 in results:
-                results[i] = results[next_number1] + results[next_number2]
+            sub_result1 = -INF
+            sub_result2 = -INF
+            if next_number1 <= THRESHOLD:
+                sub_result1 = result_small[next_number1]
+            elif next_number1 in results:
+                sub_result1 = results[next_number1]
+            if next_number2 <= THRESHOLD:
+                sub_result2 = result_small[next_number2]
+            elif next_number2 in results:
+                sub_result2 = results[next_number2]
+            if (sub_result1 != -INF) and (sub_result2 != -INF):
+                results[i] = sub_result1 + sub_result2
                 continue
             if i in results:
                 continue
@@ -37,6 +55,6 @@ with open(0, 'r') as f:
             stack.append(i)
             for z in {next_number1, next_number2}:
                 stack.append(z)
-        answer = results[n]
+        answer = answer = result_small[n] if n <= THRESHOLD else results[n]
         answers.append(answer)
     print(*answers, sep="\n")
