d8357159c5c59623c781df24371f665ec3685d8e
Lumi <djminstar573@gmail.com>
2024-10-22 12:30:41 +0900

    [Platinum I] Title: 마지막 요세푸스 문제, Time: 256 ms, Memory: 112680 KB -BaekjoonHub

--- 백준/Platinum/1179. 마지막 요세푸스 문제/마지막 요세푸스 문제.py
+++ 백준/Platinum/1179. 마지막 요세푸스 문제/마지막 요세푸스 문제.py
@@ -1,28 +1,58 @@
-# 1179 마지막 요세푸스 문제
-from functools import cache
-from sys import setrecursionlimit
+from sys import setrecursionlimit, stdout, stderr
 
-setrecursionlimit(1000000)
-
-global function_call_count
-function_call_count = 0
-
-
-@cache
-def sol(n, k):
-    global function_call_count
-    function_call_count += 1
-    if n == 1:
-        return 0
-    if k == 1:
-        return n - 1
-    if 1 < n < k:
-        return (sol(n - 1, k) + k) % n
-    h = sol(n - n // k, k) - (n % k)
-    if h < 0:
-        return h + n
-    return h + h // (k - 1)
-
-
-n, k = map(int, input().split(" "))
-print(sol(n, k) + 1)
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        x, y = int(input()), int(input())
+        results = {}
+        stack = [(x, y)]
+        while stack:
+            n, k = stack.pop()
+            eprint(n, k)
+            if (n, k) in results:
+                continue
+            elif 1 < n < k:
+                # try to find (n-1, k)
+                if n - 1 == 1:
+                    results[(n, k)] = k % n
+                elif k == 1:
+                    results[(n, k)] = (n - 1 + k) % n
+                elif (n - 1, k) in results:
+                    results[(n, k)] = (results[(n - 1, k)] + k) % n
+                else:
+                    stack.append((n, k))
+                    stack.append((n - 1, k))
+            else:
+                # try to find (n-n//k, k)
+                h = 0
+                next_n, next_k = n - n // k, k
+                if (next_n, next_k) in results:
+                    h = results[(next_n, next_k)]
+                elif next_n == 1:
+                    h = 0
+                elif next_k == 1:
+                    h = next_n - 1
+                elif 1 < next_n < next_k and (next_n - 1, next_k) in results:
+                    h = (results[(next_n - 1, next_k)] + next_k) % n
+                else:
+                    stack.append((n, k))
+                    stack.append((next_n, next_k))
+                    continue
+                h -= (n % k)
+                if h < 0:
+                    results[(n, k)] = h + n
+                else:
+                    results[(n, k)] = h + h // (k - 1)
+        answer = results[(n, k)] + 1
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
