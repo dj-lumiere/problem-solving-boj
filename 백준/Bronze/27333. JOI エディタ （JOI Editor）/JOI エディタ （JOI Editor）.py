7d15577c5ce85ea9536d7af810a923196a705c0f
Lumi <djminstar573@gmail.com>
2024-10-22 23:35:13 +0900

    [Bronze III] Title: JOI エディタ (JOI Editor), Time: 96 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/27333. JOI エディタ （JOI Editor）/JOI エディタ （JOI Editor）.py
@@ -0,0 +1,24 @@
+from itertools import product
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        s = list(input())
+        for i in range(1, n):
+            if s[i] == s[i - 1]:
+                s[i - 1] = s[i] = s[i].upper()
+        answer = "".join(s)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
