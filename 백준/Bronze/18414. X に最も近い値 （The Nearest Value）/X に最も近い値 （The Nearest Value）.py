a3ac0375e1ee884de26b81fea0e404fccffc8090
Lumi <djminstar573@gmail.com>
2024-10-22 23:46:26 +0900

    [Bronze IV] Title: X に最も近い値 (The Nearest Value), Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/18414. X に最も近い値 （The Nearest Value）/X に最も近い値 （The Nearest Value）.py
@@ -0,0 +1,19 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        x, l, r = int(input()), int(input()), int(input())
+        answer = x if x in range(l, r+1) else (l if abs(x - l) < abs(x - r) else r)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
