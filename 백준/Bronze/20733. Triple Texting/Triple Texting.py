b3df422cc7bbf2c17323d73049a01b78b09e1ce8
Lumi <djminstar573@gmail.com>
2024-10-26 23:34:31 +0900

    [Bronze II] Title: Triple Texting, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/20733. Triple Texting/Triple Texting.py
@@ -0,0 +1,24 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        s = input()
+        word_length = len(s) // 3
+        word1, word2, word3 = s[:word_length], s[word_length:2*word_length], s[2*word_length:]
+        answer = word1 if word1 == word2 or word1 == word3 else word2
+        answers.append(answer)
+    print(*answers, sep="\n")
