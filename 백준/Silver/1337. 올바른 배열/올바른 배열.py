cf51547fdb459d68e269885f997b75df8a19763e
Lumi <djminstar573@gmail.com>
2024-10-26 23:46:36 +0900

    [Silver IV] Title: 올바른 배열, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/1337. 올바른 배열/올바른 배열.py
@@ -0,0 +1,30 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        arr = sorted(int(input()) for _ in range(n))
+        min_additions = INF
+        for i in range(n):
+            end = arr[i] + 4
+            count = sum(1 for j in arr[i:] if arr[i] <= j <= end)
+            additions = 5 - count
+            if additions < min_additions:
+                min_additions = additions
+        answer = min_additions
+        answers.append(answer)
+    print(*answers, sep="\n")
