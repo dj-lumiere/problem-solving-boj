0b82723f27b79df58cb366fca7eba5494f082724
Lumi <djminstar573@gmail.com>
2024-10-26 23:38:42 +0900

    [Bronze IV] Title: Рождественская лотерея, Time: 228 ms, Memory: 126024 KB -BaekjoonHub

---
+++ 백준/Bronze/29683. Рождественская лотерея/Рождественская лотерея.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        a = int(input())
+        total_tickets = sum(int(input()) // a for _ in range(n))
+        answer = total_tickets
+        answers.append(answer)
+    print(*answers, sep="\n")
