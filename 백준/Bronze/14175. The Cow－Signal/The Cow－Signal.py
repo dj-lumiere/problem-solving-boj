85a0df477c16fdb8dc8418d0a9e8408b64339a1b
Lumi <djminstar573@gmail.com>
2024-10-26 23:36:33 +0900

    [Bronze II] Title: The Cow-Signal, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/14175. The Cow－Signal/The Cow－Signal.py
@@ -0,0 +1,25 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        m, n, k = int(input()), int(input()), int(input())
+        for _ in range(m):
+            row = input()
+            enlarged_row = ''.join(char * k for char in row)
+            for _ in range(k):
+                answers.append(enlarged_row)
+    print(*answers, sep="\n")
