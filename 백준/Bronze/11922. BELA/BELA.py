b9ac5bad56c88db7a32256032ac52ddaed987574
Lumi <djminstar573@gmail.com>
2024-10-27 21:30:51 +0900

    [Bronze II] Title: BELA, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/11922. BELA/BELA.py
@@ -0,0 +1,29 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for hh in range(t):
+        n, b = input(), input()
+        dominant_points = {'A': 11, 'K': 4, 'Q': 3, 'J': 20, 'T': 10, '9': 14, '8': 0, '7': 0}
+        non_dominant_points = {'A': 11, 'K': 4, 'Q': 3, 'J': 2, 'T': 10, '9': 0, '8': 0, '7': 0}
+        total_points = 0
+        for _ in range(4 * int(n)):
+            cardsuit = input()
+            card, suit = cardsuit[0], cardsuit[1:]
+            total_points += dominant_points[card] if suit == b else non_dominant_points[card]
+        answer = total_points
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
