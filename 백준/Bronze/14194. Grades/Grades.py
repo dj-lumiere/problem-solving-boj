55fa1dc48c0f63c36ad2b62a2e0057a314ec1607
Lumi <djminstar573@gmail.com>
2024-10-27 21:29:46 +0900

    [Bronze I] Title: Grades, Time: 156 ms, Memory: 133088 KB -BaekjoonHub

---
+++ 백준/Bronze/14194. Grades/Grades.py
@@ -0,0 +1,26 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        grades = [int(input()) for _ in range(n)]
+        grades.sort()
+        A = (grades[0] + grades[-1]) / 2
+        B = sum(grades) / n
+        answer = "Yes" if abs(A - B) < 1 else "No"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
