20712fda94a4ff52e71042cf541dfb0125b04233
Lumi <djminstar573@gmail.com>
2024-10-27 21:31:23 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.py
@@ -0,0 +1,34 @@
+from itertools import product
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for hh in range(t):
+        N, K_count = int(input()), int(input())
+        K = sorted([input() for _ in range(K_count)], reverse=True)
+        max_len = len(str(N))
+        found = False
+        for length in range(max_len, 0, -1):
+            for comb in product(K, repeat=length):
+                candidate = int(''.join(comb))
+                if candidate <= N:
+                    answer = candidate
+                    found = True
+                    break
+            if found:
+                break
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
