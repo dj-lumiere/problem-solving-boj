ed31ad0f56c250771892f23d7162bdd7b75189df
Lumi <djminstar573@gmail.com>
2024-10-27 21:46:16 +0900

    [Bronze I] Title: 운동회, Time: 104 ms, Memory: 108080 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/25425. 운동회/운동회.py
@@ -0,0 +1,31 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for hh in range(t):
+        N, M, a, K = (int(input()) for _ in range(4))
+        remaining_people = a - K
+        if remaining_people == 0:
+            max_rank = 1
+            min_rank = 1
+        else:
+            max_rank = remaining_people + 1 if remaining_people + 1 <= N else N
+            min_rank = (remaining_people + M - 1) // M + 1
+            if min_rank > N:
+                min_rank = N
+        answer = f"{max_rank} {min_rank}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
