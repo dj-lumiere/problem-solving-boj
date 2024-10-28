9382a38957c667bd69ae50b7ed3b39627afa51cc
Lumi <djminstar573@gmail.com>
2024-10-28 23:16:31 +0900

    [Bronze II] Title: George Boole, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/13234. George Boole/George Boole.py
@@ -0,0 +1,30 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for _ in range(t):
+        value1 = input()
+        operation = input()
+        value2 = input()
+        val1 = True if value1 == "true" else False
+        val2 = True if value2 == "true" else False
+        if operation == "AND":
+            res = val1 and val2
+        elif operation == "OR":
+            res = val1 or val2
+        answer = "true" if res else "false"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
