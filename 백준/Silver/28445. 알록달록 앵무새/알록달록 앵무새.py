e8ca3b5318ce66caa7c1f271a6f17dd5dbb6a741
Lumi <djminstar573@gmail.com>
2024-10-28 23:15:01 +0900

    [Silver V] Title: 알록달록 앵무새, Time: 96 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/28445. 알록달록 앵무새/알록달록 앵무새.py
@@ -0,0 +1,24 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_009
+    t = 1
+    answers = []
+    for hh in range(t):
+        father_body, father_tail, mother_body, mother_tail = input(), input(), input(), input()
+        colors = [father_body, father_tail, mother_body, mother_tail]
+        pairs = sorted({(b, t) for b in colors for t in colors})
+        answer = "\n".join([' '.join(pair) for pair in pairs])
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
