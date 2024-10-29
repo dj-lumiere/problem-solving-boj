e0cf29d9d24d9a09f1fcd522ace1d3f05d835597
Lumi <djminstar573@gmail.com>
2024-10-29 21:57:37 +0900

    [Bronze III] Title: Angles, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/15178. Angles/Angles.py
@@ -0,0 +1,24 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(t):
+        N = int(input())
+        for _ in range(N):
+            a, b, c = int(input()), int(input()), int(input())
+            if a + b + c == 180:
+                answer = f"{a} {b} {c} Seems OK"
+            else:
+                answer = f"{a} {b} {c} Check"
+            answers.append(f"{answer}")
+    print(*answers, sep="\n")
