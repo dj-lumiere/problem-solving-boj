a01276676d79a0d743c92cc1a6fde99199b02c8e
Lumi <djminstar573@gmail.com>
2024-10-31 22:25:41 +0900

    [Bronze II] Title: '나교수' 교수님의 악필, Time: 232 ms, Memory: 128416 KB -BaekjoonHub

---
+++ 백준/Bronze/16462. '나교수' 교수님의 악필/'나교수' 교수님의 악필.py
@@ -0,0 +1,31 @@
+from fractions import Fraction
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        scores = []
+        for _ in range(n):
+            q = input()
+            q_new = ''.join(['9' if c in '069' else c for c in q])
+            q_num = min(int(q_new), 100)
+            scores.append(q_num)
+        average = sum(scores) / n
+        if average - int(average) == 0.5:
+            avg_int = int(average) +1
+        else:
+            avg_int = int(average +0.5)
+        answer = f"{avg_int}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
