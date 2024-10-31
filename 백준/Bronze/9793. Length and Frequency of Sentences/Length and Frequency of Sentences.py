5bb78dc0fa8ec2071489c0bcf5ea6bffc7c151c8
Lumi <djminstar573@gmail.com>
2024-10-31 22:23:25 +0900

    [Bronze II] Title: Length and Frequency of Sentences, Time: 232 ms, Memory: 127760 KB -BaekjoonHub

---
+++ 백준/Bronze/9793. Length and Frequency of Sentences/Length and Frequency of Sentences.py
@@ -0,0 +1,27 @@
+from fractions import Fraction
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().splitlines())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        freq = {}
+        for _ in range(n):
+            sentence = input()
+            words = sentence.strip().split()
+            length = len(words)
+            freq[length] = freq.get(length, 0) + 1
+        for length in sorted(freq):
+            answer = f"{length} {freq[length]}"
+            answers.append(f"{answer}")
+    print(*answers, sep="\n")
