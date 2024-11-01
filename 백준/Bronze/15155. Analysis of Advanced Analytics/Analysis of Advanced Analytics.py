4622dacd8281d7092215a897552881ca3d8c7542
Lumi <djminstar573@gmail.com>
2024-11-01 22:02:09 +0900

    [Bronze II] Title: Analysis of Advanced Analytics, Time: 260 ms, Memory: 127760 KB -BaekjoonHub

---
+++ 백준/Bronze/15155. Analysis of Advanced Analytics/Analysis of Advanced Analytics.py
@@ -0,0 +1,27 @@
+from fractions import Fraction
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(t):
+        n, k = int(input()), int(input())
+        pages = [int(input()) for _ in range(n)]
+        notebooks, remaining = 1, k
+        for pages_needed in pages:
+            if pages_needed > remaining:
+                notebooks += 1
+                remaining = k - pages_needed
+            else:
+                remaining -= pages_needed
+        answers.append(notebooks)
+    print(*answers, sep="\n")
