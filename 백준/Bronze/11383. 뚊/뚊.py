ef8c4b8e360e53bf5a2ed5889a45277b69137620
Lumi <djminstar573@gmail.com>
2024-11-01 22:04:24 +0900

    [Bronze I] Title: 뚊, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/11383. 뚊/뚊.py
@@ -0,0 +1,21 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(t):
+        n, m = int(input()), int(input())
+        first_image = [input() for _ in range(n)]
+        second_image = [input() for _ in range(n)]
+        answer = "Eyfa" if all("".join(map(lambda x:x+x, first_image[i])) == second_image[i] for i in range(n)) else "Not Eyfa"
+        answers.append(answer)
+    print(*answers, sep="\n")
