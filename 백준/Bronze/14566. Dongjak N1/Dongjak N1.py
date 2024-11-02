848e1ef0202a7d511ed7f1ca658fec69b9e820b7
Lumi <djminstar573@gmail.com>
2024-11-02 22:16:43 +0900

    [Bronze I] Title: Dongjak N1, Time: 96 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/14566. Dongjak N1/Dongjak N1.py
@@ -0,0 +1,30 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        Ai = [int(input()) for _ in range(n)]
+        Ai.sort()
+        min_dist = INF
+        count = 0
+        for i in range(1, n):
+            diff = Ai[i] - Ai[i-1]
+            if diff < min_dist:
+                min_dist = diff
+                count = 1
+            elif diff == min_dist:
+                count += 1
+        answer = f"{min_dist} {count}"
+        answers.append(answer)
+    print(*answers, sep="\n")
