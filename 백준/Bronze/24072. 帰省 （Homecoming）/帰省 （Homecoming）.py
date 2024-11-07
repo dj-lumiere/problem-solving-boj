4dff81ff00241670fa1bfd0a3118e2882a561931
Lumi <djminstar573@gmail.com>
2024-11-07 21:39:35 +0900

    [Bronze IV] Title: 帰省 (Homecoming), Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/24072. 帰省 （Homecoming）/帰省 （Homecoming）.py
@@ -0,0 +1,23 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        A, B, C = (int(input()) for _ in range(3))
+        if A <= C < B:
+            answer = 1
+        else:
+            answer = 0
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
