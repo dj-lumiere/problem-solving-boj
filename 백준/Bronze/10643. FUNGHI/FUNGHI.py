b23e01b800ecb873eadd2c3c3a177b2bba16b604
Lumi <djminstar573@gmail.com>
2024-11-07 21:33:26 +0900

    [Bronze II] Title: FUNGHI, Time: 100 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/10643. FUNGHI/FUNGHI.py
@@ -0,0 +1,25 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        slices = [int(input()) for _ in range(8)]
+        max_sum = 0
+        for i in range(8):
+            current_sum = slices[i] + slices[(i + 1) % 8] + slices[(i + 2) % 8] + slices[(i + 3) % 8]
+            if current_sum > max_sum:
+                max_sum = current_sum
+        answer = max_sum
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
