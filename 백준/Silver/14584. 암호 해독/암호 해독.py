d2c5b3954a37a54c1e256abc147ade95dbaa697a
Lumi <djminstar573@gmail.com>
2024-11-07 21:34:28 +0900

    [Silver IV] Title: 암호 해독, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/14584. 암호 해독/암호 해독.py
@@ -0,0 +1,30 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        cipher = input()
+        N = int(input())
+        dictionary = [input() for _ in range(N)]
+        for shift in range(26):
+            decoded = ''.join([chr((ord(c) - ord('a') - shift) % 26 + ord('a')) for c in cipher])
+            for word in dictionary:
+                if word in decoded:
+                    answer = decoded
+                    break
+            else:
+                continue
+            break
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
