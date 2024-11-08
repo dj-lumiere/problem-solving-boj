a3606dc84c87c071903f50b37ea457b9faab74a9
Lumi <djminstar573@gmail.com>
2024-11-08 09:36:18 +0900

    [Silver I] Title: 완전 이진 트리, Time: 92 ms, Memory: 109108 KB -BaekjoonHub

--- 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
+++ 백준/Silver/9934. 완전 이진 트리/완전 이진 트리.py
@@ -1,47 +1,19 @@
-from bisect import bisect_left, bisect_right
-from string import ascii_uppercase, ascii_lowercase
-from time import sleep
-from datetime import datetime, timedelta
-from sys import setrecursionlimit, stdout, stderr
-from os import write
-from random import randint, shuffle
-from collections import deque, Counter
-from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
-from heapq import heappush, heappop
-from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
-from decimal import Decimal, getcontext
-from fractions import Fraction
-from functools import lru_cache, reduce
-import re
+# 9934 완전 이진 트리
 
-getcontext().prec = 1000
+K = int(input())
+K -= 1
+A = list(map(int, input().split(" ")))
+levels = [[] for i in range(K + 1)]
 
-with open(0, 'rb') as f:
-    tokens = iter(f.read().split())
-    input = lambda: next(tokens, None)
-    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
-    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
-    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
-    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
-    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
-    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
-    INF = 10 ** 18
-    MOD = 1_000_000_000
-    answers = []
-    t = 1
-    for hh in range(t):
-        K = int(input())
-        A = [int(input()) for _ in range(2 ** K - 1)]
-        levels = [[] for i in range(K)]
-        stack = [(K - 1, 2 ** (K - 1) - 1)]
-        while stack:
-            level, offset = stack.pop()
-            levels[level].append(A[offset])
-            if level == 0:
-                continue
-            stack.append((level - 1, offset + 2 ** (level - 1)))
-            stack.append((level - 1, offset - 2 ** (level - 1)))
-        levels.reverse()
-        answer = "\n".join(" ".join(map(str, v)) for v in levels)
-        answers.append(f"{answer}")
-    print(*answers)
+def height_traverse(level: int, offset: int):
+    if level == 0:
+        levels[level].append(A[offset])
+        return
+    else:
+        levels[level].append(A[offset])
+        height_traverse(level-1, offset - 2**(level-1))
+        height_traverse(level-1, offset + 2**(level-1))
+height_traverse(K, 2**K - 1)
+levels.reverse()
+for i in range(K + 1):
+    print(*levels[i], sep=' ')
