0587ee88eba3be0614fb6ed96de21941a921e89f
Lumi <djminstar573@gmail.com>
2024-11-09 11:01:00 +0900

    [Bronze II] Title: Stand on Zanzibar, Time: 132 ms, Memory: 113688 KB -BaekjoonHub

---
+++ 백준/Bronze/11636. Stand on Zanzibar/Stand on Zanzibar.py
@@ -0,0 +1,34 @@
+from collections import Counter
+from math import gcd
+from random import randint
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        sequence = []
+        while True:
+            num = int(input())
+            if num == 0:
+                break
+            sequence.append(num)
+        prev = 1
+        import_count = 0
+        for current in sequence:
+            if current > 2 * prev:
+                import_count += current - (2 * prev)
+            prev = current
+        answer = import_count
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
