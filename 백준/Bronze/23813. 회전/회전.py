186e3599dfd47a86bf46aaf1475131ff96fe1cec
Lumi <djminstar573@gmail.com>
2024-11-09 11:04:48 +0900

    [Bronze II] Title: 회전, Time: 244 ms, Memory: 126200 KB -BaekjoonHub

---
+++ 백준/Bronze/23813. 회전/회전.py
@@ -0,0 +1,36 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        N = input()
+        original = N
+        seen = set()
+        total = 0
+        while True:
+            if N in seen:
+                break
+            seen.add(N)
+            total += int(N)
+            if len(N) ==1:
+                break
+            N = N[-1] + N[:-1]
+            if N == original:
+                break
+        answer = total
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
