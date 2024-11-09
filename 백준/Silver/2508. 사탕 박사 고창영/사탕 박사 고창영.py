266834e1c8b1153a813dc9150ccb89bd304f2e29
Lumi <djminstar573@gmail.com>
2024-11-09 11:01:43 +0900

    [Silver V] Title: 사탕 박사 고창영, Time: 264 ms, Memory: 130432 KB -BaekjoonHub

---
+++ 백준/Silver/2508. 사탕 박사 고창영/사탕 박사 고창영.py
@@ -0,0 +1,30 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+getcontext().prec = 30
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        r, c = (int(input()) for _ in range(2))
+        grid = [input() for _ in range(r)]
+        count = 0
+        for i in range(r):
+            for j in range(c):
+                if j + 2 < c and grid[i][j] == '>' and grid[i][j+1] == 'o' and grid[i][j+2] == '<':
+                    count +=1
+                if i +2 < r and grid[i][j] == 'v' and grid[i+1][j] == 'o' and grid[i+2][j] == '^':
+                    count +=1
+        answer = count
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
