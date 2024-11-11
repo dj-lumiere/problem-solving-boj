31525bdcc1f65f1f0a0ad9220082d7fa8a57a970
Lumi <djminstar573@gmail.com>
2024-11-11 20:49:53 +0900

    [Bronze IV] Title: Случай с игрой, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/29267. Случай с игрой/Случай с игрой.py
@@ -0,0 +1,29 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        n, k = int(input()), int(input())
+        bullets = 0
+        last_save = None
+        for _ in range(n):
+            action = input()
+            if action == 'save':
+                last_save = bullets
+            elif action == 'load':
+                bullets = last_save if last_save is not None else 0
+            elif action == 'shoot':
+                bullets -= 1
+            elif action == 'ammo':
+                bullets += k
+            answers.append(f"{bullets}")
+    print(*answers, sep="\n")
