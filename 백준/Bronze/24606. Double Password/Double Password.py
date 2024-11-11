2ca4a76547af4d0e8ec2ba8d6a76f68ceb07af86
Lumi <djminstar573@gmail.com>
2024-11-11 19:48:43 +0900

    [Bronze III] Title: Double Password, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/24606. Double Password/Double Password.py
@@ -0,0 +1,23 @@
+from itertools import combinations
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        s1, s2 = (input() for _ in range(2))
+        total = 1
+        for i in range(4):
+            choices = {s1[i], s2[i]}
+            total *= len(choices)
+        answer = total
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
