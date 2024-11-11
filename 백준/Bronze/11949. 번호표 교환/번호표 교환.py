1c11c5b151ae3f6eab9a346e57b87c6a9578f74b
Lumi <djminstar573@gmail.com>
2024-11-11 21:09:22 +0900

    [Bronze II] Title: 번호표 교환, Time: 100 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/11949. 번호표 교환/번호표 교환.py
@@ -0,0 +1,23 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        n, m = int(input()), int(input())
+        A = [int(input()) for _ in range(n)]
+        for i in range(1, m + 1):
+            for j in range(n - 1):
+                if A[j] % i > A[j + 1] % i:
+                    A[j], A[j + 1] = A[j + 1], A[j]
+        answer = '\n'.join(map(str, A))
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
