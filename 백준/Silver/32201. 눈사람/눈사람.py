ace67215e2b2fa2583b2f64d87be630e6f48d321
Lumi <djminstar573@gmail.com>
2024-11-13 13:00:18 +0900

    [Silver III] Title: 눈사람, Time: 424 ms, Memory: 205108 KB -BaekjoonHub

---
+++ 백준/Silver/32201. 눈사람/눈사람.py
@@ -0,0 +1,46 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        N = int(input())
+        A = [int(input()) for _ in range(N)]
+        B = [int(input()) for _ in range(N)]
+        pos_A = {a: i + 1 for i, a in enumerate(A)}
+        pos_B = {b: i + 1 for i, b in enumerate(B)}
+        improvements = {}
+        max_improve = -INF
+        for b in B:
+            improve = pos_A[b] - pos_B[b]
+            improvements[b] = improve
+            if improve > max_improve:
+                max_improve = improve
+        winners = [str(b) for b in B if improvements[b] == max_improve]
+        answer = ' '.join(winners)
+        answers.append(f"{answer}")
+    print(*answers)
