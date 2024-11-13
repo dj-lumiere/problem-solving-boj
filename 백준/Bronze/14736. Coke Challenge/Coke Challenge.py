6407c3a29748b726a60aad1b16e0c7c1959ab055
Lumi <djminstar573@gmail.com>
2024-11-13 12:59:06 +0900

    [Bronze I] Title: Coke Challenge, Time: 288 ms, Memory: 137372 KB -BaekjoonHub

---
+++ 백준/Bronze/14736. Coke Challenge/Coke Challenge.py
@@ -0,0 +1,44 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        N, K, A = (int(input()) for _ in range(3))
+        pattern = [tuple(int(input()) for _ in range(2)) for _ in range(N)]
+        min_time = INF
+        for ti, si in pattern:
+            full_cycles = K // (ti * A)
+            if K % (ti * A) == 0:
+                time = full_cycles * (ti + si) - si
+            else:
+                time = full_cycles * (ti + si) + (K % (ti * A)) // A
+            if time < min_time:
+                min_time = time
+        answer = f"{min_time}"
+        answers.append(f"{answer}")
+    print(*answers)
