3ca35d9f658b3fcaaa5610129841828b001d3785
Lumi <djminstar573@gmail.com>
2024-11-15 16:19:18 +0900

    [Bronze III] Title: Roller Coaster, Time: 316 ms, Memory: 156104 KB -BaekjoonHub

---
+++ 백준/Bronze/30366. Roller Coaster/Roller Coaster.py
@@ -0,0 +1,47 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        m = int(input())
+        a = [int(input()) for _ in range(n)]
+        current_time = 0
+        current_load = 0
+        answer_list = []
+        for ai in a:
+            if current_load + ai <= m:
+                answer_list.append(str(current_time))
+                current_load += ai
+            else:
+                current_time += 1
+                answer_list.append(str(current_time))
+                current_load = ai
+        answer = "\n".join(answer_list)
+        answers.append(f"{answer}")
+    print(*answers)
