6cd1789fa27c204a31d41afdb91b37fc5b836764
Lumi <djminstar573@gmail.com>
2024-11-16 12:13:37 +0900

    [Bronze IV] Title: 당구 좀 치자 제발, Time: 116 ms, Memory: 125812 KB -BaekjoonHub

---
+++ 백준/Bronze/32642. 당구 좀 치자 제발/당구 좀 치자 제발.py
@@ -0,0 +1,27 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        sum_angry = 0
+        anger = 0
+        for _ in range(n):
+            w = int(input())
+            if w == 1:
+                anger += 1
+            else:
+                anger -= 1
+            sum_angry += anger
+        answer = sum_angry
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
