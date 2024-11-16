c252be5c2657c898ac7d15e846b0384ff605190f
Lumi <djminstar573@gmail.com>
2024-11-16 12:12:43 +0900

    [Bronze II] Title: 태보태보 총난타, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/17249. 태보태보 총난타/태보태보 총난타.py
@@ -0,0 +1,23 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        s = input()
+        pos = s.find('(^0^)')
+        left = s[:pos]
+        right = s[pos + 5:]
+        count_left = left.count("@")
+        count_right = right.count("@")
+        answer = f"{count_left} {count_right}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
