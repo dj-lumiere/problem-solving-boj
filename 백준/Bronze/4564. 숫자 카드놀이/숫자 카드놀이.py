75160caac96eb0debe51336993b3693c2e53cfba
Lumi <djminstar573@gmail.com>
2024-11-18 13:07:44 +0900

    [Bronze II] Title: 숫자 카드놀이, Time: 268 ms, Memory: 137256 KB -BaekjoonHub

---
+++ 백준/Bronze/4564. 숫자 카드놀이/숫자 카드놀이.py
@@ -0,0 +1,45 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = INF
+    answers = []
+    for hh in range(t):
+        S = int(input())
+        if S == 0:
+            break
+        sequence = [str(S)]
+        while S >= 10:
+            product = 1
+            temp = S
+            while temp > 0:
+                product *= temp % 10
+                temp //= 10
+            sequence.append(str(product))
+            S = product
+        answer = ' '.join(sequence)
+        answers.append(f"{answer}")
+    print(*answers)
