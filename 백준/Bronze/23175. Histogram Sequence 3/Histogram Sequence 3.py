d6a77ef9c9b8169ff60884e9971f3232ae79d461
Lumi <djminstar573@gmail.com>
2024-11-18 13:07:17 +0900

    [Bronze II] Title: Histogram Sequence 3, Time: 480 ms, Memory: 262048 KB -BaekjoonHub

---
+++ 백준/Bronze/23175. Histogram Sequence 3/Histogram Sequence 3.py
@@ -0,0 +1,45 @@
+from bisect import bisect_left, bisect_right
+from string import ascii_uppercase, ascii_lowercase
+from time import sleep
+from datetime import datetime, timedelta
+from sys import setrecursionlimit, stdout, stderr
+from os import write
+from random import randint, shuffle
+from collections import deque, Counter
+from math import log, gcd, floor, log2, log10, pi, ceil, factorial, sqrt
+from heapq import heappush, heappop
+from itertools import combinations, permutations, combinations_with_replacement, product, zip_longest
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from functools import lru_cache, reduce
+import re
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        m = int(input())
+        b = [int(input()) for _ in range(m)]
+        histogram_sequence = []
+        index = 0
+        while index < m:
+            current_height = b[index]
+            max_something = index + current_height
+            count = 0
+            while index < min(m, max_something) and b[index] == current_height:
+                count += 1
+                index += 1
+            histogram_sequence.append(current_height)
+        answer = ' '.join(str(x) for x in histogram_sequence)
+        answers.append(f"{answer}")
+    print(*answers)
