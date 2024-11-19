fdc33011fb83b0db8033f223ba831c8d9c2fe1d8
Lumi <djminstar573@gmail.com>
2024-11-19 22:01:02 +0900

    [Silver V] Title: Q-인덱스, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/13333. Q－인덱스/Q－인덱스.py
@@ -0,0 +1,26 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        citations = [int(input()) for _ in range(n)]
+        citations.sort(reverse=True)
+        q_index = 0
+        for i in range(n):
+            if citations[i] >= i + 1:
+                q_index = i + 1
+            else:
+                break
+        answer = q_index
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
