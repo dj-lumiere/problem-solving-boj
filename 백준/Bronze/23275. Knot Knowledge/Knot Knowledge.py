df9ffe0d17d2359c40d0588cd3efa09199aae0db
Lumi <djminstar573@gmail.com>
2024-11-20 19:40:35 +0900

    [Bronze II] Title: Knot Knowledge, Time: 236 ms, Memory: 128892 KB -BaekjoonHub

---
+++ 백준/Bronze/23275. Knot Knowledge/Knot Knowledge.py
@@ -0,0 +1,24 @@
+from bisect import bisect_right
+from collections import Counter
+from decimal import Decimal
+from itertools import product
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        a = [int(input()) for _ in range(n)]
+        b = [int(input()) for _ in range(n - 1)]
+        answer = f"{sum(a) - sum(b)}"
+        answers.append(answer)
+    print(*answers, sep="\n")
