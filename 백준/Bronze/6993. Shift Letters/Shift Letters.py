f3be8fe4afae0767bd1bfba6b4830e0a60e01ef8
Lumi <djminstar573@gmail.com>
2024-11-20 19:34:36 +0900

    [Bronze II] Title: Shift Letters, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/6993. Shift Letters/Shift Letters.py
@@ -0,0 +1,19 @@
+from itertools import product
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        s, n = input(), int(input())
+        answer = s[-n:] + s[:-n]
+        answers.append(f"Shifting {s} by {n} positions gives us: {answer}")
+    print(*answers, sep="\n")
