ea0bf2ae13f032cb7d99eaacb5db2d3af5d70506
Lumi <djminstar573@gmail.com>
2024-11-22 00:24:01 +0900

    [Bronze III] Title: POT, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/11772. POT/POT.py
@@ -0,0 +1,24 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        total = 0
+        for _ in range(n):
+            pi = int(input())
+            number = pi // 10
+            pot = pi % 10
+            total += number ** pot
+        answer = total
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
