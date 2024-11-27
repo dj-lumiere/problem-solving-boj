24a814ad833bb8f2fda0b5a8bc16c0bc9ad39e22
Lumi <djminstar573@gmail.com>
2024-11-28 00:46:05 +0900

    [Bronze III] Title: Szachy, Time: 240 ms, Memory: 127412 KB -BaekjoonHub

---
+++ 백준/Bronze/8725. Szachy/Szachy.py
@@ -0,0 +1,25 @@
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().splitlines())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        answer = 0
+        for _ in range(n):
+            row = list(map(int, input().split()))
+            answer += max(*row, 0)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
