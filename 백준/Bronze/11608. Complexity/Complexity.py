247fb4cb214ff0373095c4ee569fc4d87fe9d9c1
Lumi <djminstar573@gmail.com>
2024-11-28 00:37:23 +0900

    [Bronze II] Title: Complexity, Time: 236 ms, Memory: 128396 KB -BaekjoonHub

---
+++ 백준/Bronze/11608. Complexity/Complexity.py
@@ -0,0 +1,23 @@
+from collections import Counter
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        s = Counter(input())
+        answer = sum(sorted(s.values(), reverse=True)[2:])
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
