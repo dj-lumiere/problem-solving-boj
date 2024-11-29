653224c54c7c62bdc90f764f6b8d970a1a4a9c83
Lumi <djminstar573@gmail.com>
2024-11-29 18:46:49 +0900

    [Silver IV] Title: 카드, Time: 260 ms, Memory: 151652 KB -BaekjoonHub

---
+++ 백준/Silver/11652. 카드/카드.py
@@ -0,0 +1,25 @@
+from decimal import Decimal
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        N = int(input())
+        freq = {}
+        for _ in range(N):
+            num = int(input())
+            freq[num] = freq.get(num, 0) + 1
+        max_freq = max(freq.values())
+        candidates = [num for num, count in freq.items() if count == max_freq]
+        answer = f"{min(candidates)}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
