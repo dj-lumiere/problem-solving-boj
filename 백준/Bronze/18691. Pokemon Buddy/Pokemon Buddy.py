b9c7e9bb93f5696c176aa17a21ff03a9a7b4f107
Lumi <djminstar573@gmail.com>
2024-11-29 18:58:02 +0900

    [Bronze IV] Title: Pokemon Buddy, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/18691. Pokemon Buddy/Pokemon Buddy.py
@@ -0,0 +1,24 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        G, C, E = int(input()), int(input()), int(input())
+        if C >= E:
+            km = 0
+        else:
+            D = {1: 1, 2: 3, 3: 5}[G]
+            needed = E - C
+            km = needed * D
+        answer = f"{km}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
