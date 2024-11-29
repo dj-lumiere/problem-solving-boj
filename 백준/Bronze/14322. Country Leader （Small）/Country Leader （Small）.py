8b33da4179023d01eadd078603bf61cf5780f233
Lumi <djminstar573@gmail.com>
2024-11-29 19:04:11 +0900

    [Bronze II] Title: Country Leader (Small), Time: 104 ms, Memory: 110920 KB -BaekjoonHub

---
+++ 백준/Bronze/14322. Country Leader （Small）/Country Leader （Small）.py
@@ -0,0 +1,20 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().splitlines())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        words = [input() for _ in range(n)]
+        words.sort(key=lambda w: (-len(set(w)), w))
+        answer = words[0]
+        answers.append(f"Case #{hh + 1}: {answer}")
+    print(*answers, sep="\n")
