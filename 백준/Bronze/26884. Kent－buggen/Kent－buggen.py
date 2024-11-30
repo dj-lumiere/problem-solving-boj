1dde8056694969abd8f618473d507df393705f9d
Lumi <djminstar573@gmail.com>
2024-11-30 16:43:22 +0900

    [Bronze I] Title: Kent-buggen, Time: 164 ms, Memory: 140372 KB -BaekjoonHub

---
+++ 백준/Bronze/26884. Kent－buggen/Kent－buggen.py
@@ -0,0 +1,20 @@
+from collections import Counter
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        artists = Counter(input() for _ in range(n))
+        answer = sum(1 for v in artists.values() if v >= 2)
+        answers.append(answer)
+    print(*answers, sep="\n")
---
+++ 백준/Bronze/26884. Kent－buggen/README.md
@@ -0,0 +1,28 @@
+# [Bronze I] Kent-buggen - 26884 
+
+[문제 링크](https://www.acmicpc.net/problem/26884) 
+
+### 성능 요약
+
+메모리: 140372 KB, 시간: 164 ms
+
+### 분류
+
+자료 구조, 해시를 사용한 집합과 맵, 문자열
+
+### 제출 일자
+
+2024년 11월 30일 16:43:11
+
+### 문제 설명
+
+<p>I Spotify kan det uppstå problem om flera artister har samma namn. Givet en lista på artister, avgör hur många artistnamn som drabbas av detta problem.</p>
+
+### 입력 
+
+ <p>Första raden består av ett heltal $n$ ($1 \leq n \leq 10^5$) - antalet artister. Sedan följer $n$ rader med ett artistnamn på varje rad. Artistnamn är strängar bestående av små bokstäver (a-z) med som mest 20 tecken.</p>
+
+### 출력 
+
+ <p>Skriv ut ett heltal $m$ - antalet olika artistnamn som förekommer mer än en gång.</p>
+
