63e9e4dfe53cfdf00270609213f2fb6e7d00a655
Lumi <djminstar573@gmail.com>
2024-12-02 14:48:55 +0900

    [Bronze III] Title: Tarifa, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/13484. Tarifa/Tarifa.py
@@ -0,0 +1,20 @@
+from sys import stderr, stdout
+
+with open(0, "r", encoding="UTF-8") as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    available = 0
+    for _ in range(t):
+        x, n = int(input()), int(input())
+        p = [int(input()) for _ in range(n)]
+        answer = x * n - sum(p) + x
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
