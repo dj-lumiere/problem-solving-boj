309d00d15a4feeec829d951a27cbc335857b61ca
Lumi <djminstar573@gmail.com>
2024-12-02 15:12:10 +0900

    [Silver IV] Title: 수 이어 쓰기 1, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/1748. 수 이어 쓰기 1/수 이어 쓰기 1.py
@@ -0,0 +1,27 @@
+from sys import stderr, stdout
+
+with open(0, "r", encoding="UTF-8") as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for _ in range(t):
+        N = int(input())
+        total_digits = 0
+        length = 1
+        start = 1
+        while start <= N:
+            end = min(N, 10 ** length - 1)
+            count = end - start + 1
+            total_digits += count * length
+            start *= 10
+            length += 1
+        answer = total_digits
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
