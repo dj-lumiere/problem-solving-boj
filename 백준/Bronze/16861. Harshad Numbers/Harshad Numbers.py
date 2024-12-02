56f1a238778bfaa3a698a2958f578dbb3c1ebd54
Lumi <djminstar573@gmail.com>
2024-12-02 14:52:08 +0900

    [Bronze III] Title: Harshad Numbers, Time: 88 ms, Memory: 109240 KB -BaekjoonHub

---
+++ 백준/Bronze/16861. Harshad Numbers/Harshad Numbers.py
@@ -0,0 +1,29 @@
+from sys import stderr, stdout
+
+with open(0, "r", encoding="UTF-8") as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    available = 0
+    for _ in range(t):
+        n = int(input())
+        current = n
+        while True:
+            s = 0
+            temp = current
+            while temp > 0:
+                s += temp % 10
+                temp //= 10
+            if s != 0 and current % s == 0:
+                answer = current
+                break
+            current += 1
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
