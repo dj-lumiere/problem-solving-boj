1380dae665d143f28d1a5c8d891b98d92618b553
Lumi <djminstar573@gmail.com>
2024-12-03 13:37:12 +0900

    [Silver V] Title: 과일노리, Time: 128 ms, Memory: 121468 KB -BaekjoonHub

---
+++ 백준/Silver/14493. 과일노리/과일노리.py
@@ -0,0 +1,25 @@
+from sys import stderr, stdout
+
+with open(0, "r", encoding="UTF-8") as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        sections = [tuple(int(input()) for _ in range(2)) for _ in range(n)]
+        time = 0
+        for a, b in sections:
+            mod = time % (a + b)
+            if mod < b:
+                time += b - mod
+            time += 1
+        answer = f"{time}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
