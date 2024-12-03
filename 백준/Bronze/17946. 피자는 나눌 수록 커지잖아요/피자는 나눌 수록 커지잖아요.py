cd938580fb72c5aaaa3f022778c038914ec13ad4
Lumi <djminstar573@gmail.com>
2024-12-03 13:43:36 +0900

    [Bronze II] Title: 피자는 나눌 수록 커지잖아요, Time: 104 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/17946. 피자는 나눌 수록 커지잖아요/피자는 나눌 수록 커지잖아요.py
@@ -0,0 +1,21 @@
+from sys import stderr, stdout
+
+with open(0, "r", encoding="UTF-8") as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, -1), (1, 0), (0, 1), (-1, 0)]
+    INF = 10 ** 18
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        K = int(input())
+        slices_made = (K * K + K + 2) // 2
+        slices_given = K * (K + 1) // 2
+        slices_eat = slices_made - slices_given
+        answer = f"{slices_eat}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
