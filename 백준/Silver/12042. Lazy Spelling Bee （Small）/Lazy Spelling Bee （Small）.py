311ecd8ffa44f690c0e80e5f656c9dbd1358c1d4
Lumi <djminstar573@gmail.com>
2024-12-04 23:17:43 +0900

    [Silver IV] Title: Lazy Spelling Bee (Small), Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/12042. Lazy Spelling Bee （Small）/Lazy Spelling Bee （Small）.py
@@ -0,0 +1,32 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(1, t + 1):
+        W = input()
+        n = len(W)
+        total = 1
+        for i in range(n):
+            options = set()
+            options.add(W[i])
+            if i > 0:
+                options.add(W[i - 1])
+            if i < n - 1:
+                options.add(W[i + 1])
+            total = (total * len(options)) % MOD
+        answer = f"Case #{hh}: {total}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
