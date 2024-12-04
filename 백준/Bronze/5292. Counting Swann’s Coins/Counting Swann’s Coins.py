a8ab1ecb4542246962b80b7c63c7882273c0a457
Lumi <djminstar573@gmail.com>
2024-12-04 23:24:20 +0900

    [Bronze III] Title: Counting Swann’s Coins, Time: 92 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Bronze/5292. Counting Swann’s Coins/Counting Swann’s Coins.py
@@ -0,0 +1,33 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        N = int(input())
+        outputs = []
+        for i in range(1, N + 1):
+            if i % 15 == 0:
+                output = "DeadMan\n"
+            elif i % 3 == 0:
+                output = "Dead\n"
+            elif i % 5 == 0:
+                output = "Man\n"
+            else:
+                output = f"{i} "
+            outputs.append(output)
+        answer = "".join(outputs)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
