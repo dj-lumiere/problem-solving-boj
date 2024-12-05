f4186028fb113f7fb1db3c177cba8e8d598f139e
Lumi <djminstar573@gmail.com>
2024-12-05 17:25:28 +0900

    [Silver V] Title: Date, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

---
+++ 백준/Silver/26576. Date/Date.py
@@ -0,0 +1,42 @@
+from sys import stderr, stdout
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        for _ in range(n):
+            month = input()
+            day = input().replace(',', '')
+            year = input()
+            months = {'January': '01', 'February': '02', 'March': '03', 'April': '04', 'May': '05', 'June': '06', 'July': '07', 'August': '08', 'September': '09', 'October': '10', 'November': '11', 'December': '12'}
+            if month not in months or not (1 <= int(day) <= 31):
+                answer = "Invalid"
+            else:
+                mm = months[month]
+                dd = day.zfill(2)
+                yy = str(int(year) % 100).zfill(2)
+                answer = f"{mm}/{dd}/{yy}"
+            answers.append(f"{answer}")
+    print(*answers, sep="\n")
