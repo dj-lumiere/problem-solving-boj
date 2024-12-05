6ae8f070710b9e0253dfb77f7eb138830b6ab9d5
Lumi <djminstar573@gmail.com>
2024-12-05 17:23:28 +0900

    [Bronze II] Title: Carousel Rides, Time: 256 ms, Memory: 128400 KB -BaekjoonHub

---
+++ 백준/Bronze/24770. Carousel Rides/Carousel Rides.py
@@ -0,0 +1,46 @@
+from fractions import Fraction
+from sys import stderr, stdout
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = INF
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        m = int(input())
+        if n == m == 0:
+            break
+        offers = []
+        for _ in range(n):
+            a = int(input())
+            b = int(input())
+            if a <= m:
+                offers.append((a, b))
+        if not offers:
+            answer = "No suitable tickets offered"
+        else:
+            offers.sort(key=lambda x: (-Fraction(x[0], x[1]), -x[0]))
+            best = offers[0]
+            answer = f"Buy {best[0]} tickets for ${best[1]}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
