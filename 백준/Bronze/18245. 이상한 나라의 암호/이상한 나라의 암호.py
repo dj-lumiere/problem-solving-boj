6323f66964217d836646321da0a6e94875e368b4
Lumi <djminstar573@gmail.com>
2024-12-05 17:12:13 +0900

    [Bronze II] Title: 이상한 나라의 암호, Time: 112 ms, Memory: 113160 KB -BaekjoonHub

---
+++ 백준/Bronze/18245. 이상한 나라의 암호/이상한 나라의 암호.py
@@ -0,0 +1,25 @@
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().splitlines())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = INF
+    answers = []
+    for hh in range(t):
+        line = input()
+        if line == "Was it a cat I saw?":
+            break
+        step = hh + 1
+        answer = ''.join([line[j] for j in range(0, len(line), step + 1)])
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
