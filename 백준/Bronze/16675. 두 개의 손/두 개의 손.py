f38034c7f5751b733e3a4ef0066f2f010622c1f5
Lumi <djminstar573@gmail.com>
2024-12-08 23:26:45 +0900

    [Bronze II] Title: 두 개의 손, Time: 92 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/16675. 두 개의 손/두 개의 손.py
@@ -0,0 +1,37 @@
+from sys import stderr, stdout
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        ML, MR, TL, TR = (input() for _ in range(4))
+        wins_over = {'S': 'P', 'R': 'S', 'P': 'R'}
+        if (wins_over[ML] == TL and wins_over[ML] == TR) or (wins_over[MR] == TL and wins_over[MR] == TR):
+            answer = 'MS'
+        elif (wins_over[TL] == ML and wins_over[TL] == MR) or (wins_over[TR] == ML and wins_over[TR] == MR):
+            answer = 'TK'
+        else:
+            answer = '?'
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
