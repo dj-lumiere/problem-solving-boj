7c925e502fe5686f74c8f2d991e4d90c45f5a2a2
Lumi <djminstar573@gmail.com>
2024-12-08 23:35:24 +0900

    [Silver V] Title: Find the House, Time: 244 ms, Memory: 128816 KB -BaekjoonHub

---
+++ 백준/Silver/23568. Find the House/Find the House.py
@@ -0,0 +1,45 @@
+from fractions import Fraction
+from sys import stderr, stdout
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for _ in range(t):
+        n = int(input())
+        triples = {}
+        for _ in range(n):
+            i = int(input())
+            j = input()
+            k = int(input())
+            triples[i] = (j, k)
+        current_pos = int(input())
+        for _ in range(n):
+            j, k = triples[current_pos]
+            if j == 'L':
+                current_pos -= k
+            else:
+                current_pos += k
+        answer = current_pos
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
