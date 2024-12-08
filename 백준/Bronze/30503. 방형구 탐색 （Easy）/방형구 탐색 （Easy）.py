a3ba33a337293de0fcbe89fdea715c822c871ba6
Lumi <djminstar573@gmail.com>
2024-12-08 23:20:38 +0900

    [Bronze I] Title: 방형구 탐색 (Easy), Time: 144 ms, Memory: 118108 KB -BaekjoonHub

---
+++ 백준/Bronze/30503. 방형구 탐색 （Easy）/방형구 탐색 （Easy）.py
@@ -0,0 +1,49 @@
+from collections import deque
+from sys import stderr, stdout
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        N = int(input())
+        A = [int(input()) for _ in range(N)]
+        Q = int(input())
+        for _ in range(Q):
+            a = int(input())
+            if a == 1:
+                l = int(int(input()))
+                r = int(int(input()))
+                k = int(int(input()))
+                count = 0
+                for i in range(l - 1, r):
+                    if A[i] == k:
+                        count += 1
+                answer = count
+                answers.append(f"{answer}")
+            elif a == 2:
+                l = int(int(input()))
+                r = int(int(input()))
+                for i in range(l - 1, r):
+                    A[i] = -1
+    print(*answers, sep="\n")
