1d24a072a69a18ae89135ccd21d73bf93650448a
Lumi <djminstar573@gmail.com>
2024-12-10 23:45:53 +0900

    [Bronze I] Title: Stack 'em Up, Time: 228 ms, Memory: 128820 KB -BaekjoonHub

---
+++ 백준/Bronze/4365. Stack 'em Up/Stack 'em Up.py
@@ -0,0 +1,44 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        shuffles = [[int(input()) for _ in range(52)] for _ in range(n)]
+        values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King', 'Ace']
+        suits = ['Clubs', 'Diamonds', 'Hearts', 'Spades']
+        deck = [f"{value} of {suit}" for suit in suits for value in values]
+        while True:
+            k = input()
+            if k is None:
+                break
+            k = int(k)
+            shuffle = shuffles[k - 1]
+            deck = [deck[shuffle[j] - 1] for j in range(52)]
+            answer = '\n'.join(deck)
+            answers.append(f"{answer}")
+            answers.append("")
+    print(*answers, sep="\n")
