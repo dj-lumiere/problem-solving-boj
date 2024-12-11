d2c98b4ec35e504153b77aa1e3282b847e4e8277
Lumi <djminstar573@gmail.com>
2024-12-11 09:29:30 +0900

    [Bronze II] Title: 애너그램 거리, Time: 304 ms, Memory: 149004 KB -BaekjoonHub

---
+++ 백준/Bronze/3778. 애너그램 거리/애너그램 거리.py
@@ -0,0 +1,42 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().splitlines())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        word1 = input()
+        word2 = input()
+        count1 = [0] * 26
+        count2 = [0] * 26
+        for c in word1:
+            count1[ord(c) - 97] += 1
+        for c in word2:
+            count2[ord(c) - 97] += 1
+        distance = 0
+        for i in range(26):
+            distance += count1[i] + count2[i] - 2 * min(count1[i], count2[i])
+        answer = f"Case #{hh + 1}: {distance}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
