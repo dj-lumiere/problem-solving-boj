af15c70290a89fae44f51bdbe3f12483885083ff
Lumi <djminstar573@gmail.com>
2024-12-12 17:24:46 +0900

    [Bronze III] Title: 회전하지 않는 캘리퍼스, Time: 248 ms, Memory: 127628 KB -BaekjoonHub

---
+++ 백준/Bronze/30394. 회전하지 않는 캘리퍼스/회전하지 않는 캘리퍼스.py
@@ -0,0 +1,29 @@
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        max_y = -INF
+        min_y = INF
+        for _ in range(n):
+            x = int(input())
+            y = int(input())
+            max_y = max(max_y, y)
+            min_y = min(min_y, y)
+        answer = max_y - min_y
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
