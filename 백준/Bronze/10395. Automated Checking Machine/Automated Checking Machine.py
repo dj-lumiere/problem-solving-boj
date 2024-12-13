07fe97935bd4b037462e1af3c131481a0bbaa729
Lumi <djminstar573@gmail.com>
2024-12-13 15:28:34 +0900

    [Bronze III] Title: Automated Checking Machine, Time: 228 ms, Memory: 127508 KB -BaekjoonHub

---
+++ 백준/Bronze/10395. Automated Checking Machine/Automated Checking Machine.py
@@ -0,0 +1,25 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        connector1 = [int(input()) for _ in range(5)]
+        connector2 = [int(input()) for _ in range(5)]
+        compat = all(a != b for a, b in zip(connector1, connector2))
+        answer = 'Y' if compat else 'N'
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
