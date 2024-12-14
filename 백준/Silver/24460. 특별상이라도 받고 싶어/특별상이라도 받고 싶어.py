e370f951b952a73acf3b1944c2056cb4b2de1dda
Lumi <djminstar573@gmail.com>
2024-12-14 22:48:57 +0900

    [Silver III] Title: 특별상이라도 받고 싶어, Time: 412 ms, Memory: 217056 KB -BaekjoonHub

---
+++ 백준/Silver/24460. 특별상이라도 받고 싶어/특별상이라도 받고 싶어.py
@@ -0,0 +1,50 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        N = int(input())
+        grid = []
+        for _ in range(N):
+            row = []
+            for _ in range(N):
+                row.append(int(input()))
+            grid.append(row)
+
+
+        def select(x, y, size):
+            if size == 1:
+                return grid[x][y]
+            half = size // 2
+            selections = [select(x, y, half), select(x, y + half, half), select(x + half, y, half),
+                select(x + half, y + half, half)]
+            selections.sort()
+            return selections[1]
+
+
+        answer = select(0, 0, N)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
