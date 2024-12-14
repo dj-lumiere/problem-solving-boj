0a1b98aafdf2edc228d34bf93b7caf8f2b8bfe89
Lumi <djminstar573@gmail.com>
2024-12-14 22:58:29 +0900

    [Silver IV] Title: 수열 (Easy), Time: 284 ms, Memory: 179976 KB -BaekjoonHub

---
+++ 백준/Silver/23827. 수열 （Easy）/수열 （Easy）.py
@@ -0,0 +1,37 @@
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_007
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        A = [int(input()) for _ in range(n)]
+        total = 0
+        total_sq = 0
+        for num in A:
+            total = (total + num) % MOD
+            total_sq = (total_sq + num * num) % MOD
+        answer = (total * total - total_sq) * pow(2, -1, MOD) % MOD
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
