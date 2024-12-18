becb827fb72e0db7798fdc48e05694ab365a6a8f
Lumi <djminstar573@gmail.com>
2024-12-18 15:04:47 +0900

    [Silver IV] Title: 피사노 주기, Time: 244 ms, Memory: 128288 KB -BaekjoonHub

---
+++ 백준/Silver/9471. 피사노 주기/피사노 주기.py
@@ -0,0 +1,41 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    P = int(input())
+    t = P
+    answers = []
+    for _ in range(t):
+        n, m = int(input()), int(input())
+        a, b = 0, 1
+        for i in range(1, m * m + 1):
+            a, b = b, (a + b) % m
+            if a == 0 and b == 1:
+                k = i
+                break
+        else:
+            k = m * m
+        answer = f"{n} {k}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
