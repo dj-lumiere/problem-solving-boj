d784a1563395ec2393c6899ee3b805a80dae31af
Lumi <djminstar573@gmail.com>
2024-12-18 14:56:43 +0900

    [Silver V] Title: 인기 투표, Time: 248 ms, Memory: 128288 KB -BaekjoonHub

---
+++ 백준/Silver/11637. 인기 투표/인기 투표.py
@@ -0,0 +1,43 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        votes = [int(input()) for _ in range(n)]
+        total = sum(votes)
+        max_vote = max(votes)
+        count = votes.count(max_vote)
+        if count > 1:
+            answer = "no winner"
+        else:
+            winner = votes.index(max_vote) + 1
+            if max_vote > total / 2:
+                answer = f"majority winner {winner}"
+            else:
+                answer = f"minority winner {winner}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
