9dd9cc70d14f3c3b8c6f14212bfc8200080901c0
Lumi <djminstar573@gmail.com>
2024-12-19 19:01:20 +0900

    [Bronze I] Title: A Fistful of Dollars, Time: 236 ms, Memory: 129488 KB -BaekjoonHub

---
+++ 백준/Bronze/5169. A Fistful of Dollars/A Fistful of Dollars.py
@@ -0,0 +1,48 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        s, m = int(input()), int(input())
+        totals = [0] * (s + 1)
+        for _ in range(m):
+            si, pi = int(input()), int(input())
+            totals[si] += pi
+        suspect = "No suspect."
+        for i in range(1, s + 1):
+            is_suspect = True
+            for j in range(1, s + 1):
+                if j == i:
+                    continue
+                if totals[i] <= 2 * totals[j]:
+                    is_suspect = False
+                    break
+            if is_suspect:
+                suspect = str(i)
+                break
+        answer = f"Data Set {hh + 1}:\n{suspect}\n"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
