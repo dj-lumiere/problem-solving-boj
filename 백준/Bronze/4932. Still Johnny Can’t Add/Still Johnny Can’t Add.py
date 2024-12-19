e041c896ad5e9360a35e5bbf79563aa7442aeab7
Lumi <djminstar573@gmail.com>
2024-12-19 18:56:35 +0900

    [Bronze I] Title: Still Johnny Can’t Add, Time: 232 ms, Memory: 128156 KB -BaekjoonHub

---
+++ 백준/Bronze/4932. Still Johnny Can’t Add/Still Johnny Can’t Add.py
@@ -0,0 +1,43 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        N = int(input())
+        A = [[int(input()) for _ in range(N)] for _ in range(N)]
+        c = A[0]
+        r = [0] + [A[i][0] - c[0] for i in range(1, N)]
+        flag = True
+        for i in range(N):
+            for j in range(N):
+                if A[i][j] != r[i] + c[j]:
+                    flag = False
+                    break
+            if not flag:
+                break
+        answer = "YES" if flag else "NO"
+        answers.append(f"{hh + 1}. {answer}")
+    print(*answers, sep="\n")
