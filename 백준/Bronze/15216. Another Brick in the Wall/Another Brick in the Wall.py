b04f914ce11f303eacd51c1c6f06c2c32702080d
Lumi <djminstar573@gmail.com>
2024-12-20 14:02:44 +0900

    [Bronze II] Title: Another Brick in the Wall, Time: 236 ms, Memory: 128800 KB -BaekjoonHub

---
+++ 백준/Bronze/15216. Another Brick in the Wall/Another Brick in the Wall.py
@@ -0,0 +1,44 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        h, w, n = (int(input()) for _ in range(3))
+        bricks = [int(input()) for _ in range(n)]
+        layers = 0
+        current_sum = 0
+        for brick in bricks:
+            current_sum += brick
+            if current_sum == w:
+                layers += 1
+                current_sum = 0
+                if layers == h:
+                    break
+            elif current_sum > w:
+                break
+        answer = "YES" if layers == h else "NO"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
