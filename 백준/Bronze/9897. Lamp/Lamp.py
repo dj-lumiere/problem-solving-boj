7c9906cde873686a3ddec9df84b853865e6362ab
Lumi <djminstar573@gmail.com>
2024-12-20 13:53:44 +0900

    [Bronze II] Title: Lamp, Time: 236 ms, Memory: 128676 KB -BaekjoonHub

---
+++ 백준/Bronze/9897. Lamp/Lamp.py
@@ -0,0 +1,43 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        L, G, R = (int(input()) for _ in range(3))
+        guards = {}
+        for _ in range(G):
+            name = input()
+            a0 = int(input())
+            d = int(input())
+            guards[name] = [i for i in range(a0, L + 1, d)]
+        lamp = [False] * (L + 1)
+        for _ in range(R):
+            guard_name = input()
+            for lamp_num in guards[guard_name]:
+                lamp[lamp_num] = not lamp[lamp_num]
+        answer = sum(lamp)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
