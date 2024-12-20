670428a3a43ac0b7d7fcdf53b8f9b59e6f9840bc
Lumi <djminstar573@gmail.com>
2024-12-20 13:56:53 +0900

    [Silver IV] Title: 좋은 구간, Time: 240 ms, Memory: 128160 KB -BaekjoonHub

---
+++ 백준/Silver/1059. 좋은 구간/좋은 구간.py
@@ -0,0 +1,49 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        L = int(input())
+        S = [int(input()) for _ in range(L)]
+        n = int(input())
+        if n in S:
+            answer = 0
+        else:
+            S_sorted = sorted(S)
+            s_prev = 0
+            for s in S_sorted:
+                if s < n:
+                    s_prev = s
+                else:
+                    break
+            s_next = INF
+            for s in S_sorted:
+                if s > n:
+                    s_next = s
+                    break
+            answer = (n - s_prev) * (s_next - n) - 1
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
