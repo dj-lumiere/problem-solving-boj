f732c5d08d4b7d6469069b80afd6161cd89fa594
Lumi <djminstar573@gmail.com>
2024-12-23 09:46:21 +0900

    [Bronze II] Title: <<Кто хочет стать миллионером?>>, Time: 240 ms, Memory: 128196 KB -BaekjoonHub

---
+++ 백준/Bronze/19759. ＜＜Кто хочет стать миллионером？＞＞/＜＜Кто хочет стать миллионером？＞＞.py
@@ -0,0 +1,42 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        a = 100
+        answers.append(str(a))
+        for _ in range(n - 1):
+            candidate = 2 * a
+            while True:
+                s = str(candidate)
+                zeros = len(s) - len(s.rstrip('0'))
+                if zeros * 2 >= len(s):
+                    break
+                candidate += 1
+            a = candidate
+            answers.append(str(a))
+    print(*answers, sep="\n")
