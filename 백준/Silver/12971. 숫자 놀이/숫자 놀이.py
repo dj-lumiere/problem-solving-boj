9da2407c6637a8ec701062bbc2fa7136ed9609db
Lumi <djminstar573@gmail.com>
2024-12-24 09:57:43 +0900

    [Silver IV] Title: 숫자 놀이, Time: 260 ms, Memory: 127496 KB -BaekjoonHub

---
+++ 백준/Silver/12971. 숫자 놀이/숫자 놀이.py
@@ -0,0 +1,35 @@
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        p1, p2, p3, x1, x2, x3 = (int(input()) for _ in range(6))
+        answer = -1
+        for i in range(p1 * p2 * p3):
+            if i % p1 == x1 and i % p2 == x2 and i % p3 == x3:
+                answer = i
+                break
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
