666b5b3eff02ba00838c941d16f0c2e67be3a002
Lumi <djminstar573@gmail.com>
2024-12-25 21:18:54 +0900

    [Bronze II] Title: Suspicious Event, Time: 252 ms, Memory: 127496 KB -BaekjoonHub

---
+++ 백준/Bronze/27622. Suspicious Event/Suspicious Event.py
@@ -0,0 +1,44 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        events = [int(input()) for _ in range(n)]
+        logged_in = set()
+        suspicious = 0
+        for e in events:
+            if e > 0:
+                logged_in.add(e)
+            else:
+                x = -e
+                if x in logged_in:
+                    logged_in.remove(x)
+                else:
+                    suspicious += 1
+        answer = suspicious
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
