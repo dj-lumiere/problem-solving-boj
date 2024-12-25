3db1888be85fdfbd7c58892c72f38add2a008365
Lumi <djminstar573@gmail.com>
2024-12-25 17:06:36 +0900

    [Bronze III] Title: Flat, Time: 224 ms, Memory: 128156 KB -BaekjoonHub

---
+++ 백준/Bronze/3512. Flat/Flat.py
@@ -0,0 +1,46 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n = int(input())
+        c = int(input())
+        total = 0
+        bedroom = 0
+        balcony = 0
+        for _ in range(n):
+            a_i = int(input())
+            t_i = input()
+            total += a_i
+            if t_i == "bedroom":
+                bedroom += a_i
+            elif t_i == "balcony":
+                balcony += a_i
+        available = Fraction(total) - Fraction(balcony, 2)
+        price = available * c
+        answer = price.numerator / price.denominator
+        answers.append(f"{total}\n{bedroom}\n{answer}")
+    print(*answers, sep="\n")
