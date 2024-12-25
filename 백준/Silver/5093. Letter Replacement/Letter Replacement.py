60946ec500a82bc2ce08f033b667d653f4e8b237
Lumi <djminstar573@gmail.com>
2024-12-25 18:26:07 +0900

    [Silver V] Title: Letter Replacement, Time: 228 ms, Memory: 128156 KB -BaekjoonHub

---
+++ 백준/Silver/5093. Letter Replacement/Letter Replacement.py
@@ -0,0 +1,48 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    symbols = ['*', '?', '/', '+', '!']
+    t = INF
+    answers = []
+    for _ in range(t):
+        word = input()
+        if word == "#":
+            break
+        lower_word = word.lower()
+        char_count = {}
+        symbol_map = {}
+        symbol_idx = 0
+        result = list(word)
+        for i, ch in enumerate(lower_word):
+            char_count[ch] = char_count.get(ch, 0) + 1
+            if char_count[ch] == 2:
+                if symbol_idx < 5:
+                    symbol_map[ch] = symbols[symbol_idx]
+                    symbol_idx += 1
+            if char_count[ch] > 1 and ch in symbol_map:
+                result[i] = symbol_map[ch]
+        answer = ''.join(result)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
