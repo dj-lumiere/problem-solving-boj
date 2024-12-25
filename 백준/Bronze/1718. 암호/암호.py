4c5414fcf8f5ff94eb8b4b69c632a7eac18f13de
Lumi <djminstar573@gmail.com>
2024-12-25 18:20:12 +0900

    [Bronze II] Title: 암호, Time: 244 ms, Memory: 129328 KB -BaekjoonHub

---
+++ 백준/Bronze/1718. 암호/암호.py
@@ -0,0 +1,45 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().splitlines())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        plaintext = input()
+        key = input()
+        ciphertext = []
+        key_length = len(key)
+        for i, char in enumerate(plaintext):
+            if char == ' ':
+                ciphertext.append(' ')
+            else:
+                key_char = key[i % key_length]
+                shift = ord(key_char) - ord('a') + 1
+                new_ord = ord(char) - shift
+                if new_ord < ord('a'):
+                    new_ord += 26
+                ciphertext.append(chr(new_ord))
+        answer = ''.join(ciphertext)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
