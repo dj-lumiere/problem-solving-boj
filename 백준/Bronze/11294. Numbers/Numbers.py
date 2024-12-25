f5a04edcbd16cd60c83ff6c7ba7b7c9afa78ea06
Lumi <djminstar573@gmail.com>
2024-12-25 21:16:13 +0900

    [Bronze I] Title: Numbers, Time: 216 ms, Memory: 127492 KB -BaekjoonHub

---
+++ 백준/Bronze/11294. Numbers/Numbers.py
@@ -0,0 +1,44 @@
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().splitlines())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = INF
+    answers = []
+    for _ in range(t):
+        name = input()
+        if name == '#':
+            break
+        base = int(input())
+        num = int(input())
+        digits = '0123456789ABCDEFGHIJ'
+        if num == 0:
+            converted = '0'
+        else:
+            converted = ''
+            temp = num
+            while temp > 0:
+                converted = digits[temp % base] + converted
+                temp //= base
+        answer = f"{name}, {num}, {converted}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
