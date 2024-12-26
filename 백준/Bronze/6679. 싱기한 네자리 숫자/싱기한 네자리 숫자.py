10bf5710687cc92050d3228cba0c69f0c0c8ca2c
Lumi <djminstar573@gmail.com>
2024-12-26 11:45:08 +0900

    [Bronze II] Title: 싱기한 네자리 숫자, Time: 264 ms, Memory: 128408 KB -BaekjoonHub

---
+++ 백준/Bronze/6679. 싱기한 네자리 숫자/싱기한 네자리 숫자.py
@@ -0,0 +1,43 @@
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        for num in range(1000, 10000):
+            sum10 = sum(int(d) for d in str(num))
+            sum12 = 0
+            temp = num
+            while temp > 0:
+                sum12 += temp % 12
+                temp = temp // 12
+            sum16 = 0
+            temp = num
+            while temp > 0:
+                sum16 += temp % 16
+                temp = temp // 16
+            if sum10 == sum12 == sum16:
+                answer = num
+                answers.append(f"{answer}")
+        print(*answers, sep="\n")
