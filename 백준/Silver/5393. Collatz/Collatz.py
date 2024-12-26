b81e9d71c57768cb7a34f71649543a54ab960abc
Lumi <djminstar573@gmail.com>
2024-12-26 11:46:06 +0900

    [Silver III] Title: Collatz, Time: 232 ms, Memory: 127496 KB -BaekjoonHub

---
+++ 백준/Silver/5393. Collatz/Collatz.py
@@ -0,0 +1,37 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        N = int(input())
+        count_odds = (N + 1) // 2
+        boundary = (N - 1) // 3
+        count_odds_boundary = (boundary + 1) // 2
+        cut_odds = count_odds - count_odds_boundary
+        cut_evens = N - (N // 2)
+        answer = cut_odds + cut_evens
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
