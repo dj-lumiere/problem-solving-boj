69d37690f54bf64e1f3889d3a2a5db29c32c8526
Lumi <djminstar573@gmail.com>
2024-12-26 11:12:40 +0900

    [Silver V] Title: Right-Coupled Numbers, Time: 368 ms, Memory: 127496 KB -BaekjoonHub

---
+++ 백준/Silver/20256. Right－Coupled Numbers/Right－Coupled Numbers.py
@@ -0,0 +1,38 @@
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        x = int(input())
+        flag = 0
+        for a in range(1, x + 1):
+            if x % a == 0:
+                b = x // a
+                if a <= b <= a * 2:
+                    flag = 1
+                    break
+        answer = flag
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
