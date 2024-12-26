fd5ceccda24f8a1f7090197717ce8cc6f2f2f7eb
Lumi <djminstar573@gmail.com>
2024-12-26 11:44:02 +0900

    [Silver IV] Title: Palindromic Factors, Time: 272 ms, Memory: 128140 KB, Score: 15 point -BaekjoonHub

---
+++ 백준/Silver/25085. Palindromic Factors/Palindromic Factors.py
@@ -0,0 +1,45 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        A = int(input())
+        cnt = 0
+        i = 1
+        while i * i <= A:
+            if A % i == 0:
+                s = str(i)
+                if s == s[::-1]:
+                    cnt += 1
+                j = A // i
+                if j != i:
+                    s = str(j)
+                    if s == s[::-1]:
+                        cnt += 1
+            i += 1
+        answer = f"Case #{hh + 1}: {cnt}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
---
+++ 백준/Silver/25085. Palindromic Factors/README.md
@@ -0,0 +1,30 @@
+# [Silver IV] Palindromic Factors - 25085 
+
+[문제 링크](https://www.acmicpc.net/problem/25085) 
+
+### 성능 요약
+
+메모리: 128140 KB, 시간: 272 ms
+
+### 분류
+
+구현, 수학, 정수론
+
+### 제출 일자
+
+2024년 12월 26일 11:43:56
+
+### 문제 설명
+
+<p>You are given a positive integer $A$. Find the number of factors of $A$ which are palindromes. A number is called a <a href="https://en.wikipedia.org/wiki/Palindromic_number" target="_blank">palindrome</a> if it remains the same when the digits in decimal representation are reversed. For instance, 121 is a palindrome, while 123 is not.</p>
+
+### 입력 
+
+ <p>The first line of the input gives the number of test cases, $T$. $T$ lines follow.</p>
+
+<p>Each line represents a test case and contains a single integer $A$.</p>
+
+### 출력 
+
+ <p>For each test case, output one line containing <code>Case #x: y</code>, where $x$ is the test case number (starting from 1) and $y$ is the number of factors of $A$ which are palindromes.</p>
+
