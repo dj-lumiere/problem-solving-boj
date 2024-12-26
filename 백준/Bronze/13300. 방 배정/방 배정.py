19887d1b6f33659aa7bb25a2248c0f95969653e2
Lumi <djminstar573@gmail.com>
2024-12-26 11:10:01 +0900

    [Bronze II] Title: 방 배정, Time: 248 ms, Memory: 128152 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/13300. 방 배정/방 배정.py
@@ -0,0 +1,38 @@
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        N = int(input())
+        K = int(input())
+        counts = [[0] * 7 for _ in range(2)]
+        for _ in range(N):
+            S = int(input())
+            Y = int(input())
+            counts[S][Y] += 1
+        sum_rooms = sum((counts[S][Y] + K - 1) // K for S in [0, 1] for Y in range(1, 7) if counts[S][Y] > 0)
+        answer = sum_rooms
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
