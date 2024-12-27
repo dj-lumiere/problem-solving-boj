f421e2d421de69448989ed6e962323d307050b9b
Lumi <djminstar573@gmail.com>
2024-12-27 14:40:42 +0900

    [Bronze I] Title: Promotion Counting, Time: 220 ms, Memory: 127496 KB -BaekjoonHub

---
+++ 백준/Bronze/11976. Promotion Counting/Promotion Counting.py
@@ -0,0 +1,38 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        B_before, B_after = int(input()), int(input())
+        S_before, S_after = int(input()), int(input())
+        G_before, G_after = int(input()), int(input())
+        P_before, P_after = int(input()), int(input())
+        z = P_after - P_before
+        y = G_after - G_before + z
+        x = S_after - S_before + y
+        answer = f"{x}\n{y}\n{z}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
