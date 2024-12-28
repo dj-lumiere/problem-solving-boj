83c49b6f9ef6cdee0246301b518dcf1a8bf38d56
Lumi <djminstar573@gmail.com>
2024-12-28 14:57:59 +0900

    [Bronze I] Title: 알고리즘 수업 - 선택 정렬 2, Time: 448 ms, Memory: 131408 KB -BaekjoonHub

---
+++ 백준/Bronze/23882. 알고리즘 수업 － 선택 정렬 2/알고리즘 수업 － 선택 정렬 2.py
@@ -0,0 +1,45 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n, k = int(input()), int(input())
+        A = [int(input()) for _ in range(n)]
+        swap_count = 0
+        answer = -1
+        for last in range(n, 1, -1):
+            max_val = max(A[:last])
+            max_idx = A[:last].index(max_val)
+            if max_idx != last - 1:
+                A[max_idx], A[last - 1] = A[last - 1], A[max_idx]
+                swap_count += 1
+                if swap_count == k:
+                    answer = ' '.join(map(str, A))
+                    break
+        if swap_count < k:
+            answer = -1
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
