a25ea6065a754ff887555c3999e810f332d683dc
Lumi <djminstar573@gmail.com>
2024-12-28 11:00:19 +0900

    [Bronze II] Title: Simple Encryption, Time: 232 ms, Memory: 128016 KB -BaekjoonHub

---
+++ 백준/Bronze/6890. Simple Encryption/Simple Encryption.py
@@ -0,0 +1,40 @@
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().splitlines())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        keyword = input()
+        message = input()
+        shifts = [ord(c) - ord('A') for c in keyword]
+        filtered = ''.join([c for c in message if c.isalpha()])
+        n = len(keyword)
+        encoded = []
+        for i, c in enumerate(filtered):
+            shift = shifts[i % n]
+            shifted = chr((ord(c) - ord('A') + shift) % 26 + ord('A'))
+            encoded.append(shifted)
+        answer = ''.join(encoded)
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
