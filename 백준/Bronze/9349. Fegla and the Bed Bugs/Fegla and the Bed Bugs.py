e752ed7d24a0e77f42339ca5514083b6ebf84615
Lumi <djminstar573@gmail.com>
2024-12-28 10:53:20 +0900

    [Bronze II] Title: Fegla and the Bed Bugs, Time: 216 ms, Memory: 127492 KB -BaekjoonHub

---
+++ 백준/Bronze/9349. Fegla and the Bed Bugs/Fegla and the Bed Bugs.py
@@ -0,0 +1,45 @@
+from decimal import getcontext
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        n, k = int(input()), int(input())
+        left, right, answer = 0, n - 1, 0
+        while left <= right:
+            mid = (left + right) // 2
+            count, last = 1, 0
+            for i in range(1, n):
+                if i - last - 1 >= mid:
+                    count += 1
+                    last = i
+                    if count >= k:
+                        break
+            if count >= k:
+                answer = mid
+                left = mid + 1
+            else:
+                right = mid - 1
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
