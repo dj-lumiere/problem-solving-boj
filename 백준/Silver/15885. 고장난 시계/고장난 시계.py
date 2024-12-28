25dc2301303c723f9cbc16fddf9b82a041f785ef
Lumi <djminstar573@gmail.com>
2024-12-28 11:12:06 +0900

    [Silver V] Title: 고장난 시계, Time: 228 ms, Memory: 127508 KB -BaekjoonHub

---
+++ 백준/Silver/15885. 고장난 시계/고장난 시계.py
@@ -0,0 +1,40 @@
+from decimal import getcontext
+from fractions import Fraction
+from sys import stderr, stdout
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        a, b = (int(input()) for _ in range(2))
+        if a == 0:
+            ans = 2
+        elif a < 0:
+            t_period = 43200 / (1 - Fraction(a, b))
+            ans = 86400 // t_period
+        else:
+            t_period = 43200 / abs(1 - Fraction(a, b))
+            ans = 86400 // t_period
+        answer = f"{ans}"
+        answers.append(f"{answer}")
+    print(*answers, sep="\n")
