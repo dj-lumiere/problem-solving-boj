6501ad6121d6a05ceede4f38ed98cde87699d1e9
Lumi <djminstar573@gmail.com>
2024-12-28 14:49:58 +0900

    [Bronze II] Title: SciComLove (2023), Time: 300 ms, Memory: 161596 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/27913. SciComLove （2023）/SciComLove （2023）.py
@@ -0,0 +1,43 @@
+from decimal import Decimal, getcontext
+from fractions import Fraction
+from sys import stdout, stderr
+
+getcontext().prec = 30
+
+
+def precise_round(numerator: int, denominator: int, precision: int) -> str:
+    scaling_factor = 10 ** precision
+    raw_value = numerator * scaling_factor * 10 // denominator
+    rounded_value = (raw_value + 5) // 10
+    integer_part, fractional_part = divmod(rounded_value, scaling_factor)
+    return f"{integer_part}.{str(fractional_part).zfill(precision)}"
+
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = 1
+    answers = []
+    for hh in range(t):
+        n, q = (int(input()) for _ in range(2))
+        is_upper_base = [c.isupper() for c in 'SciComLove']
+        is_upper = [is_upper_base[i % 10] for i in range(n)]
+        total_upper = sum(is_upper)
+        for _ in range(q):
+            x = int(input())
+            pos = x - 1
+            if is_upper[pos]:
+                total_upper -= 1
+            else:
+                total_upper += 1
+            is_upper[pos] = not is_upper[pos]
+            answer = total_upper
+            answers.append(f"{answer}")
+    print(*answers, sep="\n")
