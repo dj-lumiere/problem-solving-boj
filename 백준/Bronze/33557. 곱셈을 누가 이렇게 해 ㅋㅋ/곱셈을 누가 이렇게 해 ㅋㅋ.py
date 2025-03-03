e56288185c20946c5d9ed99d165a13c8f4b85f1b
Lumi <djminstar573@gmail.com>
2025-03-03 12:29:15 +0900

    [Bronze I] Title: 곱셈을 누가 이렇게 해 ㅋㅋ, Time: 112 ms, Memory: 110984 KB -BaekjoonHub

---
+++ 백준/Bronze/33557. 곱셈을 누가 이렇게 해 ㅋㅋ/곱셈을 누가 이렇게 해 ㅋㅋ.py
@@ -0,0 +1,28 @@
+from itertools import zip_longest
+from sys import stderr, stdout
+
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(1, 0), (0, 1), (-1, 0), (0, -1)]
+    INF = 10 ** 18
+    MOD = 1_000_000_000
+    t = int(input())
+    answers = []
+    for hh in range(t):
+        a, b = int(input()), int(input())
+        correct_answer = a * b
+        if len(str(a)) < len(str(b)):
+            a, b = b, a
+        wrong_answer = int("".join(reversed(list(map(str, (i * j for i, j in
+                                                           zip_longest(map(int, reversed(str(a))), map(int, reversed(str(b))), fillvalue=1)))))))
+        answer = +(correct_answer == wrong_answer)
+        answers.append(answer)
+    print(*answers, sep="\n")
