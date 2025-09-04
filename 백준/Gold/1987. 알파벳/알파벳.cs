d7873e24c18848ae5ca0f5660ae1cc5e5226fabe
Lumi <djminstar573@gmail.com>
2025-09-04 21:01:40 +0900

    [Gold IV] Title: 알파벳, Time: 1904 ms, Memory: 6332 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.cs
@@ -0,0 +1,49 @@
+namespace ProblemSolving;
+
+public class Boj01987
+{
+    public static void Main(string[] args)
+    {
+        var _ = Console.ReadLine()
+                       .Split(' ')
+                       .Select(int.Parse)
+                       .ToArray();
+        var (r, c) = (_[0], _[1]);
+        var graph = Enumerable.Repeat(0, r)
+                              .Select(_ => Console.ReadLine()
+                                                  .ToCharArray()
+                                                  .Select(p => p - 'A')
+                                                  .ToArray())
+                              .ToArray();
+        var answer = 0L;
+        // Bit arrangement: _______C CCCCCCCC ___XXXXX ___YYYYYY ______VV VVVVVVVV VVVVVVVV VVVVVVVV
+        var deltaList = new List<(int, int)> { (1, 0), (-1, 0), (0, 1), (0, -1) };
+        var stack = new Stack<long>();
+        stack.Push((1 << 26) - 1);
+        while (stack.Count > 0)
+        {
+            var state = stack.Pop();
+            var availableAlphabet = (int)state;
+            var y = state >> 32 & 0xff;
+            var x = state >> 40 & 0xff;
+            var count = state >> 48;
+            var alphabet = graph[y][x];
+            if ((availableAlphabet & (1 << alphabet)) == 0) continue;
+            availableAlphabet ^= (1 << alphabet);
+            answer = Math.Max(answer, count + 1);
+            foreach (var (dx, dy) in deltaList)
+            {
+                var nx = x + dx;
+                var ny = y + dy;
+                if (nx < 0 || nx >= c || ny < 0 || ny >= r) continue;
+                var nextState = count + 1 << 48 | nx << 40 |
+                                ny << 32 | (uint)availableAlphabet;
+                stack.Push(nextState);
+            }
+
+            availableAlphabet ^= (1 << alphabet);
+        }
+
+        Console.WriteLine(answer);
+    }
+}
