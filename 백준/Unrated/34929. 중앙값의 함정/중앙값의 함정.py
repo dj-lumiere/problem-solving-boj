8028c88d2ade408f9609a52b30b704c8f4de7aaf
Lumi <djminstar573@gmail.com>
2025-12-23 10:14:02 +0900

    [Unrated] Title: 중앙값의 함정, Time: 240 ms, Memory: 176892 KB -BaekjoonHub

---
+++ 백준/Unrated/34929. 중앙값의 함정/중앙값의 함정.py
@@ -0,0 +1,32 @@
+from sys import stderr, stdout
+ 
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 998_244_353
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        n = int(input())
+        a = [int(input()) for _ in range(n)]
+        a.sort(reverse=True)
+        if n == 1:
+            answers.append(a[0])
+            continue
+        answers.append(a[1])
+        for i in range((n - 1) // 2):
+            answers.append(f"{a[-1]} {a[-2]} {a[-3]}")
+            a.pop()
+            v = a.pop()
+            a.pop()
+            a.append(v)
+    print(*answers, sep="\n")
