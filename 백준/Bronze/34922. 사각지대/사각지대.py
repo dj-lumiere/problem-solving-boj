af0b9090ddd76edfc26593cafae55a0b09f3e78c
Lumi <djminstar573@gmail.com>
2025-12-23 10:11:06 +0900

    [Bronze III] Title: 사각지대, Time: 88 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/34922. 사각지대/사각지대.py
@@ -0,0 +1,24 @@
+from math import pi
+from sys import stderr, stdout
+ 
+with open(0, 'r') as f:
+    tokens = iter(f.read().split())
+    input = lambda: next(tokens, None)
+    print = lambda *args, sep="\n", end="\n": stdout.write(sep.join(map(str, args)) + end)
+    rprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    eprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(str, args)) + end)
+    erprint = lambda *args, sep=" ", end="\n": stderr.write(sep.join(map(repr, args)) + end)
+    fprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(str, args)) + end)
+    frprint = lambda *args, sep=" ", end="\n", file: file.write(sep.join(map(repr, args)) + end)
+    is_inbound = lambda pos_x, size_x, pos_y, size_y: 0 <= pos_x < size_x and 0 <= pos_y < size_y
+    DELTA = [(0, 0), (0, -1), (0, 1), (-1, 0), (1, 0)]
+    INF = 10 ** 18
+    MOD = 998_244_353
+    t = 1
+    answers = []
+    for hh in range(1, t + 1):
+        w = int(input())
+        h = int(input())
+        r = int(input())
+        answers.append(w * h - r * r * pi / 4)
+    print(*answers, sep="\n")
